461af7962ab3df74d003dd4047aec35d76cd69ef
Yacine Petitprez <anykeyh@gmail.com>
2025-05-07 23:11:41 +0700

    Add basics libraries to prepare for the editor

---
+++ app/frontend/src/lib/shortcut/ShortcutManager.ts
@@ -0,0 +1,53 @@
+export type KeyMap = {
+  [key: string]: {
+    name: string,
+    description: string,
+    action: () => void
+  }
+}
+
+type KeyMapList = {
+  [key: string]: {
+    [key: string]: {
+      name: string,
+      description: string,
+      action: () => void
+    }
+  }
+}
+
+export const ShortcutManager = {
+  defaultMode: 'visual',
+  currentMode: [] as string[],
+
+  // Dictionary of all the shortcuts:
+  keyMap : {} as KeyMapList,
+
+  enterMode(mode: string, replace: boolean = false) {
+    if (replace) {
+      if (this.currentMode.length==0) {
+        this.currentMode[0] = mode;
+      } else {
+        this.currentMode[this.currentMode.length-1] = mode;
+      }
+    }
+    else {
+      this.currentMode.push(mode);
+    }
+  },
+
+  leaveMode() {
+    this.currentMode.pop();
+    if(this.currentMode.length==0) {
+      this.currentMode[0] = this.defaultMode;
+    }
+  },
+
+  getCurrentMode() {
+    return this.currentMode[this.currentMode.length-1];
+  },
+
+  registerKeyMap(mode: string, keyMap: KeyMap) {
+    this.keyMap[mode] = keyMap;
+  }
+};
