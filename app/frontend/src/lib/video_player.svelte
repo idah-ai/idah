c45ad0e56dddeb25d236f7f3fca2acdb5d42fbde
Brian West <brian.west@ingedata.ai>
2025-06-12 17:38:01 +0700

    wip annotation selection

--- app/frontend/src/lib/video_player.svelte
+++ app/frontend/src/lib/video_player.svelte
@@ -1,46 +1,78 @@
-<script>
+<script lang='ts'>
     import videojs from 'video.js'
     import 'video.js/dist/video-js.css'
     import { onMount, onDestroy } from 'svelte';
+	import type Player from 'video.js/dist/types/player';
 
+    const ZOOM_STEP = 0.5
+    const MIN_SCALE = 1
+    const MAX_SCALE = 40
 
-    let isPlaying = false;
-    let player;
-    let fps = 30;
-    let volume = 0;
-    let duration;
-    let frames;
-    let current_frame;
+    let {selections}  = $props();
+    let width = 896;
+    let height = 400;
+    let scale = $state(1);
 
-    onMount(()=> {
-        player = videojs("Videotest", {
-            controls: "false",
-            preload:"auto",
+    let video_height: number = $state(0);
+    let video_width: number = $state(0);
+
+    let videoElement: HTMLVideoElement;
+
+    let options = {
+            controls: false,
+            preload: "auto",
             loop: false,
             muted: true,
-            fluid: false,
-            poster:"http://video-js.zencoder.com/oceans-clip.png",
-        }, () => {
-            console.log("player loaded ?")
+            // responsive: true,
+            fluid: true,
+            sources: [{
+                src: 'https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8',
+            }]
+            // poster:"http://video-js.zencoder.com/oceans-clip.png",
+        }
+    let player: Player;
+    let fps = 24; // Needs to be known
+    let volume = $state(0);
+    let duration = $state(0);
+    let frames = $state(0);
+    let current_frame = $state(0);
+    let offsetX = $state(0);
+    let offsetY = $state(0);
+
+    onMount(()=> {
+        player = videojs(videoElement, options, () => {
+            console.log("this ?", player, videojs.getPlayer(videoElement))
+            video_height = player.currentHeight();
+            video_width = player.currentWidth();
+
+            console.log(player.getMedia())
+            console.log(video_width, video_height)
+
+            player.on('loadeddata', () => {
+                duration = player.duration() || 0;
+                frames = Math.round(duration * fps)
+            })
+
+            player.on('timeupdate', () => {
+                current_frame = Math.round((player.currentTime() || 0) * fps)
+            });
+
+            player.on('resize', () => {
+                let position = player.getPositions()
+                console.log(player.getMedia())
+
+                
+                video_height = position.boundingClientRect.height;
+                video_width = position.boundingClientRect.width;
+
+                console.log("size", 100/video_width, 100/video_height, 200/video_width, 200/video_height)
+
+            })
         })
-        player.on('loadeddata', () => {
-            duration = player.duration();
-            frames = duration * fps;
-        })
-        player.on('timeupdate', () => {
-            current_frame = (player.currentTime() * fps)
-            console.log(current_frame);
-        });
-        player.on('play', () => {
-            isPlaying = true;
-        });
-        player.on('pause', () => {
-            isPlaying = false;
-        });
     })
 
     onDestroy(() => {
-        player.dispose();
+        player?.dispose()
     })
 
     function togglePlay() {
@@ -53,56 +85,152 @@
     function nextFrame() {
         if (!player.paused())
             player.pause()
-        player.currentTime(player.currentTime() + 1 /fps)
+        player.currentTime((current_frame + 1) / fps)
     }
 
     function previousFrame() {
         if (!player.paused())
             player.pause()
-        player.currentTime(player.currentTime() - 1 /fps)
+        player.currentTime((current_frame - 1) / fps)
     }
 
-    function mute() {
+    function toggleMute() {
         player.muted(!player.muted())
     }
 
-    function setVolume(e) {
+    function setVolume(percent: number) {
         if (player.muted())
             player.muted(false)
-        player.volume(e.target.value/100)
+        player.volume(e.target.value / 100)
     }
 
-    function seek(e) {
-        player.currentTime(e.target.value/fps)
+    function onVolumeChange(e){
+        setVolume(e.target.value)
+    }
+
+    function seek_to_frame(frame: number) {
+        player.currentTime(frame / fps)
+    }
+
+    function onFrameSelection(e) {
+        seek_to_frame(e.target.value)
     }
 
     function zoom(e) {
+        e.preventDefault();
+       console.log(e)
+        console.log(e.offsetX, e.offsetY);
+        offsetX = (e.offsetX - offsetX)/scale
+        offsetY = (e.offsetY - offsetY)/scale
+ 
+        let delta = e.wheelDeltaY > 0 ? -ZOOM_STEP : ZOOM_STEP;
+        scale = Math.min(Math.max(MIN_SCALE, scale + delta), MAX_SCALE)
+
+        offsetX = e.offsetX - (offsetX * scale)
+        offsetY = e.offsetY - (offsetY * scale)
+
+        let maxOffsetX = (width * scale) - width
+        let maxOffsetY = (height * scale) - height
+
+        offsetX = Math.max(-maxOffsetX, Math.min(offsetX, maxOffsetX))
+        offsetY = Math.max(-maxOffsetY, Math.min(offsetY, maxOffsetY))
+
+        // videoElement.style.transform = `translate(${offsetX}px, ${offsetY}px) scale(${scale})`;
+
+        video_height = videojs.getPlayer(videoElement)?.currentHeight() || 0 * scale
+        video_width = videojs.getPlayer(videoElement)?.currentWidth() || 0 * scale
+
+
+        // console.log(video_height, video_width);
+
     }
+
 </script>
 
-<ul>
-    <li>duration: { duration }</li>
-    <li>playing: { isPlaying }</li>
+<!-- <ul>
+    <li>duration: { duration } seconds</li>
+    <li>fps: { fps }</li>
     <li>frames: { frames } </li>
     <li>current_frame: { current_frame } </li>
+    <li>offset: { offsetX }/ { offsetY } </li>
+    <li>Container size: { width } / { height }</li>
+    <li>scale: { scale } </li>
+    <li>Video size: {width * scale}/{height * scale}</li>
 </ul>
+ -->
+<div style='overflow:hidden; width:{width}px; height:{height}px'>
+    <video-js onwheel={zoom}
+        bind:this={videoElement}
+        style:transform-origin= 'top left'
+        style:transform = "translate({offsetX}px, {offsetY}px) scale({scale})"
+        >
+    </video-js>
+    <svg
+        onwheel={zoom}
+        height= {video_height}
+        width= {video_width}
+        style:stroke = 'yellow'
+        style:stroke-width= {1}
+        style:fill-opacity= {0}
+        style:transform-origin = "top left"
+        style:transform = "translate(0px, {-height}px)"
+        >
+        {#each selections as selection}
+        {console.log(selection,                selection.map((point : number[]) => {
+                    console.log('map',point);
+                    return [point[0] * video_width, point[1] * video_height]
+                }))}
+        <polygon
+            vector-effect= "non-scaling-stroke"
+            points= {
+                selection.map((point : number[]) => {
+                    console.log('map',point);
+                    // point
+                    return [point[0] * video_width, point[1] * video_height]
+                }).reduce(
+                    (acc: string, cur: number[]) => {
+                        console.log('reducer',acc, cur)
+                        return acc.concat(`${cur[0]},${cur[1]} `)
+                    },
+                    ""
+                )
+            }
+            style:transform-origin = "top left"
+            style:transform = "translate({offsetX}px, {offsetY}px) scale({scale})"
 
-<p></p>
-,.
-<!-- <video id='Videotest' class='video-js'>
-    <source src="//vjs.zencdn.net/v/oceans.mp4" type="video/mp4">
-    <source src="//vjs.zencdn.net/v/oceans.webm" type="video/webm">
-    <track kind="captions">
-</video> -->
-<video-js
-    id='Videotest'>
-    <source src="//vjs.zencdn.net/v/oceans.mp4" type="video/mp4">
-    <source src="//vjs.zencdn.net/v/oceans.webm" type="video/webm">
-    <track kind="captions"/>
-</video-js>
-<button onclick={previousFrame}>{'<'}</button>
-<button onclick={togglePlay}>p</button>
-<button onclick={nextFrame}>{'>'}</button>
-<button onclick={mute}>{'M'}</button>
-<input type='range' min=0 max=100 step=1 value={volume} oninput={setVolume}/>
-<input type='range' min=0 max={frames} step=1 value={current_frame} oninput={seek}/>
+        />
+        {/each}
+<!-- 
+        <polygon
+            vector-effect= "non-scaling-stroke"
+            points= "{0.11160714285714286 * video_width},{0.25 * video_height} {0.11160714285714286 * video_width},{0.5 * video_height} {0.22321428571428573 * video_width},{0.5 * video_height} {0.22321428571428573 * video_width},{0.25 * video_height}"
+            style:transform-origin = "top left"
+            style:transform = "translate({offsetX}px, {offsetY}px) scale({scale})"
+
+            />
+        <polygon
+            points= "250,100 350,100 350,200 250,200"
+            vector-effect= "non-scaling-stroke" 
+            style:transform = "translate({offsetX}px, {offsetY}px) scale({scale})"
+        /> -->
+    </svg>
+</div>
+        <ul>
+            <li>duration: { duration } seconds</li>
+            <li>fps: { fps }</li>
+            <li>frames: { frames } </li>
+            <li>current_frame: { current_frame } </li>
+            <li>offset: { offsetX }/ { offsetY } </li>
+            <li>Container size: { width } / { height }</li>
+            <li>scale: { scale } </li>
+            <li>Video size: {width * scale}/{height * scale}</li>
+        </ul>
+
+<div>
+    <button onclick={previousFrame}>{'<'}</button>
+    <button onclick={togglePlay}>{ 'P' }</button>
+    <button onclick={nextFrame}>{'>'}</button>
+    <button onclick={toggleMute}>{'M'}</button>
+    <input type='range' min=0 max=100 step=1 value={volume} oninput={onVolumeChange}/>
+    <input type='range' min=0 max={frames} step=1 value={current_frame} oninput={onFrameSelection}/>
+</div>
--- app/frontend/src/routes/+page.svelte
+++ app/frontend/src/routes/+page.svelte
@@ -1,9 +1,17 @@
 <script>
     import VideoPlayer from "$lib/video_player.svelte";
+
+    let a = [
+        [[0.11160714285714286 ,0.25], [0.11160714285714286,0.5],[0.22321428571428573,0.5], [0.22321428571428573,0.25]]
+    ]
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
 
-<VideoPlayer />
+<VideoPlayer
+    selections= {[
+        [[0.11160714285714286 ,0.25], [0.11160714285714286,0.5],[0.22321428571428573,0.5], [0.22321428571428573,0.25]]
+    ]}
+/>
