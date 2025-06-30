87e153476b8216880fc56c57778a9cc4c90a4227
Yacine Petitprez <anykeyh@gmail.com>
2025-06-30 14:26:40 +0700

    wip

--- app/media/app/expo/healthcheck_expo.rb
+++ app/media/app/expo/healthcheck_expo.rb
@@ -1,33 +1,41 @@
-  class HealthcheckExpo < Verse::Exposition::Base
-    http_path "/healthcheck"
+# frozen_string_literal: true
 
-    expose on_http(:get, "", auth: nil, renderer: Verse::Http::Renderer::Identity) do
-      desc <<-MD
-        Health check endpoint.
+class HealthcheckExpo < Verse::Exposition::Base
+  http_path "/healthcheck"
 
-        Returns the status of dependencies such as Redis and PostgreSQL.
-        If any dependency is down, it returns a failure status
-        with a message indicating the issue,
-        and sets the response status to 500.
-      MD
-      meta nodoc: true
-    end
-    def healthcheck
-      output = HealthcheckService.run
+  expose on_http(:get, "", auth: nil, renderer: Verse::Http::Renderer::Identity) do
+    desc <<-MD
+    expose on_schedule("17 1 * * *", per_service: false)
+      ## Health check
 
-      server.response.status = 500 unless output.success?
+      Returns the status of dependencies such as Redis and PostgreSQL.
+      If any dependency is down, it returns a failure status
+      with a message indicating the issue,
+      and sets the response status to 500.
+    MD
+    meta nodoc: true
+  end
+  def healthcheck
+    output = HealthcheckService.run
 
-      output.status.to_json
-    end
+    server.response.status = 500 unless output.success?
 
-    # Need verse-periodic gem.
-    if respond_to?(:on_schedule)
-      # Every day at 1.17am, run a memory maintenance task and compact the memory.
-      # This run on each service instance.
-      expose on_schedule("17 1 * * *", per_service: false)
-      def mem_maintenance
-        GC.start
-        GC.compact
-      end
+    output.status.to_json
+  end
+
+  # Need verse-periodic gem.
+  if respond_to?(:on_schedule)
+    # Every day at 1.17am, run a memory maintenance task and compact the memory.
+    # This run on each service instance.
+    expose on_schedule(
+      ENV.fetch(
+        "GC_COMPACT_CRON_RULE", "17 1 * * *"
+      ),
+      per_service: false
+    )
+    def mem_maintenance
+      GC.start
+      GC.compact
     end
   end
+end
