71a7b71ad5be1f0cfc4e3cbb348dee86383aca03
whelsm <jiranop.phinyo@gmail.com>
2025-08-14 13:24:49 +0700

    feat: add DateRangePickerField component with date range selection and integrated RangeCalendar

---
+++ app/frontend/src/lib/components/app/forms/fields/picker/DateRangePickerField.svelte
@@ -0,0 +1,118 @@
+<script lang="ts">
+	import { format } from "date-fns";
+
+	import Button from "$/lib/components/ui/button/button.svelte";
+	import FormField from "$/lib/components/app/forms/FormField.svelte";
+	import FormFieldErrors from "$/lib/components/app/forms/FormFieldErrors.svelte";
+	import FormFieldInfo from "$/lib/components/app/forms/FormFieldInfo.svelte";
+	import FormFieldLabel from "$/lib/components/app/forms/FormFieldLabel.svelte";
+	import { Popover, PopoverTrigger, PopoverContent } from "$/lib/components/ui/popover";
+	import RangeCalendar from "$/lib/components/ui/range-calendar/range-calendar.svelte";
+
+	import { cn } from "$/lib/utils";
+	import { CalendarIcon } from "@lucide/svelte";
+	import { CalendarDate, DateFormatter, type DateValue } from "@internationalized/date";
+
+	import type { DateRange } from "bits-ui";
+	import type { DateFieldBaseProps } from "$/lib/components/app/forms/fields/picker/DateField.types";
+
+	// Props
+	interface Props extends DateFieldBaseProps {
+		from: Date | null;
+		to: Date | null;
+		onEndValueChanged?: (value: DateValue | undefined) => void;
+	}
+	let {
+		label,
+		disabled = false,
+		required = false,
+		placeholder = "Select a date range",
+		info,
+		errors,
+		placeholderDateFormat = "MMM dd, yyyy",
+		dateStyle = "medium",
+		from = $bindable(null),
+		to = $bindable(null),
+		class: className,
+		onEndValueChanged = () => {},
+		slotLabel,
+		slotInfo,
+		slotErrors,
+	}: Props = $props();
+
+	// Variables
+	const df = new DateFormatter("en-US", { dateStyle });
+
+	let calendarValue = $state<DateRange | undefined>();
+
+	// Effects
+	$effect(() => {
+		if (calendarValue) {
+			from = calendarValue.start ? new Date(calendarValue.start.toString()) : null;
+			to = calendarValue.end ? new Date(calendarValue.end.toString()) : null;
+		} else if (from && to) {
+			const fromDatevalue = new Date(from);
+			const toDateValue = new Date(to);
+			calendarValue = {
+				start: new CalendarDate(fromDatevalue.getFullYear(), fromDatevalue.getMonth() + 1, fromDatevalue.getDate()),
+				end: new CalendarDate(toDateValue.getFullYear(), toDateValue.getMonth() + 1, toDateValue.getDate()),
+			};
+		} else {
+			from = null;
+			to = null;
+			calendarValue = undefined;
+		}
+	});
+
+	// Functions
+	function handleEndValueChange(value: DateValue | undefined) {
+		onEndValueChanged?.(value);
+	}
+</script>
+
+<FormField class={cn("", className)}>
+	{#if slotLabel}
+		{@render slotLabel()}
+	{:else}
+		<FormFieldLabel {required}>{label}</FormFieldLabel>
+	{/if}
+
+	<Popover>
+		<PopoverTrigger>
+			{#snippet child({ props })}
+				<Button
+					variant="outline"
+					class={cn("w-full justify-start text-left font-normal", {
+						"text-muted-foreground": !calendarValue,
+					})}
+					{disabled}
+					{...props}
+				>
+					<CalendarIcon class="size-4" />
+
+					{#if from && to}
+						{format(from, placeholderDateFormat)} - {format(to, placeholderDateFormat)}
+					{:else}
+						{placeholder}
+					{/if}
+				</Button>
+			{/snippet}
+		</PopoverTrigger>
+
+		<PopoverContent class="w-auto p-0">
+			<RangeCalendar bind:value={calendarValue} onEndValueChange={handleEndValueChange} />
+		</PopoverContent>
+	</Popover>
+
+	{#if slotInfo}
+		{@render slotInfo()}
+	{:else}
+		<FormFieldInfo>{info}</FormFieldInfo>
+	{/if}
+
+	{#if slotErrors}
+		{@render slotErrors()}
+	{:else}
+		<FormFieldErrors {errors} />
+	{/if}
+</FormField>
