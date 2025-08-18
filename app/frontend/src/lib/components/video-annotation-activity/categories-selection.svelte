c55911f0fe48bc5853fb14aed1f958c89ac5af82
whelsm <jiranop.phinyo@gmail.com>
2025-08-18 09:37:09 +0700

    Refactor video annotation components for improved readability and consistency
    - Updated import paths in `categories-selection.svelte` for better clarity.
    - Renamed variables for consistency (e.g., `current_frame` to `currentFrame`).
    - Simplified functions and improved formatting for better readability.
    - Enhanced the structure of the `controls.svelte` component, including consistent use of imports and improved button handling.
    - Refined the `timeline-table` and `timeline` components for better state management and clearer logic.
    - Improved event handling and state updates across components.

--- app/frontend/src/lib/components/video-annotation-activity/categories-selection.svelte
+++ app/frontend/src/lib/components/video-annotation-activity/categories-selection.svelte
@@ -1,109 +1,103 @@
-<script lang=ts>
+<script lang="ts">
+	import Badge from "@/components/ui/badge/badge.svelte";
+	import Button from "@/components/ui/button/button.svelte";
+	import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@/components/ui/collapsible";
+	import { SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar";
+
+	import type { VideoAnnotation } from "@/components/video-annotation-activity/VideoAnnotationContext";
 	import type { CategoriesDefinition, CategoryDefinition } from "@/context/ActivityContext";
-	import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "../ui/collapsible";
-	import Button from "../ui/button/button.svelte";
-	import { SidebarMenuButton, SidebarMenuItem } from "../ui/sidebar";
-	import type { VideoAnnotation } from "./VideoAnnotationContext";
-	import { Badge } from "../ui/badge";
 
-    let { current_frame, annotations, categories, selected, onSelect, onSelectAnnotation, required = false } :{
-        current_frame: number,
-        annotations: VideoAnnotation[],
-        categories: CategoriesDefinition,
-        selected: string | undefined,
-        onSelect: (selection?: string) => void,
-        onSelectAnnotation: (annotation: VideoAnnotation) => void,
-        required: boolean
-    } = $props()
+	let {
+		currentFrame,
+		annotations,
+		categories,
+		selected,
+		onSelect,
+		onSelectAnnotation,
+		required = false,
+	}: {
+		currentFrame: number;
+		annotations: VideoAnnotation[];
+		categories: CategoriesDefinition;
+		selected: string | undefined;
+		onSelect: (selection?: string) => void;
+		onSelectAnnotation: (annotation: VideoAnnotation) => void;
+		required: boolean;
+	} = $props();
 
+	// console.log(getAllCategories(categories))
 
-    // console.log(getAllCategories(categories))
+	function getAllCategories(CategoriesDefinition: CategoriesDefinition): string[] {
+		return Object.entries(categories)
+			.flatMap(([category, subCategories]) => allSubCategories(category, subCategories))
+			.map((c) => c.join("/"));
+	}
 
-    function getAllCategories(
-        CategoriesDefinition :CategoriesDefinition
-    ): string[] {
-        return Object.entries(categories)
-            .flatMap((
-                [category, subCategories]
-            ) => allSubCategories(category, subCategories))
-            .map(c => c.join("/"))
-    }
+	function allSubCategories(
+		category: string,
+		subCategories: CategoryDefinition[] | undefined,
+		parent: string[] = [],
+	): string[][] {
+		if (subCategories) {
+			return subCategories.flatMap((c) => allSubCategories(c.name, c.nestedCategories, [...parent, category]));
+		} else {
+			return [[...parent, category]];
+		}
+	}
 
-    function allSubCategories  (
-        category :string,
-        subCategories: CategoryDefinition[] | undefined,
-        parent:string[] = []) :string[][] {
-        if (subCategories) {
-            return subCategories.flatMap((c) => allSubCategories(c.name, c.nestedCategories, [...parent, category]))
-        } else {
-            return [[...parent, category]]
-        }
-    }
-
-function categoryFullName(category: string, parent:string[]) { return [...parent, category].join('/')}
+	function categoryFullName(category: string, parent: string[]) {
+		return [...parent, category].join("/");
+	}
 </script>
 
 {#snippet categorySelection(
-    category: string,
-    subCategories: CategoryDefinition[] | undefined,
-    onSelect: (selection?: string) => void,
-    selected: string | undefined,
-    parent: string[] = []
+	category: string,
+	subCategories: CategoryDefinition[] | undefined,
+	onSelect: (selection?: string) => void,
+	selected: string | undefined,
+	parent: string[] = [],
 )}
-<!-- {required ? (selected ? (selected.startsWith(categoryFullName(category, parent)) ? 'bg-green-300': 'bg-green-200') : 'bg-red-200') : selected?.startsWith(categoryFullName(category, parent)) ? 'bg-green-200': 'light'} -->
-    <Collapsible open={selected?.startsWith(categoryFullName(category, parent))}>
-        <CollapsibleTrigger>
-            {category}
-            <Badge>
-                {
-                    annotations.filter(annotation =>
-                        annotation.value.category?.startsWith(
-                            categoryFullName(category, parent)
-                        ) && current_frame >= annotation.shape.start
-                        && current_frame <= annotation.shape.end
-                    ).length
-                }
-            </Badge>
-        </CollapsibleTrigger>
-            {#if selected == categoryFullName(category, parent) }
-            <Button onclick={() => onSelect()}>-</Button>
-            {/if}
-            {#if selected != categoryFullName(category, parent)}
-                <Button onclick={
-                    () => onSelect(categoryFullName(category, parent))
-                }>
-                    +
-                </Button>
-            {/if}
+	<!-- {required ? (selected ? (selected.startsWith(categoryFullName(category, parent)) ? 'bg-green-300': 'bg-green-200') : 'bg-red-200') : selected?.startsWith(categoryFullName(category, parent)) ? 'bg-green-200': 'light'} -->
+	<Collapsible open={selected?.startsWith(categoryFullName(category, parent))}>
+		<CollapsibleTrigger>
+			{category}
+			<Badge>
+				{annotations.filter(
+					(annotation) =>
+						annotation.value.category?.startsWith(categoryFullName(category, parent)) &&
+						currentFrame >= annotation.shape.start &&
+						currentFrame <= annotation.shape.end,
+				).length}
+			</Badge>
+		</CollapsibleTrigger>
+		{#if selected == categoryFullName(category, parent)}
+			<Button onclick={() => onSelect()}>-</Button>
+		{/if}
+		{#if selected != categoryFullName(category, parent)}
+			<Button onclick={() => onSelect(categoryFullName(category, parent))}>+</Button>
+		{/if}
 
-        <CollapsibleContent style={'margin-left:10px'}>
-            {#if (subCategories) }
-                {#each subCategories as subCategory}
-                    {@render categorySelection(
-                        subCategory.name,
-                        subCategory.nestedCategories,
-                        onSelect,
-                        selected,
-                        [...parent, category]
-                    )}
-                {/each}
-            {/if}
+		<CollapsibleContent style={"margin-left:10px"}>
+			{#if subCategories}
+				{#each subCategories as subCategory}
+					{@render categorySelection(subCategory.name, subCategory.nestedCategories, onSelect, selected, [
+						...parent,
+						category,
+					])}
+				{/each}
+			{/if}
 
-                {#each annotations.filter(
-                    annotation => annotation.value.category == categoryFullName(category, parent)
-                        && current_frame >= annotation.shape.start
-                        && current_frame <= annotation.shape.end
-                )  as annotation, i}
-                <SidebarMenuItem>
-                    <SidebarMenuButton onclick={() => onSelectAnnotation(annotation)}>
-                        {annotation.value.label || `${category}_${i}`}
-                    </SidebarMenuButton>
-                </SidebarMenuItem>
-                {/each}
-        </CollapsibleContent>
-    </Collapsible>
+			{#each annotations.filter((annotation) => annotation.value.category == categoryFullName(category, parent) && currentFrame >= annotation.shape.start && currentFrame <= annotation.shape.end) as annotation, i}
+				<SidebarMenuItem>
+					<SidebarMenuButton onclick={() => onSelectAnnotation(annotation)}>
+						{annotation.value.label || `${category}_${i}`}
+					</SidebarMenuButton>
+				</SidebarMenuItem>
+			{/each}
+		</CollapsibleContent>
+	</Collapsible>
 {/snippet}
 
 {#each Object.entries(categories) as [category, subCategory]}
-    {@render categorySelection(category, subCategory, onSelect, selected) }
+	{@render categorySelection(category, subCategory, onSelect, selected)}
 {/each}
