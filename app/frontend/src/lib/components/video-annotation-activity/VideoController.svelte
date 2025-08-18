87895d0d7cae8faf1ff0a265287c96128a5a5aea
whelsm <jiranop.phinyo@gmail.com>
2025-08-18 09:37:09 +0700

    Refactor video annotation components for improved readability and consistency
    - Updated import paths in `categories-selection.svelte` for better clarity.
    - Renamed variables for consistency (e.g., `current_frame` to `currentFrame`).
    - Simplified functions and improved formatting for better readability.
    - Enhanced the structure of the `controls.svelte` component, including consistent use of imports and improved button handling.
    - Refined the `timeline-table` and `timeline` components for better state management and clearer logic.
    - Improved event handling and state updates across components.

---
+++ app/frontend/src/lib/components/video-annotation-activity/VideoController.svelte
@@ -0,0 +1,182 @@
+<script lang="ts">
+	import Button from "@/components/ui/button/button.svelte";
+	import Input from "@/components/ui/input/input.svelte";
+	import {
+		ChevronLeftIcon,
+		ChevronRightIcon,
+		FastForwardIcon,
+		PauseIcon,
+		PlayIcon,
+		Volume2Icon,
+		VolumeXIcon,
+		ZoomInIcon,
+	} from "@lucide/svelte";
+	import { Popover, PopoverTrigger, PopoverContent } from "@/components/ui/popover";
+	import {
+		DropdownMenu,
+		DropdownMenuContent,
+		DropdownMenuGroup,
+		DropdownMenuItem,
+		DropdownMenuLabel,
+		DropdownMenuTrigger,
+	} from "@/components/ui/dropdown-menu";
+	import Slider from "@/components/ui/slider/slider.svelte";
+	import Video from "@/components/video-annotation-activity/video.svelte";
+
+	import type { ChangeEventHandler } from "svelte/elements";
+
+	// Props
+	interface Props {
+		video: Video;
+		currentFrame: number;
+		totalFrames: number;
+	}
+	let { video = $bindable(), currentFrame, totalFrames }: Props = $props();
+
+	// Variables
+	interface VideoSpeedMenuItem {
+		label: string;
+		value: number;
+	}
+	const videoSpeeds: VideoSpeedMenuItem[] = [
+		{ label: "0.25 X", value: 0.25 },
+		{ label: "0.5 X", value: 0.5 },
+		{ label: "1 X", value: 1 },
+		{ label: "1.25 X", value: 1.25 },
+		{ label: "1.5 X", value: 1.5 },
+		{ label: "2 X", value: 2 },
+		{ label: "3 X", value: 3 },
+		{ label: "5 X", value: 5 },
+	];
+
+	let volume: number = $state(50);
+	let scale: number = $state(100);
+	let currentSpeed: number = $state(1);
+	let isPlaying: boolean = $state(false);
+	let isMuted: boolean = $derived(volume === 0);
+
+	// Functions
+	function setVolume(volumeToSet: number): void {
+		video.setVolume(volumeToSet);
+	}
+
+	const seekToFrame: ChangeEventHandler<HTMLInputElement> = (event) => {
+		const target = event.target as HTMLInputElement;
+		const { value } = target;
+		video.seekToFrame(Number(value));
+	};
+
+	function selectVideoSpeed(selectedSpeed: number): void {
+		currentSpeed = selectedSpeed;
+	}
+</script>
+
+<div id="video-controller" class="flex w-full items-center justify-between gap-4">
+	<!-- CONTAINER::LEFT -->
+	<div class="flex items-center gap-2">
+		<!-- VIDEO::PREVIOUS FRAME -->
+		<Button variant="outline" size="icon" onclick={() => video.previousFrame()}>
+			<ChevronLeftIcon class="size-4" />
+		</Button>
+
+		<!-- VIDEO::PLAY / PAUSE -->
+		<Button
+			variant="outline"
+			size="icon"
+			onclick={() => {
+				video.togglePlay();
+				isPlaying = !isPlaying;
+			}}
+		>
+			{#if isPlaying}
+				<PauseIcon class="size-4" />
+			{:else}
+				<PlayIcon class="size-4" />
+			{/if}
+		</Button>
+
+		<!-- VIDEO::NEXT FRAME -->
+		<Button variant="outline" size="icon" onclick={() => video.nextFrame()}>
+			<ChevronRightIcon class="size-4" />
+		</Button>
+
+		<!-- VIDEO::VOLUME -->
+		<Popover>
+			<PopoverTrigger>
+				<Button variant="outline" size="icon">
+					{#if isMuted}
+						<VolumeXIcon class="size-4" />
+					{:else}
+						<Volume2Icon class="size-4" />
+					{/if}
+				</Button>
+			</PopoverTrigger>
+
+			<PopoverContent side="top" align="center" class="max-w-10 px-2">
+				<Slider
+					type="single"
+					orientation="vertical"
+					min={0}
+					max={100}
+					step={1}
+					onValueChange={setVolume}
+					bind:value={volume}
+				/>
+			</PopoverContent>
+		</Popover>
+
+		<!-- VIDEO::FRAME ADJUSTER -->
+		<div class="inline-flex items-center gap-1 whitespace-nowrap">
+			<Input
+				type="number"
+				class="min-w-24"
+				placeholder="Frame"
+				min={0}
+				max={Math.max(0, totalFrames - 1)}
+				value={currentFrame}
+				onchange={seekToFrame}
+			/>
+			<span>/ {Math.max(0, totalFrames - 1)}</span>
+		</div>
+	</div>
+
+	<!-- CONTAINER::CENTER -->
+	<Button>Auto Track</Button>
+
+	<!-- CONTAINER::RIGHT -->
+	<div class="flex items-center gap-2">
+		<!-- VIDEO::SCALE ADJUSTER (ZOOM IN / ZOOM OUT) -->
+		<Popover>
+			<PopoverTrigger>
+				<Button variant="outline" size="icon">
+					<ZoomInIcon class="size-4" />
+				</Button>
+			</PopoverTrigger>
+
+			<PopoverContent side="top" align="center" class="max-w-10 px-2">
+				<Slider type="single" orientation="vertical" min={20} max={150} step={1} bind:value={scale} />
+				<!-- Will implement zoom functionality later when we separate TimelineTable from Controls component -->
+				<!-- onValueChange={(value) => timeline_table.setScale(value)} -->
+			</PopoverContent>
+		</Popover>
+
+		<!-- VIDEO::SPEED -->
+		<DropdownMenu>
+			<DropdownMenuTrigger>
+				<Button variant="outline">
+					<FastForwardIcon class="size-4" />
+					{videoSpeeds.find((speed) => speed.value === currentSpeed)?.label || "Speed"}
+				</Button>
+			</DropdownMenuTrigger>
+
+			<DropdownMenuContent>
+				<DropdownMenuGroup>
+					<DropdownMenuLabel>Video Speed</DropdownMenuLabel>
+					{#each videoSpeeds as { label, value }}
+						<DropdownMenuItem onclick={() => selectVideoSpeed(value)}>{label}</DropdownMenuItem>
+					{/each}
+				</DropdownMenuGroup>
+			</DropdownMenuContent>
+		</DropdownMenu>
+	</div>
+</div>
