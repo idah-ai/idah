79f05d44c334357f88a12410ed2eb7d07763deb3
whelsm <jiranop.phinyo@gmail.com>
2025-08-28 14:24:25 +0700

    feat: refactor DataTableEmpty component to utilize ResponseBlock for improved structure and reusability

--- app/frontend/src/lib/components/app/data-table/DataTableEmpty.svelte
+++ app/frontend/src/lib/components/app/data-table/DataTableEmpty.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import Button from "@/components/ui/button/button.svelte";
-	import Text from "@/components/ui/text/Text.svelte";
+	import ResponseBlock from "@/components/app/blocks/ResponseBlock.svelte";
 
 	import { InboxIcon, PlusIcon } from "@lucide/svelte";
 
@@ -11,18 +11,11 @@
 	let { dataTableName }: Props = $props();
 </script>
 
-<div class="flex flex-col items-center gap-4">
-	<Button variant="outline" size="icon">
-		<InboxIcon class="size-4" />
-	</Button>
-
-	<div class="flex flex-col items-center gap-1">
-		<Text weight="semibold" class="capitalize">No {dataTableName} added</Text>
-		<Text size="sm" class="text-muted-foreground">Please add a {dataTableName} to get started</Text>
-	</div>
-
-	<Button class="capitalize">
-		<PlusIcon class="size-4" />
-		Add {dataTableName}
-	</Button>
-</div>
+<ResponseBlock title="No {dataTableName}" description="Please add a {dataTableName} to get started" icon={InboxIcon}>
+	{#snippet actions()}
+		<Button class="capitalize">
+			<PlusIcon class="size-4" />
+			Add {dataTableName}
+		</Button>
+	{/snippet}
+</ResponseBlock>
