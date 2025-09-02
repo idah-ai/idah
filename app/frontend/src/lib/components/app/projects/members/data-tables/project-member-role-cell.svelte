5d34f48908a7b85e00768b4ccf509ba4fcbfcad9
whelsm <jiranop.phinyo@gmail.com>
2025-09-02 15:51:41 +0700

    feat: add project member role and action cells, form, and modal for managing project members

---
+++ app/frontend/src/lib/components/app/projects/members/data-tables/project-member-role-cell.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import SingleSelectField from "@/components/app/forms/fields/select/single-select-field.svelte";
+
+	import type { DataTableCellBaseProps } from "@/components/app/data-table/data-table.types";
+
+	// Props
+	interface Props extends DataTableCellBaseProps {}
+	let { record: projectMember }: Props = $props();
+
+	// Variables
+	const resource: string = "Resource::Dataset::Projects";
+</script>
+
+<SingleSelectField
+	name="{resource}/role"
+	class="flex-1"
+	placeholder="Select a role"
+	choices={[
+		{ label: "Annotator", value: "annotator" },
+		{ label: "Reviewer", value: "reviewer" },
+		{ label: "Project Manager", value: "project_manager" },
+		{ label: "Admin", value: "Admin" },
+	]}
+	required
+	clearable
+	searchable
+	searchPlaceholder="Search a role"
+	bind:value={projectMember.role}
+/>
---
+++ app/frontend/src/lib/components/app/projects/members/data-tables/project-member-row-action-cell.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import Button from "@/components/ui/button/button.svelte";
+	import ConfirmModal from "@/components/app/overlays/modals/confirm-modal.svelte";
+
+	import { Trash2Icon } from "@lucide/svelte";
+
+	// Props
+	interface Props {
+		record: Record<string, unknown>;
+	}
+	let { record: projectMember }: Props = $props();
+
+	// Variables
+	let openConfirmRemoveMemberModal: boolean = $state(false);
+</script>
+
+<Button variant="ghost" size="icon" onclick={() => (openConfirmRemoveMemberModal = true)}>
+	<Trash2Icon class="size-4" />
+</Button>
+
+<ConfirmModal
+	title="Remove member"
+	description="Are you sure you want to remove this member from the project?"
+	bind:open={openConfirmRemoveMemberModal}
+></ConfirmModal>
