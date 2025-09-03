946520389818499ec5d2fb62c4619b0ceea788fb
whelsm <jiranop.phinyo@gmail.com>
2025-09-03 13:06:51 +0700

    feat: refactor data table components to support generic records and enhance data fetching with new store functions

--- app/frontend/src/lib/components/app/data-table/data-table.types.ts
+++ app/frontend/src/lib/components/app/data-table/data-table.types.ts
@@ -1,72 +1,105 @@
 import type { Component, Snippet } from "svelte";
+
 import type { LabelValue } from "@/components/app/component.types";
+import type { BackendDataSource } from "@/data/BackendDataSource";
+import type { CollectionResponse } from "@/data/model/types";
+import type { Hash } from "@/utils/types";
+import type { ListOptions, Pagination, Sort } from "@/data/DataSource";
+import type { Record } from "@/data/model/Record";
+import type { Filters } from "@/data/filtering";
+import type { Writable } from "svelte/store";
 
 export type DataTableColumnDataType = "string" | "number" | "date" | "datetime" | "email" | "file" | "time" | "enum";
 
 export type DataTableColumnFilterBy =
-	| "string"
-	| "number-range"
-	| "single-select"
-	| "multiple-select"
-	| "date-range"
-	| "datasource";
+  | "string"
+  | "number-range"
+  | "single-select"
+  | "multiple-select"
+  | "date-range"
+  | "datasource";
 
 export type DataTableColumnFilterOperation =
-	| "eq"
-	| "neq"
-	| "gt"
-	| "gte"
-	| "lt"
-	| "lte"
-	| "in"
-	| "nin"
-	| "contains"
-	| "match";
+  | "eq"
+  | "neq"
+  | "gt"
+  | "gte"
+  | "lt"
+  | "lte"
+  | "in"
+  | "nin"
+  | "contains"
+  | "match";
 
-export interface ColumnSettings {
-	label: string;
-	dataType: DataTableColumnDataType;
-	clickable?: boolean;
-	sortable: boolean;
-	filterable: boolean;
-	filterOptions?: {
-		filterKey: string;
-		filterBy: DataTableColumnFilterBy;
-		filterOperation: DataTableColumnFilterOperation;
-		choices?: LabelValue<string | number | boolean>[];
-	};
-	filterComponent?: Component;
-	align?: "left" | "center" | "right";
-	visible: boolean;
-	hidable: boolean;
-	cellComponent?: Component<DataTableCellBaseProps, {}, "">;
-	cellComponentProps?: Record<string, any>;
-	cellOptions?: {
-		enums: LabelValue<string | number | boolean>[];
-	};
+export interface ColumnSettings<T extends Record> {
+  label: string;
+  dataType: DataTableColumnDataType;
+  clickable?: boolean;
+  sortable: boolean;
+  filterable: boolean;
+  filterOptions?: {
+    filterKey: string;
+    filterBy: DataTableColumnFilterBy;
+    filterOperation: DataTableColumnFilterOperation;
+    choices?: LabelValue<string | number | boolean>[];
+  };
+  filterComponent?: Component;
+  align?: "left" | "center" | "right";
+  visible: boolean;
+  hidable: boolean;
+  cellComponent?: Component<DataTableCellBaseProps<T>, {}, "">;
+  cellComponentProps?: Hash;
+  cellOptions?: {
+    enums: LabelValue<string | number | boolean>[];
+  };
 }
 
-export interface ColumnsSettings {
-	[fieldName: string]: ColumnSettings;
+export interface ColumnsSettings<T extends Record> {
+  [fieldName: string]: ColumnSettings<T>;
 }
 
-export interface DataTableBaseProps {
-	/**
-	 * Unique identifier for the data table
-	 * This is used to track the table's state and data from local or session storage.
-	 */
-	id: string;
-	name: string;
-	title?: string;
-	// dataSource
-	// listOptions
-	columns: ColumnsSettings;
-	hidePagination?: boolean;
+export interface DataTableBaseProps<T extends Record> {
+  /**
+   * Unique identifier for the data table
+   * This is used to track the table's state and data from local or session storage.
+   */
+  id: string;
+  name: string;
+  title?: string;
+  dataSource: BackendDataSource<T>;
+  listOptions?: ListOptions;
+  columns: ColumnsSettings<T>;
+  hidePagination?: boolean;
 
-	// Snippets
-	actions?: Snippet;
+  // Functions
+  onLoadSetContexts?: (response: CollectionResponse<T>) => Promise<Hash>;
+
+  // Snippets
+  actions?: Snippet;
 }
 
-export interface DataTableCellBaseProps {
-	record: Record<string, unknown>;
+export interface DataTableCellBaseProps<T extends Record> {
+  record: T;
+  contexts?: Hash;
+}
+
+export interface TablePreferences {
+  selectedRows: Array<string>;
+  filters: Filters;
+  pagination: Pagination;
+  sort: Sort;
+}
+
+export type TableStatus = "loading" | "loaded" | "error";
+
+export interface TableData<T extends Record> {
+  status: TableStatus;
+  error?: unknown;
+  response: CollectionResponse<T>;
+  contexts?: Hash;
+}
+
+export interface TableState<T extends Record> {
+  tableData: Writable<TableData<T>>;
+  tablePreferences: Writable<TablePreferences>;
 }
