ec45728e3aa25a292f65646401e02fc16f803cef
whelsm <jiranop.phinyo@gmail.com>
2025-09-03 13:06:57 +0700

    feat: add project data table cells and enhance project columns configuration

--- app/frontend/src/lib/components/app/projects/data-tables/project-completed-cell.svelte
+++ app/frontend/src/lib/components/app/projects/data-tables/project-completed-cell.svelte
@@ -1,14 +1,13 @@
 <script lang="ts">
-  import Link from "@/components/ui/text/Link.svelte";
-
   import type { DataTableCellBaseProps } from "@/components/app/data-table/data-table.types";
+
   import type { ProjectRecord } from "@/data/model/dataset/project-record";
 
   // Props
   interface Props extends DataTableCellBaseProps<ProjectRecord> {}
   let { record: project }: Props = $props();
+
+  // Variables
 </script>
 
-<Link href="/projects/{project.id}/tasks" showIcon>
-  {project.name}
-</Link>
+0%
