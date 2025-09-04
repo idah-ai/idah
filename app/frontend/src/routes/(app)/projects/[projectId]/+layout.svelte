ecb7b70bd8bce3022b8fc2e219811448dff097ee
whelsm <jiranop.phinyo@gmail.com>
2025-09-04 10:38:49 +0700

    feat: add loading state for project data fetching and enhance project detail layout

--- app/frontend/src/routes/(app)/projects/[projectId]/+layout.svelte
+++ app/frontend/src/routes/(app)/projects/[projectId]/+layout.svelte
@@ -5,11 +5,13 @@
 
   import PageProvider from "@/components/app/page/page-provider.svelte";
   import PageHeader from "@/components/app/page/page-header.svelte";
+  import PageLoading from "@/components/app/page/page-loading.svelte";
   import ProjectDropdownMenu from "@/components/app/projects/dropdowns/project-dropdown-menu.svelte";
   import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
   import { projectBreadcrumb } from "@/components/app/page/page-breadcrumb.constants";
   import { projectTabs, type ProjectTab } from "@/components/app/projects/tabs/project.tabs";
+  import { ProjectRecord, projectsBackendDataSource } from "@/data/model/dataset/projects/project-record";
 
   import type { PageBreadcrumbItem } from "@/components/app/page/page-breadcrumb.svelte";
 
@@ -22,7 +24,10 @@
   // Variables
   let projectId: string = page.params.projectId as string;
   let activeTab: ProjectTab = $derived(page.url.pathname.split("/").pop() as ProjectTab);
-  let breadcrumbs: PageBreadcrumbItem[] = $state([projectBreadcrumb, { label: projectId }]);
+  let breadcrumbs: PageBreadcrumbItem[] = $state([projectBreadcrumb]);
+
+  // Records
+  let project: ProjectRecord = $state(new ProjectRecord());
 
   // Lifecycle
   onMount(() => {
@@ -37,25 +42,39 @@
   });
 
   // Functions
+  async function fetchProject(): Promise<void> {
+    const projectRes = await projectsBackendDataSource.get(projectId, {
+      fields: {
+        "datasets/projects": ["name"],
+      },
+    });
+    project = projectRes.data;
+    breadcrumbs = [projectBreadcrumb, { label: project.name }];
+  }
+
   function handleTabChange(value: ProjectTab): void {
     goto(`/projects/${projectId}/${value}`);
   }
 </script>
 
-<PageProvider name="project-detail" {breadcrumbs}>
-  <PageHeader title="Video Tracking">
-    {#snippet actions()}
-      <ProjectDropdownMenu {projectId} />
-    {/snippet}
-  </PageHeader>
+{#await fetchProject()}
+  <PageLoading />
+{:then _}
+  <PageProvider name="project-detail" {breadcrumbs}>
+    <PageHeader title={project.name}>
+      {#snippet actions()}
+        <ProjectDropdownMenu {projectId} />
+      {/snippet}
+    </PageHeader>
 
-  <Tabs bind:value={activeTab}>
-    <TabsList>
-      {#each projectTabs as { label, value } (value)}
-        <TabsTrigger {value} onclick={() => handleTabChange(value)}>{label}</TabsTrigger>
-      {/each}
-    </TabsList>
-  </Tabs>
+    <Tabs bind:value={activeTab}>
+      <TabsList>
+        {#each projectTabs as { label, value } (value)}
+          <TabsTrigger {value} onclick={() => handleTabChange(value)}>{label}</TabsTrigger>
+        {/each}
+      </TabsList>
+    </Tabs>
 
-  {@render children()}
-</PageProvider>
+    {@render children()}
+  </PageProvider>
+{/await}
