9f343ebec46530e84bb94c4ff2bc5c81ac77c148
whelsm <jiranop.phinyo@gmail.com>
2025-09-04 10:26:49 +0700

    feat: refactor input and textarea fields for improved readability and maintainability; add FormBaseProps interface

--- app/frontend/src/lib/components/app/forms/fields/input/input-field.svelte
+++ app/frontend/src/lib/components/app/forms/fields/input/input-field.svelte
@@ -1,53 +1,53 @@
 <script lang="ts">
-	import FormField from "@/components/app/forms/form-field.svelte";
-	import FormFieldErrors from "@/components/app/forms/form-field-errors.svelte";
-	import FormFieldInfo from "@/components/app/forms/form-field-info.svelte";
-	import FormFieldLabel from "@/components/app/forms/form-field-label.svelte";
-	import Input from "@/components/ui/input/input.svelte";
+  import FormField from "@/components/app/forms/form-field.svelte";
+  import FormFieldErrors from "@/components/app/forms/form-field-errors.svelte";
+  import FormFieldInfo from "@/components/app/forms/form-field-info.svelte";
+  import FormFieldLabel from "@/components/app/forms/form-field-label.svelte";
+  import Input from "@/components/ui/input/input.svelte";
 
-	import { cn } from "@/utils";
+  import { cn } from "@/utils";
 
-	import type { FormFieldBaseProps } from "@/components/app/forms/form-field.types";
+  import type { FormFieldBaseProps } from "@/components/app/forms/form-field.types";
 
-	// Props
-	interface Props extends FormFieldBaseProps {
-		value: string | null;
-	}
-	let {
-		value = $bindable(null),
-		name,
-		label,
-		placeholder,
-		disabled = false,
-		required = false,
-		readonly,
-		info,
-		errors,
-		class: className,
-		slotLabel,
-		slotInfo,
-		slotErrors,
-	}: Props = $props();
+  // Props
+  interface Props extends FormFieldBaseProps {
+    value: string | null;
+  }
+  let {
+    value = $bindable(null),
+    name,
+    label,
+    placeholder,
+    disabled = false,
+    required = false,
+    readonly,
+    info,
+    errors,
+    class: className,
+    slotLabel,
+    slotInfo,
+    slotErrors,
+  }: Props = $props();
 </script>
 
 <FormField id={name} class={cn("", className)}>
-	{#if slotLabel}
-		{@render slotLabel()}
-	{:else}
-		<FormFieldLabel {required}>{label}</FormFieldLabel>
-	{/if}
+  {#if slotLabel}
+    {@render slotLabel()}
+  {:else}
+    <FormFieldLabel {required}>{label}</FormFieldLabel>
+  {/if}
 
-	<Input {name} {placeholder} {disabled} {readonly} {required} bind:value />
+  <Input {name} {placeholder} {disabled} {readonly} {required} bind:value />
 
-	{#if slotInfo}
-		{@render slotInfo()}
-	{:else}
-		<FormFieldInfo>{info}</FormFieldInfo>
-	{/if}
+  {#if slotInfo}
+    {@render slotInfo()}
+  {:else}
+    <FormFieldInfo>{info}</FormFieldInfo>
+  {/if}
 
-	{#if slotErrors}
-		{@render slotErrors()}
-	{:else}
-		<FormFieldErrors {errors}></FormFieldErrors>
-	{/if}
+  {#if slotErrors}
+    {@render slotErrors()}
+  {:else}
+    <FormFieldErrors {errors}></FormFieldErrors>
+  {/if}
 </FormField>
