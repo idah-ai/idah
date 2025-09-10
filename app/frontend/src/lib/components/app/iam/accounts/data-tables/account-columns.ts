585a92752003f910e70dbe2348aadccf9f87b499
whelsm <jiranop.phinyo@gmail.com>
2025-09-10 14:01:12 +0700

    feat: implement account management features with data table, forms, and modals

---
+++ app/frontend/src/lib/components/app/iam/accounts/data-tables/account-columns.ts
@@ -0,0 +1,92 @@
+import AccountJoinedAtCell from "@/components/app/iam/accounts/data-tables/account-joined-at-cell.svelte";
+import AccountStatusCell from "@/components/app/iam/accounts/data-tables/account-status-cell.svelte";
+import AccountRowActionCell from "@/components/app/iam/accounts/data-tables/account-row-action-cell.svelte";
+
+import { AccountRecord } from "@/data/model/iam/accounts/record";
+
+import type { ColumnsSettings } from "@/components/app/data-table/data-table.types";
+
+export const accountColumns: ColumnsSettings<AccountRecord> = {
+  name: {
+    label: "Name",
+    dataType: "string",
+    clickable: false,
+    sortable: true,
+    filterable: true,
+    filterOptions: {
+      filterKey: "name",
+      filterBy: "string",
+      filterOperation: "match",
+    },
+    visible: true,
+    hidable: false,
+  },
+  email: {
+    label: "Email",
+    dataType: "email",
+    clickable: true,
+    sortable: true,
+    filterable: true,
+    filterOptions: {
+      filterKey: "email",
+      filterBy: "string",
+      filterOperation: "match",
+    },
+    visible: true,
+    hidable: false,
+  },
+  enabled: {
+    label: "Status",
+    dataType: "boolean",
+    clickable: false,
+    sortable: true,
+    filterable: true,
+    filterOptions: {
+      filterKey: "enabled",
+      filterBy: "single-select",
+      filterOperation: "eq",
+    },
+    visible: true,
+    hidable: false,
+    cellComponent: AccountStatusCell,
+  },
+  joined_at: {
+    label: "Joined At",
+    dataType: "datetime",
+    clickable: false,
+    sortable: true,
+    filterable: true,
+    filterOptions: {
+      filterKey: "joined_at",
+      filterBy: "date-range",
+      filterOperation: "gte",
+    },
+    visible: true,
+    hidable: false,
+    cellComponent: AccountJoinedAtCell,
+  },
+  created_at: {
+    label: "Created At",
+    dataType: "datetime",
+    clickable: false,
+    sortable: true,
+    filterable: true,
+    filterOptions: {
+      filterKey: "created_at",
+      filterBy: "date-range",
+      filterOperation: "gte",
+    },
+    visible: true,
+    hidable: false,
+  },
+  action: {
+    label: "Action",
+    dataType: "string",
+    clickable: false,
+    sortable: false,
+    filterable: false,
+    visible: true,
+    hidable: false,
+    cellComponent: AccountRowActionCell,
+  },
+};
