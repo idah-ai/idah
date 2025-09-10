84deeefc5476c8e8a69ea298fa5f5dbf599c77ae
whelsm <jiranop.phinyo@gmail.com>
2025-09-10 13:26:51 +0700

    feat: implement account management features including CRUD operations and associated tests

---
+++ app/iam/app/model/account.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module Account
+  class Record < Verse::Model::Record::Base
+    type Resource::Iam::Accounts
+
+    field :id, type: Integer, primary: true
+
+    field :name, type: String
+    field :email, type: String
+
+    field :hashed_password, type: [String, NilClass], visible: false, readonly: true
+    field :sso_channel, type: [String, NilClass]
+
+    field :enabled, type: TrueClass
+
+    field :joined_at, type: [Time, NilClass]
+
+    field :created_at, type: Time, readonly: true
+    field :updated_at, type: Time, readonly: true
+  end
+
+  class Repository < Verse::Sequel::Repository
+    self.table = "accounts"
+    self.resource = Resource::Iam::Accounts
+  end
+end
