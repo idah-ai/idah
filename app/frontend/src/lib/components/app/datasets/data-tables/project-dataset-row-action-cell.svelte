b048acd699c4966109c2dde7d4134f92ab594b56
tojo <yraimtj@gmail.com>
2025-09-11 17:12:20 +0300

    move tasks tab inside dataset

---
+++ app/frontend/src/lib/components/app/datasets/data-tables/project-dataset-row-action-cell.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import ProjectDatasetDropdownMenu from "@/components/app/datasets/dropdowns/project-dataset-dropdown-menu.svelte";
+  import { DatasetRecord } from "@/data/model/dataset/dataset-record";
+
+  // Props
+  interface Props {
+    record: DatasetRecord;
+  }
+  let { record: dataset }: Props = $props();
+  console.log(dataset);
+</script>
+
+<ProjectDatasetDropdownMenu datasetId={dataset.id} projectId={dataset._jsonapiData?.relationships?.project.data.id} />
