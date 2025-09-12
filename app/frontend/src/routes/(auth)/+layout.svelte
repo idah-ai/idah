fdc0c728818014d88e2c808062ad7ce25c51add2
whelsm <jiranop.phinyo@gmail.com>
2025-09-12 13:10:37 +0700

    feat: implement authentication flow with login, sign-up, reset password, and alert components

---
+++ app/frontend/src/routes/(auth)/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import type { HTMLAttributes } from "svelte/elements";
+  import type { WithElementRef } from "@/utils";
+
+  // Props
+  let { children }: WithElementRef<HTMLAttributes<HTMLDivElement>> = $props();
+</script>
+
+<div class="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+  <div class="flex w-full max-w-sm flex-col gap-6">
+    {@render children?.()}
+  </div>
+</div>
