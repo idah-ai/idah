b9cfdfc74f86e334fe38a8ddbc3729b287789c94
whelsm <jiranop.phinyo@gmail.com>
2025-09-12 13:10:37 +0700

    feat: implement authentication flow with login, sign-up, reset password, and alert components

---
+++ app/frontend/src/lib/components/app/iam/auth/card/authentication-card.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+  import Logo from "@/components/app/brand/logo.svelte";
+
+  import type { Snippet } from "svelte";
+
+  // Props
+  interface Props {
+    title: string;
+    description: string;
+    alert?: Snippet;
+    content: Snippet;
+    footer?: Snippet;
+  }
+  let { title, description, alert, content, footer }: Props = $props();
+</script>
+
+<Card>
+  <CardHeader class="flex w-full flex-col items-center">
+    <Logo class="mb-4"></Logo>
+
+    <CardTitle>{title}</CardTitle>
+    <CardDescription>{description}</CardDescription>
+  </CardHeader>
+
+  <CardContent class="py-4">
+    {@render alert?.()}
+
+    {@render content()}
+  </CardContent>
+
+  <CardFooter class="flex w-full flex-col items-center">
+    {@render footer?.()}
+  </CardFooter>
+</Card>
---
+++ app/frontend/src/routes/(auth)/reset-password/+page.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+  import { goto } from "$app/navigation";
+
+  import AuthenticationCard from "@/components/app/iam/auth/card/authentication-card.svelte";
+  import Button from "@/components/ui/button/button.svelte";
+  import Form from "@/components/app/forms/form.svelte";
+  import InputField from "@/components/app/forms/fields/input/input-field.svelte";
+
+  import { resetPasswordSchema } from "@/data/model/iam/accounts/auth-schema";
+
+  // Variables
+  let resource: string = "iam:account";
+  let updated: boolean = $state(false);
+  let credentials = $state({
+    password: "",
+    confirmPassword: "",
+  });
+  let disabledResetPasswordButton = $derived.by(() => {
+    const validated = resetPasswordSchema.safeParse(credentials);
+    return !validated.success;
+  });
+
+  // Functions
+  async function updatePassword(): Promise<void> {
+    if (true) {
+      updated = true;
+    } else {
+      updated = false;
+    }
+  }
+</script>
+
+<AuthenticationCard
+  title={updated ? "Password changed successfully!" : "Reset your password"}
+  description={updated
+    ? "Your password has been updated. You can now login with your new password."
+    : "Enter a new password to reset your password."}
+>
+  {#snippet content()}
+    <Form>
+      {#if !updated}
+        <!-- PASSWORD -->
+        <InputField
+          name="{resource}/password"
+          label="Password"
+          type="password"
+          placeholder="Enter your password"
+          required
+          bind:value={credentials.password}
+        ></InputField>
+
+        <!-- CONFIRM PASSWORD -->
+        <InputField
+          name="{resource}/confirm_password"
+          label="Confirm new password"
+          type="password"
+          placeholder="Enter your password"
+          required
+          bind:value={credentials.confirmPassword}
+        ></InputField>
+
+        <Button class="w-full" disabled={disabledResetPasswordButton} onclick={updatePassword}>Update Password</Button>
+      {:else}
+        <Button class="w-full" onclick={() => goto("/login")}>Go to login</Button>
+      {/if}
+    </Form>
+  {/snippet}
+</AuthenticationCard>
