34e5b44f38b0e0fbe571cc0cfed570fb32547872
whelsm <jiranop.phinyo@gmail.com>
2025-09-15 11:43:42 +0700

    Refactor dataset columns and improve type safety; clean up unused imports and enhance page header functionality

--- app/frontend/src/lib/components/app/page/page-header.svelte
+++ app/frontend/src/lib/components/app/page/page-header.svelte
@@ -1,24 +1,27 @@
 <script lang="ts">
-	import Text from "$lib/components/ui/text/Text.svelte";
+  import Text from "$lib/components/ui/text/Text.svelte";
 
-	import type { WithElementRef } from "@/utils";
-	import type { HTMLAttributes } from "svelte/elements";
-	import type { Snippet } from "svelte";
+  import type { WithElementRef } from "@/utils";
+  import type { HTMLAttributes } from "svelte/elements";
+  import type { Snippet } from "svelte";
 
-	// Props
-	type Props = WithElementRef<HTMLAttributes<HTMLDivElement>> & {
-		title?: string;
-		actions?: Snippet;
-	};
-	let { ref, title, actions }: Props = $props();
+  // Props
+  type Props = WithElementRef<HTMLAttributes<HTMLDivElement>> & {
+    title?: string;
+    slotTitle?: Snippet;
+    actions?: Snippet;
+  };
+  let { ref, title, slotTitle, actions }: Props = $props();
 </script>
 
 <div bind:this={ref} class="flex items-center justify-between gap-4">
-	{#if title}
-		<Text size="h2" weight="semibold">{title}</Text>
-	{/if}
+  {#if slotTitle}
+    {@render slotTitle()}
+  {:else if title}
+    <Text size="h2" weight="semibold">{title}</Text>
+  {/if}
 
-	{#if actions}
-		{@render actions()}
-	{/if}
+  {#if actions}
+    {@render actions()}
+  {/if}
 </div>
--- app/frontend/src/routes/(app)/projects/[projectId]/datasets/+page.svelte
+++ app/frontend/src/routes/(app)/projects/[projectId]/datasets/+page.svelte
@@ -1,27 +1,22 @@
 <script lang="ts">
-  import Button from "@/components/ui/button/button.svelte";
-  import DataTable from "@/components/app/data-table/data-table.svelte";
-  import PageProvider from "@/components/app/page/page-provider.svelte";
-  import PageHeader from "@/components/app/page/page-header.svelte";
-  import DatasetFormModal from "@/components/app/datasets/overlays/dataset-form-modal.svelte";
-  import { PlusIcon } from "@lucide/svelte";
   import { page } from "$app/state";
 
-  import { projectBreadcrumb } from "@/components/app/page/page-breadcrumb.constants";
+  import Button from "@/components/ui/button/button.svelte";
+  import DataTable from "@/components/app/data-table/data-table.svelte";
+  import DatasetFormModal from "@/components/app/datasets/overlays/dataset-form-modal.svelte";
+  import PageHeader from "@/components/app/page/page-header.svelte";
+
   import { projectDatasetColumns } from "@/components/app/datasets/data-tables/project-dataset.columns";
   import { datasetsBackendDataSource } from "@/data/model/dataset/dataset-record";
   import { refetches } from "@/utils/refetch";
-
-  import type { PageBreadcrumbItem } from "@/components/app/page/page-breadcrumb.svelte";
+  import { PlusIcon } from "@lucide/svelte";
 
   // Variables
   let projectId: string = page.params.projectId as string;
   let openNewDatasetModal: boolean = $state(false);
-  // let breadcrumbs: PageBreadcrumbItem[] = $state([projectBreadcrumb]);
 </script>
 
-
-<PageHeader title="datasets">
+<PageHeader title="Datasets">
   {#snippet actions()}
     <Button onclick={() => (openNewDatasetModal = true)}>
       <PlusIcon class="size-4" />
@@ -38,7 +33,7 @@
     dataSource={datasetsBackendDataSource}
     listOptions={{
       fields: {
-        "dataset:datasets": ["name", "status", "modality","progress", "created_at", "updated_at"],
+        "dataset:datasets": ["name", "status", "modality", "progress", "created_at", "updated_at"],
         "dataset:projects": ["name"],
       },
       filters: {
@@ -48,6 +43,5 @@
     }}
   />
 {/key}
-
 
 <DatasetFormModal title="Dataset" action="create" bind:open={openNewDatasetModal} />
