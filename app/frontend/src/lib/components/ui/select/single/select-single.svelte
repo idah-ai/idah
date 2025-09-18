961cb2382b94637b09323f1d42a2b55faf5e0aa2
Karnravee Chalermmuang <ptmwilfk@gmail.com>
2025-09-18 10:52:39 +0700

    feat: add multiple and single select components with type definitions

---
+++ app/frontend/src/lib/components/ui/select/single/select-single.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import type { SingleSelectBaseProps } from  "@/components/ui/select/select.types"
+
+
+let {
+    label,
+    placeholder,
+    required = false,
+    value = $bindable(null),
+    error,
+    choices = [],
+    searchPlaceholder = "Search...",
+    clearable = false,
+  } : SingleSelectBaseProps = $props();
+</script>
