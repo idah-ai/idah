e3533e0d820c7eddc208d50f238937ffe6c82b83
whelsm <jiranop.phinyo@gmail.com>
2025-09-18 16:37:18 +0700

    refactor: remove role selection component and replace with badge display for project member roles

--- app/frontend/src/lib/components/app/projects/members/data-tables/project-member-role-cell.svelte
+++ app/frontend/src/lib/components/app/projects/members/data-tables/project-member-role-cell.svelte
@@ -1,47 +1,16 @@
 <script lang="ts">
-  import SingleSelectField from "@/components/app/forms/fields/select/single-select-field.svelte";
+  import { Badge } from "@/components/ui/badge";
 
   import { humanize } from "@/utils/string";
-  import { toast } from "svelte-sonner";
-  import {
-    ProjectMemberRecord,
-    projectMemberRoles,
-    projectMembersBackendDataSource,
-  } from "@/data/model/dataset/projects/members/record";
+  import { ProjectMemberRecord, projectMemberRoles } from "@/data/model/dataset/projects/members/record";
 
   import type { DataTableCellBaseProps } from "@/components/app/data-table/data-table.types";
 
   // Props
   interface Props extends DataTableCellBaseProps<ProjectMemberRecord> {}
   let { record: projectMember }: Props = $props();
-
-  // Variables
-  const resource: string = "Resource::Dataset::Projects";
-
-  let projectMemberRole = $derived(projectMember.role);
-
-  // Functions
-  async function updateProjectMemberRole(newRole: string): Promise<void> {
-    try {
-      await projectMembersBackendDataSource.update(projectMember.id, {
-        attributes: {
-          role: newRole,
-        },
-      });
-      toast.success(`${humanize(newRole)} role assigned to ${projectMember.email}`);
-    } catch (error) {
-      toast.error("Failed to update project member role");
-    }
-  }
 </script>
 
-<SingleSelectField
-  name="{resource}/role"
-  class="flex-1"
-  placeholder="Select a role"
-  choices={projectMemberRoles}
-  searchable
-  searchPlaceholder="Search a role"
-  onValueChange={updateProjectMemberRole}
-  bind:value={projectMemberRole}
-/>
+<Badge variant="outline" class="rounded-lg">
+  {projectMemberRoles.find((role) => role.value === projectMember.role)?.label || humanize(projectMember.role)}
+</Badge>
