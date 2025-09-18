717b11924533269c8dfe624f26571a575c8cfc86
whelsm <jiranop.phinyo@gmail.com>
2025-09-18 16:16:54 +0700

    refactor: update fields in ProjectMember and Account models to be readonly where appropriate

--- app/iam/app/model/account.rb
+++ app/iam/app/model/account.rb
@@ -7,14 +7,14 @@ module Account
     field :id, type: Integer, primary: true
 
     field :name, type: String
-    field :email, type: String
+    field :email, type: String, readonly: true
 
     field :hashed_password, type: [String, NilClass], visible: false, readonly: true
-    field :sso_channel, type: [String, NilClass]
+    field :sso_channel, type: [String, NilClass], readonly: true
 
-    field :enabled, type: [TrueClass, FalseClass]
+    field :enabled, type: [TrueClass]
 
-    field :joined_at, type: [Time, NilClass]
+    field :joined_at, type: [Time, NilClass], readonly: true
 
     field :created_at, type: Time, readonly: true
     field :updated_at, type: Time, readonly: true
