262b75d64d1a16c5154974ac1fae73b3f19c2f60
Karnravee Chalermmuang <ptmwilfk@gmail.com>
2025-09-19 09:37:21 +0700

    feat: enhance multiple select component with data fetching and selection functionality

--- app/frontend/src/lib/components/app/forms/fields/select/multiple/multiple-select-datasource-field.svelte
+++ app/frontend/src/lib/components/app/forms/fields/select/multiple/multiple-select-datasource-field.svelte
@@ -1,13 +1,20 @@
 <script lang="ts" generics="T extends Record">
+  import Button from "@/components/ui/button/button.svelte";
+  import { Command, CommandEmpty, CommandGroup, CommandItem, CommandList } from "@/components/ui/command";
   import FormField from "@/components/app/forms/form-field.svelte";
   import FormFieldErrors from "@/components/app/forms/form-field-errors.svelte";
   import FormFieldInfo from "@/components/app/forms/form-field-info.svelte";
   import FormFieldLabel from "@/components/app/forms/form-field-label.svelte";
+  import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+  import Spinner from "@/components/app/loading/spinner.svelte";
 
   import { cn } from "@/utils";
+  import { CheckIcon, ChevronsUpDownIcon, CircleXIcon } from "@lucide/svelte";
 
   import type { SelectDataSourceFieldBaseProps } from "@/components/app/forms/form-field.types";
+  import type { LabelValue } from "@/utils/types";
   import type { Record } from "@/data/model/Record";
+  import type { ListOptions } from "@/data/DataSource";
 
   // Props
   interface Props extends SelectDataSourceFieldBaseProps<T> {
@@ -20,8 +27,6 @@
     name,
     label,
     placeholder = "Select an option",
-    searchable = false,
-    searchPlaceholder = "Search an option",
     clearable = false,
     disabled = false,
     required = false,
@@ -37,7 +42,59 @@
     slotInfo,
     slotErrors,
   }: Props = $props();
-                                         
+
+  // Type
+  type Choice = LabelValue<string | number, T>;
+
+  // Variables
+  let open: boolean = $state(false);
+  let choices: Choice[] = $state([]);
+  // let selectedValue
+  let selectedChoice = $derived(choices.find((choice) => values.includes(choice.value)));
+
+  // Functions
+  function openPopover(): void {
+    if (!disabled) return;
+    open = true;
+  }
+
+  async function initialFetchChoices(): Promise<void> {
+    choices = await fetchChoices();
+  }
+
+  async function fetchChoices(): Promise<Choice[]> {
+    if (!dataSource) throw new Error("DataSource is required");
+
+    const listOpts: ListOptions = { ...listOptions };
+
+    /** Set default pagination */
+    if (!listOpts.pagination) listOpts.pagination = { page: 1, itemsPerPage: 10 };
+
+    /** Set default sort */
+    if (!listOpts.sort) listOpts.sort = ["-id"];
+
+    const response = await dataSource.list(listOpts);
+
+    return response.data.map((item) => ({
+      label: item.name,
+      value: item.id,
+      data: item,
+    }));
+  }
+
+  async function select(choice: Choice): Promise<void> {
+
+
+
+      if (values.find((v) => v == choice.value)) {
+      values = values.filter((value) => value != choice.value);
+    } else {
+      values = [...values, choice.value];
+    }
+    open = false;
+    await onValueChange?.(choice.value);
+
+  }
 </script>
 
 <FormField id={name} class={cn("", className)}>
@@ -46,6 +103,69 @@
   {:else}
     <FormFieldLabel {required}>{label}</FormFieldLabel>
   {/if}
+
+  <Popover bind:open>
+    <PopoverTrigger>
+      {#if slotTrigger}
+        {@render slotTrigger({ selectedChoice, clearable, disabled })}
+      {:else}
+        <Button
+          variant="outline"
+          class="w-full justify-between"
+          {disabled}
+          role="combobox"
+          aria-expanded={open}
+          onclick={openPopover}
+        >
+          {#if selectedChoice}
+            {selectedChoice.label}
+          {:else}
+            <span class="text-muted-foreground">{placeholder}</span>
+          {/if}
+
+          <div class="ml-auto inline-flex items-center gap-2">
+            <button
+              type="button"
+              class={cn("cursor-pointer", clearable && selectedChoice ? "opacity-50" : "opacity-0")}
+              onclick={() => {}}
+            >
+              <CircleXIcon class="size-4 shrink-0" />
+            </button>
+
+            <ChevronsUpDownIcon class="size-4 shrink-0 opacity-50"></ChevronsUpDownIcon>
+          </div>
+        </Button>
+      {/if}
+    </PopoverTrigger>
+
+    <PopoverContent align="start" class="p-0">
+      <Command>
+        <CommandList>
+          <CommandEmpty>No option found.</CommandEmpty>
+          <CommandGroup>
+            {#await initialFetchChoices()}
+              <Spinner></Spinner>
+            {:then _}
+              {#each choices as choice, index (index)}
+                {#if slotChoice}
+                  {@render slotChoice({ choice })}
+                {:else}
+                  <CommandItem onclick={() => select(choice)}>
+                     <CheckIcon
+                  class={cn("mr-2 size-4", {
+                    "opacity-0": !values.includes(choice.value),
+                  })}
+                />
+                    {choice.label}
+                  </CommandItem>
+                {/if}
+              {/each}
+            {/await}
+          </CommandGroup>
+        </CommandList>
+      </Command>
+    </PopoverContent>
+  </Popover>
 
   {#if slotInfo}
     {@render slotInfo()}
