b259271d016131b29486c532806da138705f8011
whelsm <jiranop.phinyo@gmail.com>
2025-09-19 12:04:42 +0700

    Add displayKey prop to SelectDataSourceFieldBaseProps and implement onMount lifecycle for choice selection

--- app/frontend/src/lib/components/app/forms/form-field.types.ts
+++ app/frontend/src/lib/components/app/forms/form-field.types.ts
@@ -42,6 +42,7 @@ export interface SelectFieldBaseProps extends FormFieldBaseProps {
 }
 
 export interface SelectDataSourceFieldBaseProps<T extends Record> extends Omit<SelectFieldBaseProps, "choices"> {
+  displayKey: keyof T;
   dataSource: DataSource<T>;
   listOptions?: ListOptions;
 }
