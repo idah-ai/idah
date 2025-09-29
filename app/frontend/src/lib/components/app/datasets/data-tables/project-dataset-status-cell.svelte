376c5b7280e3465d76eb65ab1ab73a866a2fba18
whelsm <jiranop.phinyo@gmail.com>
2025-09-29 11:25:41 +0700

    feat(dataset): add dataset status badge component and integrate into data table

---
+++ app/frontend/src/lib/components/app/datasets/data-tables/project-dataset-status-cell.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import DatasetStatus from "@/components/app/datasets/badges/dataset-status.svelte";
+
+  import { DatasetRecord } from "@/data/model/dataset/dataset-record";
+
+  import type { DataTableCellBaseProps } from "@/components/app/data-table/data-table.types";
+
+  // Props
+  interface Props extends DataTableCellBaseProps<DatasetRecord> {}
+  let { record: dataset }: Props = $props();
+
+  // Variables
+</script>
+
+<DatasetStatus {dataset}></DatasetStatus>
