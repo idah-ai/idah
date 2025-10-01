954375876c753516fba3dc2db693fb19e8836ee6
whelsm <jiranop.phinyo@gmail.com>
2025-10-01 09:43:20 +0700

    Add allowed_filters to datasets

--- app/dataset/app/expo/datasets_expo.rb
+++ app/dataset/app/expo/datasets_expo.rb
@@ -9,7 +9,18 @@ class DatasetsExpo < BaseExpo
     allowed_included "entries", "project"
 
     show
-    index
+    index do
+      allowed_filters :name__match,
+                      :status__in,
+                      :progress__gte,
+                      :progress__lte,
+                      :modality__in,
+                      :created_at__gte,
+                      :created_at__lte,
+                      :updated_at__gte,
+                      :updated_at__lte
+    end
+
     create do
       authorized_relationships project: [:link]
     end
