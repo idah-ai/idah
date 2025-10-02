86aa2055936cf02b35406b608047fc5bfbb7c41f
Sorakrit Kungvanruttana <sorakrit-audi@Sorakrits-MacBook-Air.local>
2025-10-02 14:14:12 +0700

    polishing project member specs

--- app/dataset/app/model/project_member_spec.rb
+++ app/dataset/app/model/project_member_spec.rb
@@ -19,9 +19,9 @@ RSpec.describe ProjectMember, database: true do
       system_repo.find!(
         system_repo.create(
           project_id: test_project1.id,
-          account_id: Verse::Spec.users[:annotator][:user_data][:id],
-          name: "annotator user",
-          email: "annotator@email.com"
+          account_id: 1,
+          name: "first user",
+          email: "first@email.com"
         )
       )
     }
@@ -38,9 +38,9 @@ RSpec.describe ProjectMember, database: true do
       system_repo.find!(
         system_repo.create(
           project_id: test_project2.id,
-          account_id: Verse::Spec.users[:annotator][:user_data][:id],
-          name: "annotator user",
-          email: "annotator@email.com"
+          account_id: 1,
+          name: "first user",
+          email: "first@email.com"
         )
       )
     }
@@ -49,16 +49,20 @@ RSpec.describe ProjectMember, database: true do
       system_repo.find!(
         system_repo.create(
           project_id: test_project2.id,
-          account_id: Verse::Spec.users[:reviewer][:user_data][:id],
-          name: "reviewer user",
-          email: "reviewer@email.com"
+          account_id: 2,
+          name: "second user",
+          email: "second@email.com"
         )
       )
     }
 
     describe "scope definitions" do
       context "scope: all", as: :system do
-        subject { described_class.new(current_auth_context) }
+        # subject { described_class.new(current_auth_context) }
+        before do
+          @test_rights = ["dataset:project_members.*.*"]
+        end
+        subject { described_class.new(Verse::Auth::Context.new(@test_rights)) }
         it "returns all project members" do
           project_members = subject.index({})
 
@@ -67,19 +71,38 @@ RSpec.describe ProjectMember, database: true do
       end
 
       # scope: memberships that are in the same project as the user
-      context "scope: same_project", as: :reviewer do
-        subject { described_class.new(current_auth_context) }
+      context "scope: same_project" do
+        before do
+          @test_rights = ["dataset:project_members.read.same_project"]
+          @test_account_id = 2
+        end
+        subject { described_class.new(Verse::Auth::Context.new(@test_rights, metadata: { id: @test_account_id })) }
+
         it "returns project members that are in the same project as the user" do
           project_members = subject.index({})
 
           expect(project_members.size).to eq(2)
+
+          # both owned and member in same project
+          expect(project_members.first.project_id).to eq(project_members.last.project_id)
         end
       end
 
       # scope: user's membership
-      context "scope: own", as: :annotator do
-        subject { described_class.new(current_auth_context) }
-        xit "returns all memberships" do
+      context "scope: own" do
+        before do
+          @test_rights = ["dataset:project_members.read.own"]
+          @test_account_id = 2
+        end
+        subject { described_class.new(Verse::Auth::Context.new(@test_rights, metadata: { id: @test_account_id })) }
+
+        it "returns owned memberships" do
+          project_members = subject.index({})
+
+          expect(project_members.size).to eq(1)
+
+          # user's own account_id
+          expect(project_members.first.account_id).to eq(@test_account_id)
         end
       end
     end
