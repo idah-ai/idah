9ba8bf7c05fe73c3de15bb5c26fb280bebf9055f
Yacine Petitprez <anykeyh@gmail.com>
2025-10-02 14:02:31 +0700

    add account settings

---
+++ app/setting/app/model/account_setting.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+module AccountSetting
+  class Record < Verse::Model::Record::Base
+    field :id, type: Integer, primary: true
+
+    field :account_id, type: Integer
+    field :key, type: String, readonly: true
+    field :plugin, type: String, readonly: true
+    field :value, type: [
+      Hash, Array, String,
+      Integer, Float, TrueClass, NilClass
+    ]
+  end
+
+  class Repository < Verse::Sequel::Repository
+    self.table = "account_settings"
+
+    encoder :value, Verse::Sequel::JsonEncoder
+
+    query
+    def get(key, account_id:, plugin: "", default: nil)
+      find_by({
+        key:,
+        account_id:,
+        plugin:
+      })&.value || default
+    end
+
+    event(name: "updated")
+    def set(key, value, account_id:, plugin: "")
+      table = scoped(:update)
+
+      value = Sequel.lit("?::jsonb", value.to_json)
+
+      table.insert_conflict(
+        target: %i[key account_id plugin],
+        update: { value: }
+      ).insert({
+        key:,
+        value:,
+        account_id:,
+        plugin:
+      })
+
+      true
+    end
+  end
+end
