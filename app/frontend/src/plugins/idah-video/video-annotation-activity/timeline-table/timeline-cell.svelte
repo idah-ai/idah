1e8e68f21c6d9fb99514fe221a734e2ee3a0e7d1
whelsm <jiranop.phinyo@gmail.com>
2025-10-02 16:24:46 +0700

    Enhance video annotation functionality with label configuration and context menus

--- app/frontend/src/plugins/idah-video/video-annotation-activity/timeline-table/timeline-cell.svelte
+++ app/frontend/src/plugins/idah-video/video-annotation-activity/timeline-table/timeline-cell.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
-  import { Button } from "@/components/ui/button";
-  import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+  import { ContextMenu, ContextMenuContent, ContextMenuItem, ContextMenuTrigger } from "@/components/ui/context-menu";
+
   import { cn } from "@/utils";
+  import { ArrowLeftRightIcon, Trash2Icon } from "@lucide/svelte";
+  import { getContext } from "svelte";
 
   let {
     frame,
@@ -26,6 +28,9 @@
     onDeleteFrame: (frame: number) => void;
   } = $props();
 
+  // Contexts
+  const labelConfig = getContext("labelConfig");
+
   // Variables
   let cellWidth: number = $derived(
     (1 / ((range[1] - range[0] + (scale - ((range[1] - range[0]) % scale))) / 100)) * scale,
@@ -36,35 +41,36 @@
 
 <div
   class={cn("inline-block h-full", {
-    "bg-primary/30": isSelected,
-    "bg-primary-foreground": isHovered,
+    "bg-primary/10": isSelected,
+    "bg-primary/20": isHovered,
   })}
   style:box-sizing="border-box"
   style:width="{cellWidth}%"
   onclick={() => onSeekFrame(frame)}
   {...restProps}
 >
-  <div class="py-1" style:height="100%">
-    {#if inSpan}
-      <div class="relative" style:background-color="#CFFAFE" style:height="100%">
-        {#if keyframes.length}
-          <Popover>
-            <PopoverTrigger class="absolute top-0 h-full w-full pt-0">
-              <div style="margin:auto" style:background-color="#06B6D4" style:height="25%" style:width="50%"></div>
-            </PopoverTrigger>
-            <PopoverContent>
-              <ul>
-                {#each keyframes as keyframe, index (index)}
-                  <li>
-                    <Button onclick={() => onSeekFrame?.(keyframe)}>Seek to {keyframe}</Button>
-                    <Button onclick={() => onDeleteFrame?.(keyframe)}>X</Button>
-                  </li>
-                {/each}
-              </ul>
-            </PopoverContent>
-          </Popover>
-        {/if}
-      </div>
-    {/if}
-  </div>
+  {#if inSpan}
+    <div class="relative" style:background-color="#fef9c2" style:height="80%" style:margin-top="8%">
+      {#if keyframes.length}
+        <ContextMenu>
+          <ContextMenuTrigger class="absolute top-2.5 h-full w-full pt-0">
+            <div style="margin:auto" style:background-color="#06B6D4" style:height="25%" style:width="50%"></div>
+          </ContextMenuTrigger>
+          <ContextMenuContent>
+            {#each keyframes as keyframe, index (index)}
+              <ContextMenuItem onclick={() => onSeekFrame?.(keyframe)}>
+                <ArrowLeftRightIcon class="size-4"></ArrowLeftRightIcon>
+                Seek frame {keyframe}
+              </ContextMenuItem>
+
+              <ContextMenuItem onclick={() => onDeleteFrame?.(keyframe)}>
+                <Trash2Icon class="size-4"></Trash2Icon>
+                Delete frame
+              </ContextMenuItem>
+            {/each}
+          </ContextMenuContent>
+        </ContextMenu>
+      {/if}
+    </div>
+  {/if}
 </div>
