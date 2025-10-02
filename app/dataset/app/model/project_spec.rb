5aad0ab5ea79216ff549baa8a6e52d6834467ac8
Sorakrit Kungvanruttana <sorakrit-audi@Sorakrits-MacBook-Air.local>
2025-10-02 12:09:14 +0700

    project repo scopes & specs

--- app/dataset/app/model/project_spec.rb
+++ app/dataset/app/model/project_spec.rb
@@ -4,22 +4,13 @@ require "spec_helper"
 
 RSpec.describe Project, database: true do
   describe Project::Repository do
-    # subject{ described_class.new(Verse::Auth::Context.new) }
     system_repo = described_class.new(Verse::Auth::Context[:system])
-
-    let(:account_id) { 1 } # TODO: mocking, should be taken from context
-
-    # let(:project1) {
-    #   subject.create
-    # }
-
-    # describe "#scope:all" do
+    project_member_repo = ProjectMember::Repository.new(Verse::Auth::Context[:system])
 
     let!(:test_project1) {
       system_repo.find!(
         system_repo.create(
           name: "testing_project_1",
-          created_by_id: 1
         )
       )
     }
@@ -27,48 +18,58 @@ RSpec.describe Project, database: true do
       system_repo.find!(
         system_repo.create(
           name: "testing_project_2",
-          created_by_id: 999
         )
       )
     }
-    # let!(:test_project3) {
-    #   repo.find!(
-    #     repo.create(
-    #       name: "testing_project_3",
-    #       created_by_id: 2
-    #     )
-    #   )
-    # }
 
-    context "#scope:all", as: :system do
-      subject { described_class.new(current_auth_context) }
-      it "returns all projects" do
-        projects = system_repo.index({})
+    describe "scope definitions" do
+      context "#scope:all", as: :system do
+        subject { described_class.new(current_auth_context) }
+        it "returns all projects" do
+          projects = system_repo.index({})
 
-        expect(projects.size).to eq(2)
+          expect(projects.size).to eq(2)
+        end
       end
-    end
 
-    describe "#scope:own", as: :project_manager do
-      subject { described_class.new(current_auth_context) }
-      before do
-        @created_project = system_repo.find!(
-          subject.create(
-            name: "testing_project_created",
+      # scope: projects the user owns/creates
+      context "#scope:own", as: :project_manager do
+        subject { described_class.new(current_auth_context) }
+        before do
+          @created_project = system_repo.find!(
+            subject.create(
+              name: "testing_project_created",
+            )
           )
-        )
-      end
-      it "returns projects the user owns/creates" do
-        own_projects = subject.index({})
-        binding.pry
+        end
+        it "returns projects the user owns/creates" do
+          own_projects = subject.index({})
 
-        expect(own_projects.size).to eq(1)
-        expect(own_projects.first.name).to eq("testing_project_created")
+          expect(own_projects.size).to eq(1)
+          expect(own_projects.first.name).to eq(@created_project.name)
+        end
       end
-    end
 
-    describe "#scope:member" do
-      xit "returns projects the user is a member of" do
+      # scope: projects the user is a member of
+      context "#scope:member", as: :annotator do
+        subject { described_class.new(current_auth_context) }
+        before do
+          @testing_project = test_project2
+          project_member_repo.find!(
+            project_member_repo.create(
+              project_id: @testing_project.id,
+              account_id: current_auth_context.metadata[:id],
+              name: "annotator_member",
+              email: "annotator@email.com",
+            )
+          )
+        end
+        it "returns projects the user is a member of" do
+          member_projects = subject.index({})
+
+          expect(member_projects.size).to eq(1)
+          expect(member_projects.first.name).to eq(@testing_project.name)
+        end
       end
     end
   end
