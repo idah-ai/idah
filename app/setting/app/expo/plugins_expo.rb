eccf952b470bf102e862ba5980b1109dd35d73ee
Yacine Petitprez <anykeyh@gmail.com>
2025-10-07 17:18:58 +0700

    wip

--- app/setting/app/expo/plugins_expo.rb
+++ app/setting/app/expo/plugins_expo.rb
@@ -2,4 +2,15 @@ class PluginsExpo < BaseExpo
   http_path "/plugins"
 
   use_service Plugins::Service
+
+  expose on_http(:get, ":plugin/index.js") do
+    input do
+      field :plugin, String
+    end
+  end
+  def serve_frontend
+    service.serve_frontend(
+      params[:plugin]
+    )
+  end
 end
