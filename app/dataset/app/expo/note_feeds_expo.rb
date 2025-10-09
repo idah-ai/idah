89ed461d1185327844ff36824939b1cfdcb1840e
tojo <yraimtj@gmail.com>
2025-10-09 15:06:11 +0300

    WIP note review system

---
+++ app/dataset/app/expo/note_feeds_expo.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+class NoteFeedsExpo < BaseExpo
+  http_path "/note_feeds"
+
+  use_service NoteFeed::Service
+
+  json_api NoteFeed::Record, http_opts: { auth: nil } do
+    allowed_included "entry", "annotation", "note_comments"
+
+    show
+    index do
+      allowed_filters :entry_id__eq,
+                      :annotation_id__eq,
+                      :created_by_id__eq,
+                      :anchor_type__eq,
+                      :status__eq,
+                      :status__in,
+                      :created_at__gte,
+                      :created_at__lte,
+                      :updated_at__gte,
+                      :updated_at__lte
+    end
+
+    create do
+      authorized_relationships entry: [:link],
+                              annotation: [:link]
+    end
+    update
+    delete
+  end
+
+  expose on_http(:post, "/:id/resolve", auth: nil) do
+    desc "Resolve a note feed by updating status from pending to resolved"
+    input do
+      field :id, String
+    end
+  end
+  def resolve
+    note_feed_id = params[:id]
+    service.resolve(note_feed_id)
+  end
+end
---
+++ app/dataset/app/service/note_feed/service.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+module NoteFeed
+  class Service < Verse::Service::Base
+    use note_feeds: NoteFeed::Repository
+
+    def index(filter = {}, included: [], page: 1, items_per_page: 1000, sort: nil, query_count: false)
+      note_feeds.index(
+        filter,
+        included: included,
+        page: page,
+        items_per_page: items_per_page,
+        sort: sort,
+        query_count: query_count
+      )
+    end
+
+    def show(id, included: [])
+      note_feeds.find!(id, included: included)
+    end
+
+    def create(record)
+      attr = record.attributes
+
+      attr[:id] = record.id || UUIDv7.generate
+
+      if record.entry
+        attr[:entry_id] = record.entry.id
+      else
+        raise Verse::Error::ValidationFailed,
+              "entry is required to create a note feed"
+      end
+
+      if record.annotation
+        attr[:annotation_id] = record.annotation.id
+      end
+
+      id = note_feeds.create(attr)
+
+      note_feeds.find!(id)
+    end
+
+    def update(record)
+      note_feeds.update!(record.id, record.attributes)
+      note_feeds.find!(record.id)
+    end
+
+    def delete(id)
+      note_feeds.delete(id)
+    end
+
+    def resolve(id)
+      note_feeds.update!(id, status: "resolved")
+      note_feeds.find!(id)
+    end
+  end
+end
