24f0410bd5115b14c32bac33a3ae121170b64581
whelsm <jiranop.phinyo@gmail.com>
2025-10-15 09:38:12 +0700

    feat: add record for note feeds and comments

---
+++ app/frontend/src/lib/data/model/dataset/notes/feeds/record.ts
@@ -0,0 +1,36 @@
+import { createBackendDataSource } from "@/data/BackendDataSource";
+import { field, Record, RecordFactory, relationship, type } from "@/data/model/Record";
+
+import { AnnotationRecord } from "@/data/model/dataset/annotations/record";
+import { EntryRecord } from "@/data/model/dataset/entries/record";
+import { NoteCommentRecord } from "@/data/model/dataset/notes/comments/record";
+
+import type { Hash } from "@/utils/types";
+
+@type("dataset:note_feeds")
+export class NoteFeedRecord extends Record {
+  @field() public entry_id!: string;
+  @field() public annotation_id!: string;
+  @field() public readonly created_by_id!: number;
+
+  @field() public anchor_type!: string; // ['entry', 'annotation']
+  @field() public position!: Hash;
+
+  @field() public readonly status!: string;
+
+  @field() public content_md!: string;
+
+  @field() public created_at!: string;
+  @field() public updated_at!: Date;
+
+  @relationship() public entry!: EntryRecord;
+  @relationship() public annotation!: AnnotationRecord;
+  @relationship() public note_comments!: NoteCommentRecord[];
+}
+
+RecordFactory.registerTypes(NoteFeedRecord);
+
+export const noteFeedsBackendDataSource = createBackendDataSource(
+  NoteFeedRecord,
+  `${import.meta.env.VITE_IDAH_HOST}/api/v1/dataset/note_feeds`,
+);
