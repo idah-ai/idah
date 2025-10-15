a26a24108d819210544cde5990b0df2fa024072a
tojo <yraimtj@gmail.com>
2025-10-15 14:18:51 +0300

    remove eq filter

--- app/dataset/db/migrations/20251015000000_replace_created_by_id_with_email.rb
+++ app/dataset/db/migrations/20251015000000_replace_created_by_id_with_email.rb
@@ -3,12 +3,12 @@
 Sequel.migration do
   change do
     alter_table(:note_feeds) do
-      add_column :created_by_email, String, null: true
+      add_column :created_by_email, String, null: true, index: true
       drop_column :created_by_id
     end
 
     alter_table(:note_comments) do
-      add_column :created_by_email, String, null: true
+      add_column :created_by_email, String, null: true, index: true
       drop_column :created_by_id
     end
   end
