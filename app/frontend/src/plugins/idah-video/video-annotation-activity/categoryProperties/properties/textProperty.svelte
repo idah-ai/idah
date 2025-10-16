82e19d5c13c8a317e05fa0e9b3058a0bed6094b3
Karnravee Chalermmuang <ptmwilfk@gmail.com>
2025-10-16 13:56:28 +0700

    refactor: enhance property filtering logic and clean up text property component

--- app/frontend/src/plugins/idah-video/video-annotation-activity/categoryProperties/properties/textProperty.svelte
+++ app/frontend/src/plugins/idah-video/video-annotation-activity/categoryProperties/properties/textProperty.svelte
@@ -1,38 +1,28 @@
 <script lang="ts">
   import { Checkbox } from "@/components/ui/checkbox";
   import Input from "@/components/ui/input/input.svelte";
-  import Label from "@/components/ui/label/label.svelte";
-  import type { PropertyField } from "@/plugin/interface/Activity";
+  import Text from "@/components/ui/text/Text.svelte";
   import { formatConformity, propertyFullfilled } from "..";
+
   import type { AnnotationValue } from "@/context/AnnotationContext";
+  import type { PropertyField } from "@/plugin/interface/Activity";
 
-  let {
-    property,
-    value,
-    onValueChange
-  }: {property: PropertyField, value : any, onValueChange: (v:any) => void} = $props()
+  let { property, value, onValueChange }: { property: PropertyField; value: any; onValueChange: (v: any) => void } =
+    $props();
 
-  const invalid = $derived(!propertyFullfilled(value, property))
+  const invalid = $derived(!propertyFullfilled(value, property));
 
-  const format = $derived(invalid ? formatConformity(value, property): [])
+  const format = $derived(invalid ? formatConformity(value, property) : []);
 </script>
 
+<div class="my-2 flex flex-col gap-1">
+  <Text class="text-gray-700" weight="medium" size="sm">{property.label}</Text>
 
-
-<div>
-  <Label>
-    {property.label}
-  </Label>
-  <Input
-    type='text'
-    aria-invalid={invalid}
-    {value}
-    onchange={(e) => onValueChange(e.target.value)}
-    />
+  <Input type="text" aria-invalid={invalid} {value} onchange={(e) => onValueChange(e.target.value)} />
   {#if invalid}
     <ul>
       {#each format as [k, v]}
-        <li style:color=red>{k}:<span>{v}</span></li>
+        <li style:color="red">{k}:<span>{v}</span></li>
       {/each}
     </ul>
   {/if}
