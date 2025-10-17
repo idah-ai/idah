6bda80bf7dd1955bb9d984ce59fb678fef591666
whelsm <jiranop.phinyo@gmail.com>
2025-10-17 15:15:10 +0700

    feat: add markdown components

--- app/frontend/src/lib/components/app/markdown/markdown-preview.svelte
+++ app/frontend/src/lib/components/app/markdown/markdown-preview.svelte
@@ -1,1 +1,29 @@
-Markdown Preview
+<script lang="ts">
+  import { marked } from "marked";
+
+  // Props
+  interface Props {
+    value: string | null;
+  }
+  let { value }: Props = $props();
+
+  // Variables
+  // const renderer = {
+  //   heading(token: Tokens.Heading) {},
+  // };
+
+  // Functions
+  function parseMarkdown(text: string): string {
+    marked.setOptions({
+      renderer: new marked.Renderer(),
+      gfm: true,
+      breaks: true,
+    });
+
+    // marked.use({renderer})
+
+    return marked.parse(text).toString();
+  }
+</script>
+
+{@html parseMarkdown(value ?? "")}
