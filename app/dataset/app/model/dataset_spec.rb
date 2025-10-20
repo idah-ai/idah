194f12b55081199d0e13e699d07bc4204d825371
Sorakrit Kungvanruttana <sorakrit-audi@Sorakrits-MacBook-Air.local>
2025-10-20 14:10:40 +0700

    WIP: specs

--- app/dataset/app/model/dataset_spec.rb
+++ app/dataset/app/model/dataset_spec.rb
@@ -4,10 +4,116 @@ require "spec_helper"
 
 RSpec.describe Dataset, database: true do
   describe Dataset::Repository do
-    subject{ described_class.new(Verse::Auth::Context.new) }
+    system_repo = described_class.new(Verse::Auth::Context[:system])
+    project_repo = Project::Repository.new(Verse::Auth::Context[:system])
+    project_member_repo = ProjectMember::Repository.new(Verse::Auth::Context[:system])
 
-    it "can be instantiated" do
-      expect(subject).to be_a(Dataset::Repository)
+    let!(:test_project1) {
+      project_repo.find!(
+        project_repo.create(
+          name: "testing_project_1",
+        )
+      )
+    }
+
+    let!(:test_dataset1) {
+      system_repo.find!(
+        system_repo.create(
+          project_id: test_project1.id,
+          name: "testing_dataset_1",
+          modality: "video",
+          workflow_configuration: {},
+          labeling_configuration: {},
+        )
+      )
+    }
+
+    let!(:test_project2) {
+      project_repo.find!(
+        project_repo.create(
+          name: "testing_project_2",
+        )
+      )
+    }
+
+    let!(:test_dataset2) {
+      system_repo.find!(
+        system_repo.create(
+          project_id: test_project2.id,
+          name: "testing_dataset_2",
+          modality: "image",
+          workflow_configuration: {},
+          labeling_configuration: {},
+        )
+      )
+    }
+
+    let!(:test_project3) {
+      project_repo.find!(
+        project_repo.create(
+          name: "testing_project_3",
+        )
+      )
+    }
+
+    describe "scope definitions" do
+      context "scope: all", as: :admin do
+        subject { described_class.new(current_auth_context) }
+
+        it "returns all datasets" do
+          datasets = subject.index({})
+
+          expect(datasets.size).to eq(2)
+        end
+      end
+
+      context "scope: as_user", as: :user do
+        subject { described_class.new(current_auth_context) }
+        before do
+          @account_id = current_auth_context.metadata[:id]
+
+          # add testing user as testing project's member
+          project_member_repo.create(
+            project_id: test_project2.id,
+            account_id: @account_id,
+            name: "test user",
+            email: "test@email.com",
+            permission_set: "annotator",
+          )
+          project_member_repo.create(
+            project_id: test_project3.id,
+            account_id: @account_id,
+            name: "test user",
+            email: "test@email.com",
+            permission_set: "annotator",
+          )
+
+          # testing user creates a dataset
+          subject.create(
+            project_id: test_project3.id,
+            name: "user_created_dataset",
+            modality: "video",
+            workflow_configuration: {},
+            labeling_configuration: {},
+          )
+
+          @memberships = project_member_repo.index({ account_id: @account_id })
+        end
+
+        it "returns user-scoped datasets" do
+          datasets = subject.index({})
+
+          expect(datasets.size).to eq(2)
+          # datasets that are in projects that is a member of or owned datasets
+          datasets.each do |dataset|
+            expect(
+              @memberships.any? { |m|
+                m.project_id == dataset.project_id
+              } || dataset.created_by_id == @account_id
+            ).to be_truthy
+          end
+        end
+      end
     end
   end
 end
