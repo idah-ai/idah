7322079fd35d73f8e649396cb4adeab26a51f8b1
Brian West <brian.west@ingedata.ai>
2025-10-29 17:52:36 +0700

    remove logs

--- app/media/app/expo/processor_expo.rb
+++ app/media/app/expo/processor_expo.rb
@@ -3,10 +3,8 @@
 class ProcessorExpo < BaseExpo
   use_service Processor::Service
 
-  puts "WE EXPOSE HERE OR WHAT?"
   expose on_resource_event(Resource::Dataset::Entries, "created")
   def on_entry_created
-    pp({on_entry_created: {params:}})
     service.process_entry(
       params[:resource_id]
     )
