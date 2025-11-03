4baf900f3f22c9a7c16d9fa45b178f25576e9d1e
tojo <yraimtj@gmail.com>
2025-11-03 16:30:50 +0300

    update rspec

--- app/dataset/app/expo/entries_expo.rb
+++ app/dataset/app/expo/entries_expo.rb
@@ -81,7 +81,7 @@ class EntriesExpo < BaseExpo
   def on_job_updated
     job_id = message.content[:resource_id]
 
-    service.mark_entries_status_as(job_id , "ready")
+    service.mark_entries_status_as(job_id, "ready")
   end
 
   expose on_resource_event(Resource::Media::Jobs, "errored")
