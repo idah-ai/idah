7e22543c0c6f41b3e0f68838dc7c35bbb02ab218
Yacine Petitprez <anykeyh@gmail.com>
2025-11-05 18:00:05 +0700

    Implement the endpoint

--- app/setting/app/expo/plugins_expo.rb
+++ app/setting/app/expo/plugins_expo.rb
@@ -25,6 +25,19 @@ class PluginsExpo < BaseExpo
 
   expose on_http(
     :get,
+    "modalities/:modality_name",
+    auth: nil
+  ) do
+    input do
+      field :modality_name, String
+    end
+  end
+  def show_modality
+    service.show_modality(params[:modality_name])
+  end
+
+  expose on_http(
+    :get,
     ":plugin/assets/*",
     auth: nil,
     renderer: Verse::Http::Renderer::Binary
