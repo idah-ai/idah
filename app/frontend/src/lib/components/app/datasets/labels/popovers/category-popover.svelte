f77558bcb3aa81f416d659b5b3a005f939d700d3
whelsm <jiranop.phinyo@gmail.com>
2025-11-05 13:51:43 +0700

    feat: add default color to category-popover

--- app/frontend/src/lib/components/app/datasets/labels/popovers/category-popover.svelte
+++ app/frontend/src/lib/components/app/datasets/labels/popovers/category-popover.svelte
@@ -39,8 +39,8 @@
         <!-- ICON -->
         <div
           class="flex size-5 items-center justify-center rounded-sm"
-          style:background-color={color}
-          style:color={text_color}
+          style:background-color={color || "#000000"}
+          style:color={text_color || "#FFFFFF"}
         >
           <SquareDashedMousePointerIcon class="size-3 text-white" />
         </div>
