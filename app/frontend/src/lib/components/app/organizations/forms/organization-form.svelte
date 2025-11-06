8b14fbc7eef97cc5aa826341ce87fb1c7ffd2c19
Karnravee Chalermmuang <ptmwilfk@gmail.com>
2025-11-06 18:30:30 +0700

    refactor: remove unused Form component and restore FieldSet structure in organization form

--- app/frontend/src/lib/components/app/organizations/forms/organization-form.svelte
+++ app/frontend/src/lib/components/app/organizations/forms/organization-form.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import InputField from "@/components/app/forms/fields/input/input-field.svelte";
-  import Form from "@/components/app/forms/form.svelte";
-  // import { FieldGroup, FieldSet } from "@/components/ui/field";
+
+  import { FieldGroup, FieldSet } from "@/components/ui/field";
   import { OrganizationRecord } from "@/data/model/dataset/organizations/record";
 
   import type { FormBaseProps } from "@/components/app/forms/form.types";
@@ -24,19 +24,17 @@
   });
 </script>
 
-<!-- <FieldSet class="p-1">
-  <FieldGroup> -->
-<Form>
-  <!-- ORGANIZATION::NAME -->
-  <InputField
-    name="{resource}/name"
-    label="Name"
-    placeholder="Organization Name"
-    required
-    errors={fieldErrors["name"]}
-    value={name}
-    oninput={(e) => (name = e.currentTarget.value)}
-  />
-</Form>
-<!-- </FieldGroup>
-</FieldSet> -->
+<FieldSet class="p-1">
+  <FieldGroup>
+    <!-- ORGANIZATION::NAME -->
+    <InputField
+      name="{resource}/name"
+      label="Name"
+      placeholder="Organization Name"
+      required
+      errors={fieldErrors["name"]}
+      value={name}
+      oninput={(e) => (name = e.currentTarget.value)}
+    />
+  </FieldGroup>
+</FieldSet>
