0f7457214c5a921dcb5b3b219f4a2b7bf8501e56
tojo <yraimtj@gmail.com>
2025-11-07 12:59:57 +0300

    allow athentification on every service

--- app/dataset/app/expo/entries_expo.rb
+++ app/dataset/app/expo/entries_expo.rb
@@ -5,7 +5,7 @@ class EntriesExpo < BaseExpo
 
   use_service Entry::Service
 
-  json_api Entry::Record, http_opts: { auth: nil } do
+  json_api Entry::Record do
     allowed_included "dataset", "dataset.project"
     show
     index do
@@ -23,7 +23,7 @@ class EntriesExpo < BaseExpo
     delete
   end
 
-  expose on_http(:patch, "/:id/assign", auth: nil) do
+  expose on_http(:patch, "/:id/assign") do
     desc "Assign a project member to an entry"
     input do
       field :id, String
@@ -42,7 +42,7 @@ class EntriesExpo < BaseExpo
   end
 
   # POST /api/v1/dataset/entries/:id/submit
-  expose on_http(:post, "/:id/submit", auth: nil) do
+  expose on_http(:post, "/:id/submit") do
     desc "Submit workflow event for an entry"
     input do
       field :id, String
@@ -59,7 +59,7 @@ class EntriesExpo < BaseExpo
   end
 
   # POST /api/v1/dataset/entries/:id/error
-  expose on_http(:post, "/:id/error", auth: nil) do
+  expose on_http(:post, "/:id/error") do
     desc "Trigger error event for an entry"
     input do
       field :id, String
