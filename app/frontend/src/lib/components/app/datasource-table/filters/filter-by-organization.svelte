52c53717d29deda7d930fe5f5f52d1a3c0a59d4d
Karnravee Chalermmuang <ptmwilfk@gmail.com>
2025-11-10 11:41:54 +0700

    refactor: simplify FilterByOrganization component by removing unnecessary context checks and options

--- app/frontend/src/lib/components/app/datasource-table/filters/filter-by-organization.svelte
+++ app/frontend/src/lib/components/app/datasource-table/filters/filter-by-organization.svelte
@@ -2,22 +2,15 @@
   import SingleSelectDatasourceField from "@/components/app/forms/fields/select/single/single-select-datasource-field.svelte";
 
   import { OrganizationRecord, organizationsBackendDataSource } from "@/data/model/dataset/organizations/record";
+  import { ProjectRecord } from "@/data/model/dataset/projects/project-record";
 
   import type {
     DataTableColumnFilterOperation,
     DataTableFilterBaseProps,
   } from "@/components/app/datasource-table/types";
-  import { ProjectRecord } from "@/data/model/dataset/projects/project-record";
 
   // Props
-  let { columnSetting, contexts, filters, onFilter }: DataTableFilterBaseProps<ProjectRecord> = $props();
-
-  // Contexts
-  if (!contexts || !("organizationId" in contexts)) {
-    throw new Error("`organizationId` is required in contexts for FilterByOrganization");
-  }
-
-  let { organizationId } = contexts as { organizationId: string };
+  let { columnSetting, filters, onFilter }: DataTableFilterBaseProps<ProjectRecord> = $props();
 
   // Variables
   const resource: string = OrganizationRecord.type;
@@ -38,14 +31,9 @@
 <SingleSelectDatasourceField
   name="{resource}/organization_id"
   dataSource={organizationsBackendDataSource}
-  listOptions={{
-    filters: {
-      id: organizationId,
-    },
-  }}
   displayKey="name"
-  searchable
-  searchKeyWithOperation="name__match"
   value={filters[filterKeyWithOperation]}
+  placeholder="Select an organization"
+  searchKeyWithOperation="name__match"
   onValueChange={handleFilter}
 ></SingleSelectDatasourceField>
