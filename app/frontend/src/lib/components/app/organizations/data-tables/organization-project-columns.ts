f4ab1a0aeede0b831555054eb3862da20f64a7b5
Karnravee Chalermmuang <ptmwilfk@gmail.com>
2025-11-10 09:41:53 +0700

    feat: add organization project columns and update datasource table integration

---
+++ app/frontend/src/lib/components/app/organizations/data-tables/organization-project-columns.ts
@@ -0,0 +1,34 @@
+import { ProjectRecord } from "@/data/model/dataset/projects/project-record";
+
+import type { ColumnsSettings } from "@/components/app/datasource-table/types";
+
+export const organizationProjectColumns: ColumnsSettings<ProjectRecord> = {
+  name: {
+    label: "Project Name",
+    dataType: "string",
+    clickable: false,
+    sortable: true,
+    filterable: true,
+    filterOptions: {
+      filterKey: "name",
+      filterBy: "string",
+      filterOperation: "match",
+    },
+    visible: true,
+    hidable: false,
+  },
+  created_at: {
+    label: "Created At",
+    dataType: "datetime",
+    clickable: false,
+    sortable: true,
+    filterable: true,
+    filterOptions: {
+      filterKey: "created_at",
+      filterBy: "date-range",
+      filterOperation: "gte",
+    },
+    visible: true,
+    hidable: false,
+  },
+};
