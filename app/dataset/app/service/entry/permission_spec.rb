ea021247bd2de2635dc324a5eec0d380d4332451
sorakrit-audi <sorakrit.audi@gmail.com>
2025-11-13 13:39:46 +0700

    fix specs

--- app/dataset/app/service/entry/permission_spec.rb
+++ app/dataset/app/service/entry/permission_spec.rb
@@ -11,10 +11,10 @@ RSpec.describe Entry::Service, database: true do
 
   # Projects
   let!(:first_project_id) {
-    project_repo.create(name: "Project 1", created_by_email: "system@example.com")
+    project_repo.create(name: "Project 1", created_by_email: "system@example.com", organization_id: 1)
   }
   let!(:second_project_id) {
-    project_repo.create(name: "Project 2", created_by_email: "system@example.com")
+    project_repo.create(name: "Project 2", created_by_email: "system@example.com", organization_id: 1)
   }
 
   # Project Members
