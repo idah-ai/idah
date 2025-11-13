d12a12470bf985fab5479f75fe35297561e535ad
Karnravee Chalermmuang <ptmwilfk@gmail.com>
2025-11-13 14:00:41 +0700

    style: enhance dark mode styles and improve hover effects in categories selection and timeline table

--- app/frontend/src/plugins/idah-video/video-annotation-activity/timeline-table/timeline-cell.svelte
+++ app/frontend/src/plugins/idah-video/video-annotation-activity/timeline-table/timeline-cell.svelte
@@ -63,8 +63,8 @@
 
 <div
   class={cn("inline-block h-full border-b py-1 first:border-l", {
-    "bg-primary/60": isSelected,
-    "bg-primary/70 cursor-pointer": isHovered,
+    "bg-primary/50": isSelected,
+    "bg-primary/60 cursor-pointer": isHovered,
   })}
   style:box-sizing="border-box"
   style:width="{cellWidth}%"
