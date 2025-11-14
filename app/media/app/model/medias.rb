067c9b7a546bb4ec16aacee07bdc69f3f065c084
sorakrit-audi <sorakrit.audi@gmail.com>
2025-11-14 15:53:54 +0700

    WIP: media permissions

--- app/media/app/model/medias.rb
+++ app/media/app/model/medias.rb
@@ -22,6 +22,10 @@ module Medias
     field :created_at, type: Time
     field :updated_at, type: Time
 
+    field :project_id, type: Integer
+    field :dataset_id, type: Integer
+    field :entry_id, type: Integer
+
     def open
       Verse::Plugin[:shrine].with_storage do |storage|
         storage.open(id)
@@ -34,5 +38,41 @@ module Medias
   class Repository < Verse::Sequel::Repository
     self.table = "medias"
     self.resource = Resource::Media::Medias
+
+    def scoped(action)
+      auth_context.can!(action, self.class.resource) do |scope|
+        scope.all? { table }
+
+        scope.as_org_owner? { table.where(project_id: fetch_project_from_orgs) }
+
+        scope.as_user? { table.where(project_id: fetch_project_from_memberships) }
+      end
+    end
+
+    privates
+
+    def fetch_project_from_orgs
+      org_ids = auth_context.custom_scopes[:org]
+
+      Verse::Cache.with_cache(
+        "media/medias/service/projects",
+        "account_id:#{auth_context.metadata[:id]}",
+        expires_in: 180
+      ) do
+        Api[:idah].dataset.projects.index(organization_id: org_ids).data.map(&:id).uniq
+      end
+    end
+
+    def fetch_project_from_memberships
+      account_id = auth_context.metadata[:id]
+
+      Verse::Cache.with_cache(
+        "media/medias/service/memberships",
+        "account_id:#{account_id}",
+        expires_in: 180
+      ) do
+        Api[:idah].dataset.project_members.index(filter: {account_id: }).data.map(&:project_id).uniq
+      end
+    end
   end
 end
