fa0829648b13161754fb00232886fc61834b328b
whelsm <jiranop.phinyo@gmail.com>
2025-11-17 10:00:16 +0700

    style: add idah-logo on dark mode and favicon

--- app/frontend/src/routes/+layout.svelte
+++ app/frontend/src/routes/+layout.svelte
@@ -1,11 +1,19 @@
 <script lang="ts">
-  import "../app.css";
   import { ModeWatcher } from "mode-watcher";
+
+  import favicon from "@/components/app/brand/idah-logo-favicon.svg";
+
   import { Toaster } from "@/components/ui/sonner";
+
+  import "../app.css";
 
   // Props
   let { children } = $props();
 </script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
 
 <main class="bg-background relative flex min-h-screen flex-col">
   <ModeWatcher />
