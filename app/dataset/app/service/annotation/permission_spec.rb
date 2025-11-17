fd8c74416b5c610207c6ce51520ab9d038ffa958
sorakrit-audi <sorakrit.audi@gmail.com>
2025-11-17 13:24:32 +0700

    fixing specs

--- app/dataset/app/service/annotation/permission_spec.rb
+++ app/dataset/app/service/annotation/permission_spec.rb
@@ -12,13 +12,13 @@ RSpec.describe Annotation::Service, database: true do
 
   # Projects
   let(:first_project_id) {
-    project_repo.create(name: "Project 1", created_by_email: "system@example.com")
+    project_repo.create(name: "Project 1", created_by_email: "system@example.com", organization_id: 1)
   }
   let(:second_project_id) {
-    project_repo.create(name: "Project 2", created_by_email: "system@example.com")
+    project_repo.create(name: "Project 2", created_by_email: "system@example.com", organization_id: 1)
   }
   let(:third_project_id) {
-    project_repo.create(name: "Project 3", created_by_email: "system@example.com")
+    project_repo.create(name: "Project 3", created_by_email: "system@example.com", organization_id: 1)
   }
 
   # Accounts IDs
