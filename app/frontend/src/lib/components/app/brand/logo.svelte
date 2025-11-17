24d5aeb8ce194590bd733b3681693407a126b5ea
whelsm <jiranop.phinyo@gmail.com>
2025-11-17 10:00:16 +0700

    style: add idah-logo on dark mode and favicon

--- app/frontend/src/lib/components/app/brand/logo.svelte
+++ app/frontend/src/lib/components/app/brand/logo.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
   import type { HTMLAttributes } from "svelte/elements";
 
-  import idahLogoSvg from "@/components/app/brand/idah-logo.svg";
+  import { mode } from "mode-watcher";
 
-  import { type WithElementRef } from "@/utils";
+  import idahLogoDarkSvg from "@/components/app/brand/idah-logo-dark.svg";
+  import idahLogoLightSvg from "@/components/app/brand/idah-logo-light.svg";
+
+  import { cn, type WithElementRef } from "@/utils";
 
   // Props
   let { class: className }: WithElementRef<HTMLAttributes<HTMLImageElement>> = $props();
+
+  // Variables
+  let isLightMode = $derived(mode.current === "light")
 </script>
 
-<img src={idahLogoSvg} alt="idah-logo" class={className} />
+<img src={isLightMode ? idahLogoLightSvg : idahLogoDarkSvg} alt="idah-logo-{isLightMode ? "light" : "dark"}" class={cn("size-24", className)} />
