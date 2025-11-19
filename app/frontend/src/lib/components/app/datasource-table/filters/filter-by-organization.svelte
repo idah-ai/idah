9310bd41531097cfcb1d102dbd1ebdb3558ea534
whelsm <jiranop.phinyo@gmail.com>
2025-11-19 10:51:49 +0700

    chore: update project to filter by multiple organizationId

--- app/frontend/src/lib/components/app/datasource-table/filters/filter-by-organization.svelte
+++ app/frontend/src/lib/components/app/datasource-table/filters/filter-by-organization.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
-  import SingleSelectDatasourceField from "@/components/app/forms/fields/select/single/single-select-datasource-field.svelte";
+  import MultipleSelectDatasourceField from "@/components/app/forms/fields/select/multiple/multiple-select-datasource-field.svelte";
 
-  import { OrganizationRecord, organizationsBackendDataSource } from "@/data/model/iam/organizations/record";
   import { ProjectRecord } from "@/data/model/dataset/projects/project-record";
+  import { OrganizationRecord, organizationsBackendDataSource } from "@/data/model/iam/organizations/record";
 
   import type {
     DataTableColumnFilterOperation,
     DataTableFilterBaseProps,
   } from "@/components/app/datasource-table/types";
+  import type { LabelValue } from "@/utils/types";
 
   // Props
   let { columnSetting, filters, onFilter }: DataTableFilterBaseProps<ProjectRecord> = $props();
@@ -15,25 +16,25 @@
   // Variables
   const resource: string = OrganizationRecord.type;
   const filterKey: string = columnSetting.filterOptions?.filterKey || "organization_id";
-  const filterOperation: DataTableColumnFilterOperation = columnSetting.filterOptions?.filterOperation || "eq";
+  const filterOperation: DataTableColumnFilterOperation = columnSetting.filterOptions?.filterOperation || "in";
   const filterKeyWithOperation: string = `${filterKey}__${filterOperation}`;
 
   // Functions
-  function handleFilter(value: string | number): void {
+  function handleFilter(selectedChoices: LabelValue<string | number>[]): void {
     onFilter({
       filters: {
-        [filterKeyWithOperation]: value,
+        [filterKeyWithOperation]: selectedChoices.map((choice) => choice.value),
       },
     });
   }
 </script>
 
-<SingleSelectDatasourceField
+<MultipleSelectDatasourceField
   name="{resource}/organization_id"
   dataSource={organizationsBackendDataSource}
   displayKey="name"
-  value={filters[filterKeyWithOperation]}
+  values={filters[filterKeyWithOperation]}
   placeholder="Select an organization"
   searchKeyWithOperation="name__match"
-  onValueChange={handleFilter}
-></SingleSelectDatasourceField>
+  onSelected={handleFilter}
+></MultipleSelectDatasourceField>
