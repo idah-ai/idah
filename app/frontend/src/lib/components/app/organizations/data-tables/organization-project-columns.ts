cfc7a2f60a1e73942f68b33ba5d86662faee4b95
whelsm <jiranop.phinyo@gmail.com>
2025-11-19 10:12:16 +0700

    refactor: create a column constant for project & account

--- app/frontend/src/lib/components/app/organizations/data-tables/organization-project-columns.ts
+++ app/frontend/src/lib/components/app/organizations/data-tables/organization-project-columns.ts
@@ -1,37 +1,9 @@
-import OrganizationProjectNameCell from "@/components/app/organizations/data-tables/organization-project-name-cell.svelte";
-
+import { projectCreatedAtColumn, projectNameColumn } from "@/components/app/projects/datasource-tables/project-columns";
 import { ProjectRecord } from "@/data/model/dataset/projects/project-record";
 
 import type { ColumnsSettings } from "@/components/app/datasource-table/types";
 
 export const organizationProjectColumns: ColumnsSettings<ProjectRecord> = {
-  name: {
-    label: "Project Name",
-    dataType: "string",
-    clickable: false,
-    sortable: true,
-    filterable: true,
-    filterOptions: {
-      filterKey: "name",
-      filterBy: "string",
-      filterOperation: "match",
-    },
-    visible: true,
-    hidable: false,
-    cellComponent: OrganizationProjectNameCell,
-  },
-  created_at: {
-    label: "Created At",
-    dataType: "datetime",
-    clickable: false,
-    sortable: true,
-    filterable: true,
-    filterOptions: {
-      filterKey: "created_at",
-      filterBy: "date-range",
-      filterOperation: "gte",
-    },
-    visible: true,
-    hidable: false,
-  },
+  name: projectNameColumn,
+  created_at: projectCreatedAtColumn,
 };
