1b05cdce0fbbfaa08cac8293f997c30dcce1a7d0
whelsm <jiranop.phinyo@gmail.com>
2025-11-19 10:12:49 +0700

    feat: display each selected organization owner

--- app/frontend/src/lib/components/app/organizations/forms/organization-owners-form.svelte
+++ app/frontend/src/lib/components/app/organizations/forms/organization-owners-form.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
+  import { Trash2Icon, UserRoundIcon } from "@lucide/svelte";
+
   import MultipleSelectDatasourceField from "@/components/app/forms/fields/select/multiple/multiple-select-datasource-field.svelte";
-  import { Badge } from "@/components/ui/badge";
+  import Badge from "@/components/ui/badge/badge.svelte";
+  import Button from "@/components/ui/button/button.svelte";
   import { FieldGroup, FieldSet } from "@/components/ui/field";
 
+  import { Item, ItemActions, ItemContent, ItemMedia, ItemTitle } from "@/components/ui/item";
   import { AccountRecord, accountsBackendDataSource } from "@/data/model/iam/accounts/record";
 
   // Props
@@ -14,14 +18,20 @@
   // Variables
   const resource: string = AccountRecord.type;
 
-  let selectedAccounts = $state([]);
+  let selectedAccounts: { account: AccountRecord }[] = $state([]);
+
+  // Functions
+  function removeSelectedOwner(accountId: string) {
+    owners = owners.filter((id) => id !== accountId);
+    selectedAccounts = selectedAccounts.filter((item) => item.account.id !== accountId);
+  }
 </script>
 
 <FieldSet class="p-1">
   <FieldGroup>
     <MultipleSelectDatasourceField
       name="{resource}/owner"
-      values={selectedAccounts}
+      values={owners}
       dataSource={accountsBackendDataSource}
       placeholder="Select owners"
       searchKeyWithOperation="email__match"
@@ -29,8 +39,9 @@
       searchPlaceholder="Search owners by email"
       displayKey="email"
       clearable
-      onSelected={(selected) => {
-        owners.push(String(selected));
+      onSelected={(selectedChoices) => {
+        owners = selectedChoices.map((choice) => String(choice.value));
+        selectedAccounts = selectedChoices.map((choice) => ({ account: choice.data as AccountRecord }));
       }}
     >
       {#snippet slotTriggerValues({ selectedChoices })}
@@ -43,5 +54,26 @@
         </div>
       {/snippet}
     </MultipleSelectDatasourceField>
+
+    <section class="flex flex-col gap-2">
+      {#each selectedAccounts as selectedAccount (selectedAccount)}
+        {@const { id, email } = selectedAccount.account}
+        <Item variant="outline" size="sm">
+          <ItemMedia variant="icon">
+            <UserRoundIcon />
+          </ItemMedia>
+
+          <ItemContent>
+            <ItemTitle>{email}</ItemTitle>
+          </ItemContent>
+
+          <ItemActions>
+            <Button variant="ghost" size="icon-sm" onclick={() => removeSelectedOwner(id)}>
+              <Trash2Icon />
+            </Button>
+          </ItemActions>
+        </Item>
+      {/each}
+    </section>
   </FieldGroup>
 </FieldSet>
