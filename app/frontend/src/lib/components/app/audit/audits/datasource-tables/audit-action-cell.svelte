cb281925069e71c3163f9fd1c7e18880aceda18d
whelsm <jiranop.phinyo@gmail.com>
2025-11-24 14:34:37 +0700

    feat: setup frontend audit log page & components

---
+++ app/frontend/src/lib/components/app/audit/audits/datasource-tables/audit-action-cell.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import AuditActionBadge from "@/components/app/audit/audits/badges/audit-action-badge.svelte";
+
+  import { AuditRecord } from "@/data/model/audit/audits/record";
+
+  import type { DataTableCellBaseProps } from "@/components/app/datasource-table/types";
+
+  // Props
+  let { record: auditRecord }: DataTableCellBaseProps<AuditRecord> = $props();
+</script>
+
+<AuditActionBadge {auditRecord} />
--- app/frontend/src/lib/components/app/datasets/tabs/dataset.tabs.ts
+++ app/frontend/src/lib/components/app/datasets/tabs/dataset.tabs.ts
@@ -1,9 +1,10 @@
 import type { BaseTabs } from "@/components/ui/tabs/tabs.types";
 
-export type DatasetTab = "entries" | "analytics" | "labels";
+export type DatasetTab = "entries" | "analytics" | "labels" | "audit-logs";
 
 export const datasetTabs: BaseTabs<DatasetTab> = [
   { label: "Entries", value: "entries" },
   // { label: "Analytics", value: "analytics" },
   { label: "Label Editor", value: "labels" },
+  { label: "Audit Logs", value: "audit-logs" },
 ];
