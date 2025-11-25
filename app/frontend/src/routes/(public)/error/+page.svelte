f2b5401273a76633fd71c414796bc97b5bad8dab
Karnravee Chalermmuang <ptmwilfk@gmail.com>
2025-11-25 16:37:40 +0700

    feat: enhance invalid invitation landing page layout and styling

--- app/frontend/src/routes/(public)/error/+page.svelte
+++ app/frontend/src/routes/(public)/error/+page.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
   import Text from "@/components/ui/text/Text.svelte";
+
 </script>
 
-<div class="flex h-full w-full content-center justify-center">
-  <Text size="h1">The invitation link is invalid or expired.</Text>
-  <Text>If you believe this is a mistake, please contact the administrator.</Text>
+<div class="flex h-screen w-full items-center justify-center">
+  <div class="flex flex-col gap-3 text-center">
+    <Text size="h4" weight="bold">The invitation link is invalid or expired.</Text>
+    <Text>If you believe this is a mistake, please contact the administrator.</Text>
+  </div>
 </div>
