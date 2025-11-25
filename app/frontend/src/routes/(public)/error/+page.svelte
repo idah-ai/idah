e887bd036893b4fa8c953a74b583c82ee22cc0b1
Karnravee Chalermmuang <ptmwilfk@gmail.com>
2025-11-25 14:47:13 +0700

    feat: add invalid invitation landing page

---
+++ app/frontend/src/routes/(public)/error/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import Text from "@/components/ui/text/Text.svelte";
+</script>
+
+<div class="flex h-full w-full content-center justify-center">
+  <Text size="h1">The invitation link is invalid or expired.</Text>
+  <Text>If you believe this is a mistake, please contact the administrator.</Text>
+</div>
