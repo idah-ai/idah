f061ae2e2087428620632db5e80cfd7cfed4f2a3
Brian West <brian.west@ingedata.ai>
2025-11-26 08:38:50 +0700

    timeline delete selection reactivity fix + hide unknown format errors

--- app/frontend/src/plugins/idah-video/video-annotation-activity/categoryProperties/properties/MultiSelectProperty.svelte
+++ app/frontend/src/plugins/idah-video/video-annotation-activity/categoryProperties/properties/MultiSelectProperty.svelte
@@ -59,8 +59,8 @@
 
         {#if formatter && formatter(v)}
           <li style:color="red">{formatter(v)}</li>
-        {:else}
-          <li style:color="red">{k}:<span>{v}</span></li>
+          <!-- {:else}
+          <li style:color="red">{k}:<span>{v}</span></li> -->
         {/if}
       {/each}
     </ul>
