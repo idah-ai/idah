520f48a36f7b5218c09773e46a3226e13ae89df1
Karnravee Chalermmuang <ptmwilfk@gmail.com>
2025-11-27 10:02:49 +0700

    feat: Implement account joining functionality and error handling
    - Updated the join method in accountsBackendDataSource to remove the joinedAt parameter and use the current date instead.
    - Added a new error page for invalid or expired links with appropriate messaging and an SVG graphic.
    - Created a new reset password page that handles account joining and redirects to the error page on failure.
    - Removed the old reset password page implementation.

---
+++ app/frontend/src/routes/(public)/error/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import Text from "@/components/ui/text/Text.svelte";
+  import Invalidlink from "../invalid-link.svg";
+</script>
+
+<div class="flex h-screen w-full items-center justify-center">
+  <div class="flex flex-col gap-4 text-center">
+    <img class="h-50 w-full items-center justify-center" src={Invalidlink} alt="invalid-link" />
+
+    <Text size="h2" weight="bold">Invalid or expired link</Text>
+
+    <div class="flex flex-col gap-1 text-center">
+      <Text>The invitation link is invalid or expired.</Text>
+      <Text>If you believe this is a mistake, please contact the administrator.</Text>
+    </div>
+  </div>
+</div>
---
+++ app/frontend/src/routes/(public)/reset_password/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import { goto } from "$app/navigation";
+    import { page } from "$app/state";
+    import { accountsBackendDataSource } from "@/data/model/iam/accounts/record";
+
+  let accountId: string = page.url.searchParams.get("id") as string;
+  console.log(accountId);
+
+  async function joinAccount(): Promise<void> {
+    try{
+
+      const response = await accountsBackendDataSource.join({ id: accountId });
+    }
+    catch (error) {
+      goto("/error");
+    }
+    
+  }
+</script>
+
+{#await joinAccount()}
+    
+  {:then _} 
+    
+  {/await}
