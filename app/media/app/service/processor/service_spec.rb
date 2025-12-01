e88cc0a3eaec25647145dcf191b01b7d9a3159e3
sorakrit-audi <sorakrit.audi@gmail.com>
2025-12-01 13:41:30 +0700

    fix spec

--- app/media/app/service/processor/service_spec.rb
+++ app/media/app/service/processor/service_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe Processor::Service, type: :service, database: true do
           priority: 1,
           wf_step: "processing",
           status: "ready",
-          job_id: 123,
+          job_id: UUIDv7.generate,
           resource: "some-resource-identifier",
           assigned_to_member_id: nil,
           created_at: "2023-10-27T10:00:00Z",
