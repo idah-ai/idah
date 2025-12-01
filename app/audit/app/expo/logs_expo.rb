f409bc72f2e5aab3ec8d1b9ff6e808cd5151ec2a
sorakrit-audi <sorakrit.audi@gmail.com>
2025-12-01 10:39:17 +0700

    WIP: audit logs

---
+++ app/audit/app/expo/logs_expo.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+class LogsExpo < BaseExpo
+  http_path "/logs"
+
+  use_service Log::Service
+
+  json_api Log::Record do
+    show
+    index
+  end
+
+  def create_audit_log
+    service.create(message)
+  end
+
+  # resources we want to include in Audit Logs
+  # TODO: complete this list and refactor/regex somehow ?
+  %w[
+    iam:accounts
+    dataset:projects
+    dataset:project_members
+    dataset:datasets
+    dataset:entries
+    dataset:annotations
+  ].each do |resource|
+    # events/actions we want to include in Audit Logs
+    %w[created updated deleted].each do |event|
+      attach_exposition(
+        :create_audit_log,
+        build_expose(on_resource_event(resource, event))
+      )
+    end
+  end
+end
