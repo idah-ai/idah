f696a597283e1284a4a2b35dc200b2e246b0099d
Karnravee Chalermmuang <ptmwilfk@gmail.com>
2025-12-02 12:14:03 +0700

    Refactor authentication status component to streamline loading and authorization rendering logic

--- app/frontend/src/lib/components/app/iam/auth/authentication-status.svelte
+++ app/frontend/src/lib/components/app/iam/auth/authentication-status.svelte
@@ -1,38 +1,38 @@
 <script lang="ts">
-	import { cn } from "@/utils";
-	import { onMount, type Snippet } from "svelte";
-
-	interface Props {
-		class?: string | null;
-		loading?: Snippet;
-		authorized?: Snippet;
-		unauthorized?: Snippet;
-	}
-	let { class: className, loading, authorized, unauthorized }: Props = $props();
-
-	// internal state
-	let authStatus: "loading" | "authorized" | "unauthorized" = $state("loading");
-
-	onMount(async () => {
-		await checkAuthStatus();
-	});
-
-	async function checkAuthStatus(): Promise<void> {
-		try {
-			// your real API call
-			authStatus = "unauthorized";
-		} catch (err) {
-			authStatus = "unauthorized";
-		}
-	}
+  import { cn } from "@/utils";
+  import { onMount, type Snippet } from "svelte";
+  
+	// Props
+  interface Props {
+    class?: string | null;
+    // Slots
+    loading?: Snippet;
+    authorized?: Snippet;
+    unauthorized?: Snippet;
+  }
+  let { class: className, loading, authorized, unauthorized }: Props = $props();
+  
+	// Lifecycle
+  onMount(async () => {
+    await checkAuthStatus();
+  });
+	
+  // Functions
+  async function checkAuthStatus(): Promise<void> {}
 </script>
 
 <div class={cn("", className)}>
-	{#if authStatus === "loading"}
-		{@render loading?.()}
-	{:else if authStatus === "authorized"}
-		{@render authorized?.()}
-	{:else if authStatus === "unauthorized"}
-		{@render unauthorized?.()}
-	{/if}
+  {#if loading}
+    {@render loading?.()}
+  {:else}
+    <!-- <ApplicationLoading /> -->
+  {/if}
+
+  {@render authorized?.()}
+
+  {#if unauthorized}
+    {@render unauthorized?.()}
+  {:else}
+    <!-- <Redirect to="/login" /> -->
+  {/if}
 </div>
