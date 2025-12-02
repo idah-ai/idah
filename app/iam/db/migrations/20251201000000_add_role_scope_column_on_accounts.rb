9b68746a028790892480ff142f780d2c8535821f
tojo <yraimtj@gmail.com>
2025-12-02 10:04:37 +0300

    remove unsused migration

--- app/iam/db/migrations/20251201000000_add_role_scope_column_on_accounts.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-Sequel.migration do
-  change do
-    alter_table(:accounts) do
-      add_column :role_scope, :jsonb, null: false, default: "{}"
-    end
-  end
-end
