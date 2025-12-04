937f823b46d9dbe9df9a27e8e20a6cdf2857cad3
Brian West <brian.west@ingedata.ai>
2025-12-04 20:24:16 +0700

    plugin refactor wip

---
+++ app/sync/app/service/export/dataset_context.rb
@@ -0,0 +1,30 @@
+module Export
+  class DatasetContext
+    attr_reader :dataset, :entries
+
+    def initialize(dataset, entries)
+      @dataset = dataset
+      @entries = entries
+    end
+
+    def self.from_dataset(dataset)
+      DatasetContext.new(
+        dataset,
+        Verse::Util::Iterator.chunk_iterator(1) do |entry_page|
+          entries_response = Api[:idah].dataset.entries.index(
+            filters: {dataset_id: dataset[:id]},
+            page: {number: entry_page, size: 100}, query_count: false)
+          raise entries_response.errors if entries_response.errors
+
+          if entries_response.data.empty?
+            nil
+          else
+            entries_response.data
+          end
+        end.lazy.map(&:data).map do |entry|
+          EntryContext.from_entry entry
+        end
+      )
+    end
+  end
+end
