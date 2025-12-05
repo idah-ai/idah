be1c028989d3d88c7e11e55265a6b6b683e2ca00
Brian West <brian.west@ingedata.ai>
2025-12-05 15:35:56 +0700

    quick review

--- app/sync/app/service/export/universal_portable_dataset.rb
+++ app/sync/app/service/export/universal_portable_dataset.rb
@@ -8,89 +8,104 @@ module Export
     def run
       Open3.popen3(
         "bin/datset-static", # TODO: build or embed binary within plugin
-        "-i", [:export, context.name, Time.now.to_i, :upd].join('.'),
+        "-i", [context.name, :upd].join('.'),
         "append"
       ) do |stdin, stdout, stderr, wait_thr|
-        stdin.puts({command: 'init', args: {}}.to_json)
+        stdout_thr = Thread.new do
+          stdout.readline do |line|
+            Verse.logger.info {"#{line.chomp}"}
+          end
+        end
 
-        context.datasets.each do |dataset_context|
-          stdin.puts({
-            command: 'dataset:create',
-            args: {
-              id: dataset_context.dataset[:id],
-              name: dataset_context.dataset[:attributes][:name],
-              modality: dataset_context.dataset[:attributes][:modality],
-              metadata: {
-                "Created-At": dataset_context.dataset[:attributes][:created_at]&.gsub(/ (\+\d{2})(\d{2})/, '\1:\2'),
-                "Updated-At": dataset_context.dataset[:attributes][:updated_at]&.gsub(/ (\+\d{2})(\d{2})/, '\1:\2'),
-                "Created-By": nil
-              }.to_json
-            }
-          }.to_json)
+        files = []
+        begin
+          stdin.puts({command: 'init', args: {}}.to_json)
+          stdin.flush
 
-          dataset_context.entries.each do |entry_context|
-            file = Tempfile.new(entry_context.entry[:attributes][:resource])
-            file.write(entry_context.media_file)
-            file.close
-
+          context.datasets.each do |dataset_context|
             stdin.puts({
-              command: 'media:create',
+              command: 'dataset:create',
               args: {
-                id: entry_context.media_info[:id],
-                key: entry_context.media_info[:attributes][:key],
-                file: file.path,
-                mimetype: entry_context.media_info[:attributes][:mime_type],
+                id: dataset_context.dataset[:id],
+                name: dataset_context.dataset[:attributes][:name],
+                modality: dataset_context.dataset[:attributes][:modality],
                 metadata: {
-                  "Created-At": entry_context.media_info[:attributes][:created_at]&.gsub(/ (\+\d{2})(\d{2})/, '\1:\2'),
-                  "Updated-At": entry_context.media_info[:attributes][:updated_at]&.gsub(/ (\+\d{2})(\d{2})/, '\1:\2'),
-                  "Created-By": entry_context.media_info[:attributes][:created_by]
-                }.to_json
-              }
-            }.to_json)
-
-            stdin.puts({
-              command: 'entry:create',
-              args: {
-                id: entry_context.entry[:id],
-                dataset_id: entry_context.entry[:attributes][:dataset_id],
-                url: entry_context.entry[:attributes][:resource],
-                metadata: {
-                  "Created-At": entry_context.entry[:attributes][:created_at]&.gsub(/ (\+\d{2})(\d{2})/, '\1:\2'),
-                  "Updated-At": entry_context.entry[:attributes][:updated_at]&.gsub(/ (\+\d{2})(\d{2})/, '\1:\2'),
+                  "Created-At": dataset_context.dataset[:attributes][:created_at]&.gsub(/ (\+\d{2})(\d{2})/, '\1:\2'),
+                  "Updated-At": dataset_context.dataset[:attributes][:updated_at]&.gsub(/ (\+\d{2})(\d{2})/, '\1:\2'),
                   "Created-By": nil
-                }.to_json
+                }
               }
             }.to_json)
-
-            entry_context.annotations.each do |annotation_context|
+            stdin.flush
+            dataset_context.entries.each do |entry_context|
+              file = Tempfile.new(entry_context.entry[:attributes][:resource])
+              file.write(entry_context.media_file)
+              file.close
+              files << file
               stdin.puts({
-                command: 'annotation:create',
+                command: 'media:create',
                 args: {
-                  id: annotation_context.annotation[:id],
-                  entry_id: annotation_context.annotation[:attributes][:entry_id],
-                  type: Hash(annotation_context.annotation[:attributes][:dimensions])[:type],
-                  shape: Hash(annotation_context.annotation[:attributes][:dimensions]).select {|k, _| k != :type}.to_json,
-                  annotation: Hash(annotation_context.annotation[:attributes][:annotation]).to_json,
+                  id: entry_context.media_info[:id],
+                  key: entry_context.media_info[:attributes][:key],
+                  file: file.path,
+                  mimetype: entry_context.media_info[:attributes][:mime_type],
                   metadata: {
-                    "Created-At": annotation_context.annotation[:attributes][:created_at]&.gsub(/ (\+\d{2})(\d{2})/, '\1:\2'),
-                    "Updated-At": annotation_context.annotation[:attributes][:updated_at]&.gsub(/ (\+\d{2})(\d{2})/, '\1:\2'),
-                    "Created-By": nil
-                  }.to_json
+                    "Created-At": entry_context.media_info[:attributes][:created_at]&.gsub(/ (\+\d{2})(\d{2})/, '\1:\2'),
+                    "Updated-At": entry_context.media_info[:attributes][:updated_at]&.gsub(/ (\+\d{2})(\d{2})/, '\1:\2'),
+                    "Created-By": entry_context.media_info[:attributes][:created_by]
+                  }
                 }
               }.to_json)
+              stdin.flush
+              stdin.puts({
+                command: 'entry:create',
+                args: {
+                  id: entry_context.entry[:id],
+                  dataset_id: entry_context.entry[:attributes][:dataset_id],
+                  url: entry_context.entry[:attributes][:resource],
+                  metadata: {
+                    "Created-At": entry_context.entry[:attributes][:created_at]&.gsub(/ (\+\d{2})(\d{2})/, '\1:\2'),
+                    "Updated-At": entry_context.entry[:attributes][:updated_at]&.gsub(/ (\+\d{2})(\d{2})/, '\1:\2'),
+                    "Created-By": nil
+                  }
+                }
+              }.to_json)
+              stdin.flush
+              entry_context.annotations.each do |annotation_context|
+                stdin.puts({
+                  command: 'annotation:create',
+                  args: {
+                    id: annotation_context.annotation[:id],
+                    entry_id: annotation_context.annotation[:attributes][:entry_id],
+                    type: Hash(annotation_context.annotation[:attributes][:dimensions])[:type],
+                    shape: Hash(annotation_context.annotation[:attributes][:dimensions]).select {|k, _| k != :type}.to_json,
+                    annotation: Hash(annotation_context.annotation[:attributes][:annotation]).to_json,
+                    metadata: {
+                      "Created-At": annotation_context.annotation[:attributes][:created_at]&.gsub(/ (\+\d{2})(\d{2})/, '\1:\2'),
+                      "Updated-At": annotation_context.annotation[:attributes][:updated_at]&.gsub(/ (\+\d{2})(\d{2})/, '\1:\2'),
+                      "Created-By": nil
+                    }
+                  }
+                }.to_json)
+                stdin.flush
+              end
             end
           end
-
-          # todo >>>
+        ## todo review VVV
+        rescue Exception => e
+          Verse.logger.error {"UPD export error:#{e}"}
+          raise e
+        ensure
+          stdout_thr.join
           stdin.close
-          value = wait_thr.value
-          error = stderr.read
-          if !error.empty? #  TODO review return value && raise on time
-            raise RuntimeError, error
-          else
-            puts stdout.read
+          if wait_thr.value.exitstatus != 0
+            raise "updcli error #{wait_thr.value} #{stderr.read}"
           end
-          # <<< todo
+          files.each &:unlink # earlier ?
+          # why isnt it flushed within thread ?
+          # (updcli STDOUT/STDERR newline flushed ?, open3?pty?)
+          Verse::logger.info { "UPD export unflushed Logs...\n#{stdout.read}" }
+          Verse::logger.info { "UPD export done: #{wait_thr.value}" }
         end
       end
     end
