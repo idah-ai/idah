cfa874941b9d3ea78d107c8a50a9cbf627bb841f
tojo <yraimtj@gmail.com>
2025-12-08 12:39:22 +0300

    fix rspec

--- app/dataset/app/service/project_member/service_spec.rb
+++ app/dataset/app/service/project_member/service_spec.rb
@@ -11,7 +11,12 @@ RSpec.describe ProjectMember::Service, database: true do
   let(:project_member_repo) { ProjectMember::Repository.new(auth_context) }
 
   let!(:project_id) do
-    project_repo.create(name: "Test Project", description: "A test project", created_by_email: "user@example.com")
+    project_repo.create(
+      name: "Test Project",
+      description: "A test project",
+      created_by_email: "user@example.com",
+      organization_id: 1
+    )
   end
 
   let(:attributes) do
