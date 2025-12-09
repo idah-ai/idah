a19187b5ecccb291399546a7134c6d6cc3a6c504
Karnravee Chalermmuang <ptmwilfk@gmail.com>
2025-12-09 13:50:22 +0700

    refactor: update password validation rules in resetPasswordSchema

--- app/frontend/src/lib/data/model/iam/accounts/auth-schema.ts
+++ app/frontend/src/lib/data/model/iam/accounts/auth-schema.ts
@@ -34,20 +34,11 @@ export const sendResetPasswordLinkSchema = z.object({
 
 const passwordRules = z
   .string()
-  .min(8, "Password must be at least 8 characters long")
-  .max(100, "Password must be at most 100 characters long")
-  .refine((value) => /[A-Z]/.test(value), {
-    message: "Password must include at least one uppercase letter",
-  })
-  .refine((value) => /[a-z]/.test(value), {
-    message: "Password must include at least one lowercase letter",
-  })
-  .refine((value) => /\d/.test(value), {
-    message: "Password must include at least one digit",
-  })
-  .refine((value) => /[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]/.test(value), {
-    message: "Password must include at least one special character",
-  });
+  .min(8, "Password must be at least 8 characters.")
+  .regex(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+    "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.",
+  );
 
 export const resetPasswordSchema = z
   .object({
@@ -58,20 +49,3 @@ export const resetPasswordSchema = z
     message: "Passwords do not match",
     path: ["confirmPassword"],
   });
-
-// export const resetPasswordSchema = z
-//   .object({
-//     password: z
-//       .string()
-//       .min(8, "Password must be at least 8 characters long")
-//       .max(100, "Password must be at most 100 characters long"),
-
-//     confirmPassword: z
-//       .string()
-//       .min(8, "Confirm Password must be at least 8 characters long")
-//       .max(100, "Confirm Password must be at most 100 characters long"),
-//   })
-//   .refine((data) => data.password === data.confirmPassword, {
-//     message: "Passwords do not match",
-//     path: ["confirmPassword"],
-//   });
