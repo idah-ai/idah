b064ae626ac5d57a8de847309428eede61494735
Brian West <brian.west@ingedata.ai>
2025-12-11 12:16:10 +0700

    refactor UPD process

--- plugins/idah-sync-upd-process/backends/sync/universal_portable_dataset_process/sync.rb
+++ plugins/idah-sync-upd-process/backends/sync/universal_portable_dataset_process/sync.rb
@@ -164,77 +164,21 @@ module UniversalPortableDatasetProcess
     end
 
     def process_dataset_context(dataset_context, &block)
-      yield({
-        command: 'dataset:create',
-        args: {
-          id: dataset_context.dataset[:id],
-          name: dataset_context.dataset[:attributes][:name],
-          modality: dataset_context.dataset[:attributes][:modality],
-          metadata: {
-            "Created-At": dataset_context.dataset[:attributes][:created_at]&.gsub(/ (\+\d{2})(\d{2})/, '\1:\2'),
-            "Updated-At": dataset_context.dataset[:attributes][:updated_at]&.gsub(/ (\+\d{2})(\d{2})/, '\1:\2'),
-            "Created-By": nil
-          }
-        }
-      }.to_json)
+      on_dataset dataset_context, &block
       dataset_context.entries.each do |entry_context|
         process_entry_context entry_context, &block
       end
     end
 
     def process_entry_context(entry_context, &block)
-      file = Tempfile.new(entry_context.entry[:attributes][:resource])
-      file.write(entry_context.media_file)
-      file.close
-      @files << file
-      yield({
-        command: 'media:create',
-        args: {
-          id: entry_context.media_info[:id],
-          key: entry_context.media_info[:attributes][:key],
-          file: file.path,
-          mimetype: entry_context.media_info[:attributes][:mime_type],
-          metadata: {
-            "Created-At": entry_context.media_info[:attributes][:created_at]&.gsub(/ (\+\d{2})(\d{2})/, '\1:\2'),
-            "Updated-At": entry_context.media_info[:attributes][:updated_at]&.gsub(/ (\+\d{2})(\d{2})/, '\1:\2'),
-            "Created-By": entry_context.media_info[:attributes][:created_by]
-          }
-        }
-      }.to_json)
-      yield({
-        command: 'entry:create',
-        args: {
-          id: entry_context.entry[:id],
-          dataset_id: entry_context.entry[:attributes][:dataset_id],
-          url: entry_context.entry[:attributes][:resource],
-          metadata: {
-            "Created-At": entry_context.entry[:attributes][:created_at]&.gsub(/ (\+\d{2})(\d{2})/, '\1:\2'),
-            "Updated-At": entry_context.entry[:attributes][:updated_at]&.gsub(/ (\+\d{2})(\d{2})/, '\1:\2'),
-            "Created-By": nil
-          }
-        }
-      }.to_json)
+      on_entry entry_context, &block
       entry_context.annotations.each do |annotation_context|
         process_annotation_context annotation_context, &block
       end
     end
 
     def process_annotation_context(annotation_context, &block)
-      yield({
-        command: 'annotation:create',
-        args: {
-          id: annotation_context.annotation[:id],
-          entry_id: annotation_context.annotation[:attributes][:entry_id],
-          type: Hash(annotation_context.annotation[:attributes][:dimensions])[:type],
-          shape: Hash(annotation_context.annotation[:attributes][:dimensions]).select {|k, _| k != :type}.to_json,
-          annotation: Hash(annotation_context.annotation[:attributes][:annotation]).to_json,
-          metadata: {
-            "Created-At": annotation_context.annotation[:attributes][:created_at]&.gsub(/ (\+\d{2})(\d{2})/, '\1:\2'),
-            "Updated-At": annotation_context.annotation[:attributes][:updated_at]&.gsub(/ (\+\d{2})(\d{2})/, '\1:\2'),
-            "Created-By": nil
-          }
-        }
-      }.to_json)
+      on_annotation annotation_context, &block
     end
   end
 end
