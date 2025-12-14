cce460fe277b80b16beb935823e2747593619c88
Brian West <brian.west@ingedata.ai>
2025-12-14 16:15:06 +0700

    isolate api

--- app/sync/app/service/context.rb
+++ app/sync/app/service/context.rb
@@ -1,34 +1,10 @@
 module Context
-  RootContext = Data.define(:name, :datasets, :entries, :annotations)
+  RootContext = Data.define(:name, :api)
 
   def self.root(args, api = :idah)
-    args = Hash(args)
-    dataset_id = args.dig(:datasets, :id)
-    entry_id = args.dig(:entries, :id)
-    annotation_id = args.dig(:annotations, :id)
-    datasets, entries, annotations = nil, nil, nil
-    if (dataset_id)
-      datasets = ContextApi::Datasets.new({id: dataset_id}, args, api),
-      entries = ContextApi::Entries.new({dataset_id:}, args, api)
-      # TODO check
-      annotations = ContextApi::Annotations.new({dataset_id:}, args, api)
-    elsif (entry_id)
-      entries = ContextApi::Entries.new({id: entry_id}, args, api)
-      entry = entries.show
-      datasets = entry.datasets
-      annotations = entry.annotations
-    elsif (annotation_id)
-      annotations = ContextApi::Annotations.new({id: annotation_id}, args, api)
-      annotation = annotations.show
-      entries = annotation.entries
-      entry = entries.show
-      datasets = entry.datasets
-    end
     RootContext.new(
       [:export, Time.now.to_i].join("."),
-      datasets || ContextApi::Datasets.new({}, args, api),
-      entries || ContextApi::Entries.new({}, args, api),
-      annotations || ContextApi::Annotations.new({}, args, api)
+      ContextApi.api(args, api)
     )
   end
 end
