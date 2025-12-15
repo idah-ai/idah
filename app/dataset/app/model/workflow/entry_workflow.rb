793ee78a18ccf4ea9761856cc4dee8e8d46b0d41
roger <rcserrano06@gmail.com>
2025-12-15 13:05:39 +0700

    Dataset: move workflow logic to Workflow::SimpleReviewAnnotationWorkflow

--- app/dataset/app/model/workflow/entry_workflow.rb
+++ app/dataset/app/model/workflow/entry_workflow.rb
@@ -12,7 +12,7 @@ module Workflow
       state :error
 
       # Transition from start to annotation phase
-      event :submit do
+      event :submit, after: :on_submit do
         transitions from: :start, to: :annotate
 
         transitions from: :annotate, to: :review, if: :should_sample?
@@ -47,6 +47,44 @@ module Workflow
     def allowed_note_feed?
       # check if current step allows note feeds by state
       ["annotate", "review"].include?(@entry.wf_step)
+    end
+
+    private
+
+    def on_submit
+      account_id = entries.auth_context.metadata[:id]
+      current_state = aasm.current_state
+      from_state = aasm.from_state
+      to_state = aasm.to_state
+
+      assigned_to_id =
+        case from_state
+        when :start
+          account_id
+        when :annotate
+          # If moving to review step, assign to reviewer (nil for unassigned)
+          @entry.reviewed_by_id
+        when :review
+          # If moving back to annotation step, re-assign to original annotator
+          to_state == :annotate ? @entry.submitted_by_id : nil
+        end
+
+      # Set submitted_by_id coming from annotation step
+      submitted_by_id = from_state == :annotate ? account_id : @entry.submitted_by_id
+
+      # Set reviewed_by_id coming from review step
+      reviewed_by_id = from_state == :review ? account_id : @entry.reviewed_by_id
+
+      entries.submit(
+        entry.id,
+        {
+          wf_step: current_state.to_s,
+          status: current_state == :done ? "completed" : "in_progress",
+          assigned_to_id:,
+          submitted_by_id:,
+          reviewed_by_id:,
+        }
+      )
     end
   end
 end
