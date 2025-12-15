a645019ae7d06194c77026e2ec32c2a50dbdf979
Brian West <brian.west@ingedata.ai>
2025-12-15 14:20:50 +0700

    :file_class

--- app/sync/app/service/export/service.rb
+++ app/sync/app/service/export/service.rb
@@ -8,7 +8,7 @@ module Export
       Jobs::Service.new(auth_context).create(
         "Export::Job",
         arguments: {
-          from_class: "Context::UniversalPortableDataset::Append",
+          file_class: "Context::UniversalPortableDataset::Append",
           context_args:,
           process_class: "Export::UniversalPortableDataset",
           process_args: {},}
