1e8616590d92b0580e89d493ce474203c6f15f6b
Karnravee Chalermmuang <ptmwilfk@gmail.com>
2025-12-17 12:01:10 +0700

    feat: add dark and light logo components for improved branding

---
+++ app/frontend/src/lib/components/app/brand/idah-logo-light.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import type { HTMLAttributes } from "svelte/elements";
+
+  import idahLogoLightSvg from "@/components/app/brand/idah-logo-light.svg";
+  
+  import { cn, type WithElementRef } from "@/utils";
+
+  // Props
+  let { class: className }: WithElementRef<HTMLAttributes<HTMLImageElement>> = $props();
+</script>
+
+<img src={idahLogoLightSvg} alt="idah-logo-light" class={cn("size-24", className)} />
+
--- app/frontend/src/lib/components/app/brand/logo.svelte
+++ app/frontend/src/lib/components/app/brand/logo.svelte
@@ -1,22 +1,22 @@
 <script lang="ts">
   import type { HTMLAttributes } from "svelte/elements";
-
+  
   import { mode } from "mode-watcher";
-
-  import idahLogoDarkSvg from "@/components/app/brand/idah-logo-dark.svg";
-  import idahLogoLightSvg from "@/components/app/brand/idah-logo-light.svg";
-
-  import { cn, type WithElementRef } from "@/utils";
+  
+  import IdahLogoDark from "@/components/app/brand/idah-logo-dark.svelte";
+  import IdahLogoLight from "@/components/app/brand/idah-logo-light.svelte";
+  
+  import { type WithElementRef } from "@/utils";
 
   // Props
   let { class: className }: WithElementRef<HTMLAttributes<HTMLImageElement>> = $props();
 
   // Variables
-  let isLightMode = $derived(mode.current === "light");
+  let isLightMode = $state(mode.current === "light");
 </script>
 
-<img
-  src={isLightMode ? idahLogoLightSvg : idahLogoDarkSvg}
-  alt="idah-logo-{isLightMode ? "light" : "dark"}"
-  class={cn("size-24", className)}
-/>
+{#if isLightMode}
+  <IdahLogoLight class={className} />
+{:else}
+  <IdahLogoDark class={className} />
+{/if}
