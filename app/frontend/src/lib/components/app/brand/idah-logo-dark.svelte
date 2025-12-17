a7b9d1a7bc2148a2217a9c91e7f43589fcf1c53d
Karnravee Chalermmuang <ptmwilfk@gmail.com>
2025-12-17 13:30:00 +0700

    fix: remove unnecessary whitespace in logo components for cleaner code

--- app/frontend/src/lib/components/app/brand/idah-logo-dark.svelte
+++ app/frontend/src/lib/components/app/brand/idah-logo-dark.svelte
@@ -2,7 +2,7 @@
   import type { HTMLAttributes } from "svelte/elements";
 
   import idahLogoDarkSvg from "@/components/app/brand/idah-logo-dark.svg";
-  
+
   import { cn, type WithElementRef } from "@/utils";
 
   // Props
@@ -10,5 +10,3 @@
 </script>
 
 <img src={idahLogoDarkSvg} alt="idah-logo-dark" class={cn("size-24", className)} />
-
-
