a804a5be26cafb25f165af6ad8e129603d1b227e
Brian West <brian.west@ingedata.ai>
2025-12-26 17:43:31 +0700

    auth_context filtering preparation

--- app/sync/app/expo/export_expo.rb
+++ app/sync/app/expo/export_expo.rb
@@ -9,6 +9,22 @@ class ExportExpo < BaseExpo
     desc "export endpoint"
     input do
       field? :filter do
+        field? :organizations do
+          field? :id, String
+          field? :name, String
+        end
+        field? :projects do
+          field? :id, String
+          field? :name, String
+          field? :created_by_email, String
+        end
+        field? :project_members do
+          field? :id, String
+          field? :name, String
+          field? :email, String
+          field? :role, String
+          field? :invited_by_id, String
+        end
         field? :datasets do
           field? :id, String
           field? :project_id, String
---
+++ app/sync/app/service/context/context_api/projects.rb
@@ -0,0 +1,27 @@
+module Context
+  module ContextApi
+    class Projects < Crud
+      Context = Data.define(:record, :api, :members, :datasets, :entries, :annotations)
+
+      def initialize(api = :idah, args = {}, context_filters = {}, opts = {}, &context_builder)
+        context_builder ||= proc do |project|
+          Context.new(project,
+            Projects.new(api, args, merge_context_filters(id: project[:id])),
+            ProjectMembers.new(api, args, merge_context_filters(project_id: project[:id])),
+            Datasets.new(api, args, merge_context_filters(project_id: project[:id])),
+            Entries.new(api, args, merge_context_filters(project_id: project[:id])),
+            Annotations.new(api, args, merge_context_filters(project_id: project[:id])),
+          )
+        end
+        super(
+          Api[api].dataset.projects,
+          api,
+          args,
+          context_filters,
+          opts,
+          context_builder
+        )
+      end
+    end
+  end
+end
