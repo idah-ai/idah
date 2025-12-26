9014da401ad40fd7f85e382780a4785af7574757
Brian West <brian.west@ingedata.ai>
2025-12-26 09:24:09 +0700

    prettier

--- app/frontend/src/plugins/idah-video/layout/sidebar/categories-selection.svelte
+++ app/frontend/src/plugins/idah-video/layout/sidebar/categories-selection.svelte
@@ -275,7 +275,7 @@
       {#await haveAnnotationsInCategory(category.id) then hasAnnotations}
         <CollapsibleTrigger
           class={cn("text-secondary-foreground flex w-full items-center justify-between pr-1 text-xs", {
-            "bg-secondary border-ring text-secondary-foreground border-1 rounded-sm": selected == category.id,
+            "bg-secondary border-ring text-secondary-foreground rounded-sm border-1": selected == category.id,
             "hover:bg-primary-foreground hover:dark:bg-accent hover:cursor-pointer hover:rounded-sm":
               !category.requiredNested,
             "hover:bg-accent hover:cursor-pointer hover:rounded-sm": !toolMode,
