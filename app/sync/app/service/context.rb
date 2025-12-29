406e6c468fd0a0ff565033df528de10ff28065ac
Brian West <brian.west@ingedata.ai>
2025-12-29 09:27:34 +0700

    review context / remove exports

--- app/sync/app/service/context.rb
+++ app/sync/app/service/context.rb
@@ -1,5 +1,11 @@
 module Context
   Context = Data.define(:contexts) do
+    def initialize(contexts: [])
+      raise "Invalid Context missing name" if Array(contexts).any? { |c| !c.respond_to?(:name) }
+      raise "Invalid Context name" if Array(contexts).any? { |c| !c.name.is_a? String }
+      super
+    end
+
     def method_missing(name, *args)
       context = contexts.find { |c| c.name.to_sym == name }
       if context
@@ -10,11 +16,11 @@ module Context
     end
 
     def respond_to_missing?(name, include_private = false)
-      contexts.any? { |c| c.name == name } || super
+      contexts.any? { |c| c.name.to_sym == name } || super
     end
   end
 
-  def self.new(contexts)
-    Context.new(contexts)
+  def self.new(contexts = [])
+    Context.new(contexts: contexts)
   end
 end
