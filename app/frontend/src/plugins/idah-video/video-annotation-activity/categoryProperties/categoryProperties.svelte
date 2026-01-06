f99ad2ad2b2ab7165bf8ef9aadf0384045477515
tojo <yraimtj@gmail.com>
2026-01-06 16:45:39 +0300

    reput category name

--- app/frontend/src/plugins/idah-video/video-annotation-activity/categoryProperties/categoryProperties.svelte
+++ app/frontend/src/plugins/idah-video/video-annotation-activity/categoryProperties/categoryProperties.svelte
@@ -68,7 +68,11 @@
 
 <div>
   <div class="flex pb-1">
-    <Text class="text-muted-foreground" weight="medium" size="xs">BKDHJKSHFDJKSDHFJKS</Text>
+    <Text class="text-muted-foreground" weight="medium" size="xs"
+      >{((s: string) => [s.slice(0, 1).toUpperCase(), s.slice(1)].join(""))(
+        type.split(":").reverse()[0].split(new RegExp(/-|_/)).join(" "),
+      )}</Text
+    >
   </div>
   {#key $idb_updated_at}
     <Select type="single" onValueChange={onSelectCategory} {disabled}>
