47ca4af1242960ce2c71c16a26044204c7542e8b
sorakrit-audi <sorakrit.audi@gmail.com>
2026-01-08 13:58:04 +0700

    adjust specs

--- app/audit/app/service/log/service_spec.rb
+++ app/audit/app/service/log/service_spec.rb
@@ -37,161 +37,133 @@ RSpec.describe Log::Service, database: true do
       expect(log.resource_id).to eq content[:resource_id]
       expect(log.actor_account_id).to eq 1
     end
-  end
 
-  describe "#audit_log_details" do
-    it "creates an audit log for organization record" do
-      organization_id = 1
-      content = {
-        args: [],
-        resource_id: organization_id,
-        metadata: {
-          # usually included in the message
-          expo: "OrganizationsExpo",
-          service: "Organization::Service",
-          at: Time.now,
-          account_id: actor_account_id,
-          # added metadata
-          actor_account_id:,
-          actor_account_email:
+    describe "#audit_log_details" do
+      it "creates an audit log for organization record" do
+        organization_id = 1
+        event = "iam:organizations:created"
+        content = {
+          args: [],
+          resource_id: organization_id,
+          metadata: {
+            # usually included in the message
+            expo: "OrganizationsExpo",
+            service: "Organization::Service",
+            at: Time.now,
+            account_id: actor_account_id,
+            # added metadata
+            actor_account_id:,
+            actor_account_email:
+          }
         }
-      }
-      expect(content[:metadata]&.[](:organization_id)).to be_nil # should be extracted from resource_id
+        expect(content[:metadata]&.[](:organization_id)).to be_nil # should be extracted from resource_id
 
-      # publish an event
-      Verse.publish_resource_event(
-        resource_type: "iam:organizations",
-        resource_id: organization_id,
-        event: "created",
-        payload: content
-      )
+        log = subject.create_from_event(event, content)
 
-      log = subject.index({}).first
+        expect(log.actor_account_id).to eq actor_account_id
+        expect(log.actor_account_email).to eq actor_account_email
+        expect(log.resource_type).to eq "organizations"
+        expect(log.organization_id).to eq content[:resource_id]
+        expect(log.project_id).to be_nil
+        expect(log.dataset_id).to be_nil
+        expect(log.entry_id).to be_nil
+      end
 
-      expect(log.actor_account_id).to eq actor_account_id
-      expect(log.actor_account_email).to eq actor_account_email
-      expect(log.resource_type).to eq "organizations"
-      expect(log.organization_id).to eq content[:resource_id]
-      expect(log.project_id).to be_nil
-      expect(log.dataset_id).to be_nil
-      expect(log.entry_id).to be_nil
-    end
-
-    it "creates an audit log for project record" do
-      project_id = UUIDv7.generate
-      content = {
-        args: [],
-        resource_id: project_id,
-        metadata: {
-          # usually included in the message
-          expo: "ProjectsExpo",
-          service: "Project::Service",
-          at: Time.now,
-          account_id: actor_account_id,
-          # added metadata
-          actor_account_id:,
-          actor_account_email:,
-          organization_id: 1
+      it "creates an audit log for project record" do
+        project_id = UUIDv7.generate
+        event = "dataset:projects:created"
+        content = {
+          args: [],
+          resource_id: project_id,
+          metadata: {
+            # usually included in the message
+            expo: "ProjectsExpo",
+            service: "Project::Service",
+            at: Time.now,
+            account_id: actor_account_id,
+            # added metadata
+            actor_account_id:,
+            actor_account_email:,
+            organization_id: 1
+          }
         }
-      }
-      expect(content[:metadata]&.[](:project_id)).to be_nil # should be extracted from resource_id
+        expect(content[:metadata]&.[](:project_id)).to be_nil # should be extracted from resource_id
 
-      # publish an event
-      Verse.publish_resource_event(
-        resource_type: "dataset:projects",
-        resource_id: project_id,
-        event: "created",
-        payload: content
-      )
+        log = subject.create_from_event(event, content)
 
-      log = subject.index({}).first
+        expect(log.actor_account_id).to eq actor_account_id
+        expect(log.actor_account_email).to eq actor_account_email
+        expect(log.resource_type).to eq "projects"
+        expect(log.organization_id).to eq content[:metadata][:organization_id]
+        expect(log.project_id).to eq content[:resource_id]
+        expect(log.dataset_id).to be_nil
+        expect(log.entry_id).to be_nil
+      end
 
-      expect(log.actor_account_id).to eq actor_account_id
-      expect(log.actor_account_email).to eq actor_account_email
-      expect(log.resource_type).to eq "projects"
-      expect(log.organization_id).to eq content[:metadata][:organization_id]
-      expect(log.project_id).to eq content[:resource_id]
-      expect(log.dataset_id).to be_nil
-      expect(log.entry_id).to be_nil
-    end
-
-    it "creates an audit log for dataset record" do
-      dataset_id = UUIDv7.generate
-      content = {
-        args: [],
-        resource_id: dataset_id,
-        metadata: {
-          # usually included in the message
-          expo: "DatasetsExpo",
-          service: "Dataset::Service",
-          at: Time.now,
-          account_id: actor_account_id,
-          # added metadata
-          actor_account_id:,
-          actor_account_email:,
-          organization_id: 1,
-          project_id: UUIDv7.generate
+      it "creates an audit log for dataset record" do
+        dataset_id = UUIDv7.generate
+        event = "dataset:datasets:created"
+        content = {
+          args: [],
+          resource_id: dataset_id,
+          metadata: {
+            # usually included in the message
+            expo: "DatasetsExpo",
+            service: "Dataset::Service",
+            at: Time.now,
+            account_id: actor_account_id,
+            # added metadata
+            actor_account_id:,
+            actor_account_email:,
+            organization_id: 1,
+            project_id: UUIDv7.generate
+          }
         }
-      }
-      expect(content[:metadata]&.[](:dataset_id)).to be_nil # should be extracted from resource_id
+        expect(content[:metadata]&.[](:dataset_id)).to be_nil # should be extracted from resource_id
 
-      # publish an event
-      Verse.publish_resource_event(
-        resource_type: "dataset:datasets",
-        resource_id: dataset_id,
-        event: "created",
-        payload: content
-      )
+        log = subject.create_from_event(event, content)
 
-      log = subject.index({}).first
+        expect(log.actor_account_id).to eq actor_account_id
+        expect(log.actor_account_email).to eq actor_account_email
+        expect(log.resource_type).to eq "datasets"
+        expect(log.organization_id).to eq content[:metadata][:organization_id]
+        expect(log.project_id).to eq content[:metadata][:project_id]
+        expect(log.dataset_id).to eq content[:resource_id]
+        expect(log.entry_id).to be_nil
+      end
 
-      expect(log.actor_account_id).to eq actor_account_id
-      expect(log.actor_account_email).to eq actor_account_email
-      expect(log.resource_type).to eq "datasets"
-      expect(log.organization_id).to eq content[:metadata][:organization_id]
-      expect(log.project_id).to eq content[:metadata][:project_id]
-      expect(log.dataset_id).to eq content[:resource_id]
-      expect(log.entry_id).to be_nil
-    end
-
-    it "creates an audit log for entry record" do
-      entry_id = UUIDv7.generate
-      content = {
-        args: [],
-        resource_id: entry_id,
-        metadata: {
-          # usually included in the message
-          expo: "EntriesExpo",
-          service: "Entry::Service",
-          at: Time.now,
-          account_id: actor_account_id,
-          # added metadata
-          actor_account_id:,
-          actor_account_email:,
-          organization_id: 1,
-          project_id: UUIDv7.generate,
-          dataset_id: UUIDv7.generate
+      it "creates an audit log for entry record" do
+        entry_id = UUIDv7.generate
+        event = "dataset:entries:created"
+        content = {
+          args: [],
+          resource_id: entry_id,
+          metadata: {
+            # usually included in the message
+            expo: "EntriesExpo",
+            service: "Entry::Service",
+            at: Time.now,
+            account_id: actor_account_id,
+            # added metadata
+            actor_account_id:,
+            actor_account_email:,
+            organization_id: 1,
+            project_id: UUIDv7.generate,
+            dataset_id: UUIDv7.generate
+          }
         }
-      }
-      expect(content[:metadata]&.[](:entry_id)).to be_nil # should be extracted from resource_id
+        expect(content[:metadata]&.[](:entry_id)).to be_nil # should be extracted from resource_id
 
-      # publish an event
-      Verse.publish_resource_event(
-        resource_type: "dataset:entries",
-        resource_id: entry_id,
-        event: "created",
-        payload: content
-      )
+        log = subject.create_from_event(event, content)
 
-      log = subject.index({}).first
-
-      expect(log.actor_account_id).to eq actor_account_id
-      expect(log.actor_account_email).to eq actor_account_email
-      expect(log.resource_type).to eq "entries"
-      expect(log.organization_id).to eq content[:metadata][:organization_id]
-      expect(log.project_id).to eq content[:metadata][:project_id]
-      expect(log.dataset_id).to eq content[:metadata][:dataset_id]
-      expect(log.entry_id).to eq content[:resource_id]
+        expect(log.actor_account_id).to eq actor_account_id
+        expect(log.actor_account_email).to eq actor_account_email
+        expect(log.resource_type).to eq "entries"
+        expect(log.organization_id).to eq content[:metadata][:organization_id]
+        expect(log.project_id).to eq content[:metadata][:project_id]
+        expect(log.dataset_id).to eq content[:metadata][:dataset_id]
+        expect(log.entry_id).to eq content[:resource_id]
+      end
     end
   end
 
