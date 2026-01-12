3a0b8b08b42577f1b80333921684cbd34eb47aab
roger <rcserrano06@gmail.com>
2026-01-12 11:32:51 +0700

    Frontend: add assigned_to_id filter on account entries url

--- app/frontend/src/lib/components/app/projects/entries/account-entries.svelte
+++ app/frontend/src/lib/components/app/projects/entries/account-entries.svelte
@@ -9,6 +9,8 @@
   }
 
   let entries: EntryRecord[] = $state([]);
+  let countEntriesByDataset: Record<string, number> = $state({});
+  let nameDatasetMap: Record<string, string> = $state({});
   let { accountId, projectId }: Props = $props();
 
   async function fetchEntries(): Promise<void> {
@@ -21,9 +23,31 @@
     entries = (
       await entriesBackendDataSource.list({
         filters: filters,
-        fields: { "dataset:entries": ["resource", "project_id", "dataset_id"] },
+        fields: {
+          "dataset:datasets": ["name"],
+          "dataset:entries": ["resource", "project_id", "dataset_id", "assigned_to_id"],
+        },
+        included: ["dataset"],
       })
     ).data;
+
+    countEntriesByDataset = entries.reduce(
+      (acc, entry) => {
+        const datasetId = entry.dataset_id;
+        acc[datasetId] = (acc[datasetId] || 0) + 1;
+        return acc;
+      },
+      {} as Record<string, number>,
+    );
+
+    nameDatasetMap = entries.reduce(
+      (acc, entry) => {
+        const datasetId = entry.dataset_id;
+        acc[datasetId] = entry.dataset.name;
+        return acc;
+      },
+      {} as Record<string, string>,
+    );
   }
 
   onMount(async () => {
@@ -33,12 +57,17 @@
 
 {#if entries.length > 0}
   <div class="text-muted-foreground text-sm">
-    <div>Following entries({entries.length}) will be unassigned from this account.</div>
-    <ul class="mt-2 ml-5 max-h-40 overflow-y-auto">
-      {#each entries as entry (entry.id)}
+    <div>Entries on these datasets will be unassigned from this account.</div>
+    <ul class="ml-5 mt-2 max-h-40 overflow-y-auto">
+      {#each Object.entries(countEntriesByDataset) as [datasetId, count]}
         <li>
-          <Link href={`/projects/${entry.project_id}/datasets/${entry.dataset_id}/entries`} target="_blank">
-            {entry.resource}
+          <Link
+            href={`/projects/${projectId}/datasets/${datasetId}/entries?assigned_to_id=${accountId}`}
+            target="_blank"
+          >
+            {nameDatasetMap[datasetId]}
+            ({count}
+            {count === 1 ? "entry" : "entries"})
           </Link>
         </li>
       {/each}
