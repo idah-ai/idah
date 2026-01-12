c4a9d80dde0dd9a7dcbb5d0b842c84c1542d9721
sorakrit-audi <sorakrit.audi@gmail.com>
2026-01-12 14:53:37 +0700

    shortcut implementations: multiple frames next/previous, escape

--- app/frontend/src/plugins/idah-video/video-annotation-activity/shortcut.ts
+++ app/frontend/src/plugins/idah-video/video-annotation-activity/shortcut.ts
@@ -1,8 +1,18 @@
 import { KeyMapBuilder } from "@/shortcut/KeyMapBuilder";
 import { ShortcutManager } from "@/shortcut/ShortcutManager";
-import { DEFAULT_MODE, IDAH_VIDEO_BOUNDING_BOX } from "../type";
+import { DEFAULT_MODE, IDAH_NOTE, IDAH_VIDEO_BOUNDING_BOX, IDAH_VIDEO_LOCALSTORAGE_FRAME_STEP } from "../type";
 import type Video from "./video.svelte";
 import type { IActivityContext } from "@/plugin/interface/Activity";
+
+// let see what we need
+type KeyMapContext = {
+  context: IActivityContext;
+  player: () => Video | undefined;
+  toggleCommandCB: () => void;
+  flush: () => void;
+  switch_mode: (mode: string) => void;
+  zoom: { in: () => void; out: () => void };
+};
 
 const CommonInjecter = (context: KeyMapContext) => {
   const flushAction = () => {
@@ -30,11 +40,15 @@ const CommonInjecter = (context: KeyMapContext) => {
 
   return (b) => {
     b.on([b.Alt], "S", flushAction, "Flush", "flush action");
-    b.on([b.Ctrl], "Z", undoAction, "Undo", "Undo last action");
     b.on([b.Ctrl], "Space", toggleCommand, "Commands Dialog", "Toggle this commands dialog");
-
+    b.on([b.Ctrl], "Z", undoAction, "Undo", "Undo last action");
     b.on([b.Ctrl, b.Shift], "Z", redoAction, "Redo", "Redo last undone action if any");
+
+    // modes
     b.on(null, "D", enterMode(ShortcutManager.defaultMode, true), "Default View", "Reset View");
+    b.on(null, "Escape", enterMode(ShortcutManager.defaultMode, true), "Default View", "Reset View");
+    b.on(null, "B", enterMode(IDAH_VIDEO_BOUNDING_BOX), "Bounding box", "Enter Bouding box mode");
+    b.on(null, "N", enterMode(IDAH_NOTE), "Note", "Enter Note mode");
   };
 };
 
@@ -51,16 +65,6 @@ const createBoundingBoxModeKeyMap = (context: KeyMapContext) => {
   });
 };
 
-// let see what we need
-type KeyMapContext = {
-  context: IActivityContext;
-  player: () => Video | undefined;
-  toggleCommandCB: () => void;
-  flush: () => void;
-  switch_mode: (mode: string) => void;
-  zoom: { in: () => void; out: () => void };
-};
-
 const createVisualModeKeyMap = (context: KeyMapContext) => {
   const tooglePlay = () => {
     context.player()?.togglePlay();
@@ -72,24 +76,31 @@ const createVisualModeKeyMap = (context: KeyMapContext) => {
     console.log("Next Frame executed");
   };
 
+  const nextMultipleFrames = () => {
+    const frameStep = Number(localStorage.getItem(IDAH_VIDEO_LOCALSTORAGE_FRAME_STEP)) || 10;
+    context.player()?.nextFrame(frameStep);
+    console.log(`Next ${frameStep} Frame executed`);
+  };
+
   const previousFrame = () => {
     context.player()?.previousFrame();
     console.log("Previous Frame executed");
   };
+
+  const previousMultipleFrames = () => {
+    const frameStep = Number(localStorage.getItem(IDAH_VIDEO_LOCALSTORAGE_FRAME_STEP)) || 10;
+    context.player()?.previousFrame(frameStep);
+    console.log(`Previous ${frameStep} Frame executed`);
+  };
+
   const startFrame = () => {
     context.player()?.seekToFrame(0);
     console.log("startFrame executed");
   };
+
   const endFrame = () => {
     context.player()?.seekToFrame(context.player()?.getFrames() || 0);
     console.log("endFrame executed");
-  };
-
-  const enterMode = (mode: string, replace: boolean = false) => {
-    return () => {
-      ShortcutManager.enterMode(mode, replace);
-      context.switch_mode(mode);
-    };
   };
 
   const keyMap = KeyMapBuilder((b) => {
@@ -97,9 +108,11 @@ const createVisualModeKeyMap = (context: KeyMapContext) => {
     b.on(null, "Space", tooglePlay, "Toogle Play", "Play/Pause the video player");
     b.on(null, "ArrowRight", nextFrame, "Next", "go to the next frame");
     b.on(null, "ArrowLeft", previousFrame, "Previous", "go to the previous frame");
+    b.on(null, "V", nextMultipleFrames, "Next 10 Frames", "Go to the next 10 frames");
+    b.on(null, "C", previousMultipleFrames, "Previous 10 Frames", "Go to the Previous 10 frames");
     b.on([b.Ctrl, b.Alt], "ArrowRight", endFrame, "End", "go to the starting frame");
     b.on([b.Ctrl, b.Alt], "ArrowLeft", startFrame, "Start", "go to the ending frame");
-    b.on(null, "B", enterMode(IDAH_VIDEO_BOUNDING_BOX), "Bounding box", "Enter Bouding box mode");
+
     // test, need uniform way to map accross keyboards (-/+)
     b.on(null, "Equal", context.zoom.in, "Zoom in", "Zoom In");
     b.on(null, "Minus", context.zoom.out, "Zoom Out", "Zoom Out");
@@ -108,12 +121,24 @@ const createVisualModeKeyMap = (context: KeyMapContext) => {
   return keyMap;
 };
 
+// need note mode ?
+// open/close note sidebar on mode enter/leave ?
+const createNoteModeKeyMap = (context: KeyMapContext) => {
+  return KeyMapBuilder((b) => {
+    CommonInjecter(context)(b);
+
+    // shift+enter to submit current editing note ?
+  });
+};
+
 export function registerVisualModeShortcuts(context: KeyMapContext) {
   const visualModeKeyMap = createVisualModeKeyMap(context);
   const boundingBoxModeKeyMap = createBoundingBoxModeKeyMap(context);
+  const noteModeKeyMap = createNoteModeKeyMap(context);
 
   ShortcutManager.registerKeyMap(DEFAULT_MODE, visualModeKeyMap);
   ShortcutManager.registerKeyMap(IDAH_VIDEO_BOUNDING_BOX, boundingBoxModeKeyMap);
+  ShortcutManager.registerKeyMap(IDAH_NOTE, noteModeKeyMap);
 
   // Set visual as the default mode if needed
   ShortcutManager.defaultMode = DEFAULT_MODE;
