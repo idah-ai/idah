76c8bae6463d80a1b3c69cb722b51f4b52b07a65
Karnravee Chalermmuang <ptmwilfk@gmail.com>
2026-01-20 17:01:38 +0700

    feat: Implement a new sidebar navigation component and remove various documentation UI components.

---
+++ doc/public/src/components/app/Sidebar/Sidebar.data.ts
@@ -0,0 +1,40 @@
+type SidebarProps = {
+  label: string;
+  href?: string;
+  children: SidebarProps[];
+};
+
+export const sidebars: SidebarProps[] = [
+  {
+    label: "IDAH Documentation",
+    href: "/",
+    children: [],
+  },
+  {
+    label: "PROJECTS",
+    children: [
+      { label: "Projcts, and Organisation", href: "/projects/organisation", children: [] },
+      { label: "Create a project", href: "/projects/create", children: [] },
+      { label: "Rename a Project", href: "/projects/rename", children: [] },
+      { label: "Delete a Project", href: "/projects/delete", children: [] },
+      {
+        label: "Project Members",
+        href: "/projects/members",
+        children: [
+          { label: "Adding new Member", href: "/projects/members/add", children: [] },
+          { label: "Edit Member", href: "/projects/members/edit", children: [] },
+          { label: "Delete Member", href: "/projects/members/delete", children: [] },
+        ],
+      },
+    ],
+  },
+  {
+    label: "WORKFLOWS",
+    children: [
+      { label: "What is Workflows ?", href: "/workflows/what-is", children: [] },
+      { label: "Create a Workflow", href: "/workflows/create", children: [] },
+      { label: "Build a Workflow", href: "/workflows/build", children: [] },
+      { label: "Test a Workflow", href: "/workflows/test", children: [] },
+    ],
+  },
+];
