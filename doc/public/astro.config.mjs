018d5fc19c3340af155d9bb4e97ba9b947abf0d4
Karnravee Chalermmuang <ptmwilfk@gmail.com>
2026-01-20 14:29:51 +0700

    feat: add sidebar components and styles
    - Introduced `sidebar-rail`, `sidebar-separator`, and `sidebar-trigger` components for enhanced sidebar functionality.
    - Created `sidebar` component to manage sidebar layout and behavior.
    - Implemented new `skeleton` component for loading states.
    - Added table components including `table`, `table-body`, `table-caption`, `table-cell`, `table-footer`, `table-head`, `table-header`, and `table-row` for structured data representation.
    - Developed `tabs` components including `tabs`, `tabs-content`, `tabs-list`, and `tabs-trigger` for tabbed navigation.
    - Created `textarea` component for user input.
    - Implemented `tooltip` components for contextual information display.
    - Added utility functions for class name management and mobile detection.
    - Established global CSS styles for consistent theming and layout.
    - Updated TypeScript configuration for improved module resolution.

--- doc/public/astro.config.mjs
+++ doc/public/astro.config.mjs
@@ -3,29 +3,34 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
+import svelte from "@astrojs/svelte";
+
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: true,
-    }),
-    icon({
-      include: {
-        tabler: ["*"],
-        lucide: ["*"],
-        solar: ["*"],
-        "flat-color-icons": [
-          "template",
-          "gallery",
-          "approval",
-          "document",
-          "advertising",
-          "currency-exchange",
-          "voice-presentation",
-          "business-contact",
-          "database",
-        ],
-      },
-    }),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: true,
+  }), icon({
+    include: {
+      tabler: ["*"],
+      lucide: ["*"],
+      solar: ["*"],
+      "flat-color-icons": [
+        "template",
+        "gallery",
+        "approval",
+        "document",
+        "advertising",
+        "currency-exchange",
+        "voice-presentation",
+        "business-contact",
+        "database",
+      ],
+    },
+  }), svelte()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
