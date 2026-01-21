dcbd16113d263cc1295cfeac0b8e5a0589f035a9
Karnravee Chalermmuang <ptmwilfk@gmail.com>
2026-01-21 10:20:32 +0700

    feat: implement new sidebar component and restructure sidebar data

---
+++ doc/public/src/lib/app/sidebar/sidebar.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import AppSidebar from "$lib/app/sidebar/app-sidebar.svelte";
+  import { SidebarProvider, SidebarTrigger } from "$lib/components/ui/sidebar";
+</script>
+
+<SidebarProvider>
+  <AppSidebar>
+    <SidebarTrigger />
+  </AppSidebar>
+</SidebarProvider>
