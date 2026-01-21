8530ecfd2b45f755802e80f582f2bc7c5977dbdc
Karnravee Chalermmuang <ptmwilfk@gmail.com>
2026-01-21 13:47:02 +0700

    feat: Add snippet component and refactor sidebar integration into `PageLayout.astro` with improved scrolling behavior.

--- doc/public/src/lib/app/sidebar/sidebar.svelte
+++ doc/public/src/lib/app/sidebar/sidebar.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
   import AppSidebar from "$lib/app/sidebar/app-sidebar.svelte";
-  import { SidebarProvider, SidebarTrigger } from "$lib/components/ui/sidebar";
+  import { SidebarProvider } from "$lib/components/ui/sidebar";
 </script>
 
-<SidebarProvider class="hidden lg:block w-64 shrink-0 h-[calc(100vh-64px)]">
-  <AppSidebar>
-    <SidebarTrigger />
-  </AppSidebar>
-</SidebarProvider>
+<aside class="hidden lg:block w-64 shrink-0 border-r">
+  <SidebarProvider>
+    <AppSidebar />
+  </SidebarProvider>
+</aside>
