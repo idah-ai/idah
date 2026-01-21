6f9436d1b1eaf605905191a0ed57f4b21ef3574d
Karnravee Chalermmuang <ptmwilfk@gmail.com>
2026-01-21 10:20:32 +0700

    feat: implement new sidebar component and restructure sidebar data

---
+++ doc/public/src/lib/app/sidebar/app-sidebar.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import { sidebars } from "$lib/app/sidebar/sidebar.data";
+  import {
+    Sidebar,
+    SidebarContent,
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarGroupLabel,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+  } from "$lib/components/ui/sidebar";
+</script>
+
+<Sidebar>
+  <SidebarContent>
+    <SidebarGroup>
+      <SidebarGroupLabel>Application</SidebarGroupLabel>
+      <SidebarGroupContent>
+        <SidebarMenu>
+          {#each sidebars as sidebar}
+            <SidebarMenuItem key={sidebar.label}>
+              <SidebarMenuButton>
+                <a href={sidebar.href}>{sidebar.label}</a>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          {/each}
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
+  </SidebarContent>
+</Sidebar>
