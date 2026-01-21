97c7e0f9f95e551c5b261926a747ac706c482315
Karnravee Chalermmuang <ptmwilfk@gmail.com>
2026-01-21 16:00:46 +0700

    feat: add a generic Table Svelte component and integrate it into the project members add page.

---
+++ doc/public/src/lib/app/table/table.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  import TableBody from "$lib/components/ui/table/table-body.svelte";
+  import TableCell from "$lib/components/ui/table/table-cell.svelte";
+  import TableHead from "$lib/components/ui/table/table-head.svelte";
+  import TableHeader from "$lib/components/ui/table/table-header.svelte";
+  import TableRow from "$lib/components/ui/table/table-row.svelte";
+  import Table from "$lib/components/ui/table/table.svelte";
+
+  interface DataTable {
+    [key: string]: string;
+  }
+
+  interface TableProps {
+    dataTable: DataTable[];
+  }
+
+  let { dataTable }: TableProps = $props();
+</script>
+
+<div class="w-full">
+  <div class="overflow-hidden rounded-md border">
+    <Table>
+      {#each dataTable as data}
+        <TableHeader>
+          <TableRow class="*:border-border [&>:not(:last-child)]:border-r">
+            {#each Object.entries(data) as [key]}
+              <TableHead class="capitalize">
+                {key}
+              </TableHead>
+            {/each}
+          </TableRow>
+        </TableHeader>
+
+        <TableBody>
+          <TableRow class="*:border-border [&>:not(:last-child)]:border-r">
+            {#each Object.entries(data) as [key, value]}
+              <TableCell>
+                {value}
+              </TableCell>
+            {/each}
+          </TableRow>
+        </TableBody>
+      {/each}
+    </Table>
+  </div>
+</div>
