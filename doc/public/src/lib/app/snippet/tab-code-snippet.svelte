a1db5c6398be0d5613a172d1b4f2f9c8eb656b54
Karnravee Chalermmuang <ptmwilfk@gmail.com>
2026-01-21 15:16:23 +0700

    feat: Add a new Svelte component for tabbed code snippets and integrate it into the projects members add page.

---
+++ doc/public/src/lib/app/snippet/tab-code-snippet.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import CodeSnippet from "$lib/app/snippet/code-snippet.svelte";
+  import TabsContent from "$lib/components/ui/tabs/tabs-content.svelte";
+  import TabsList from "$lib/components/ui/tabs/tabs-list.svelte";
+  import TabsTrigger from "$lib/components/ui/tabs/tabs-trigger.svelte";
+  import Tabs from "$lib/components/ui/tabs/tabs.svelte";
+
+  interface TabCodeSnippetProps {
+    value: string;
+    code: string;
+  }
+
+  interface TabCodeSnippetsProps {
+    value: string;
+    items: TabCodeSnippetProps[];
+  }
+
+  let { value, items }: TabCodeSnippetsProps = $props();
+</script>
+
+<Tabs {value} class="w-[420px]">
+  <TabsList class="rounded-lg bg-muted p-1">
+    {#each items as item}
+      <TabsTrigger value={item.value} class="rounded-md px-3 text-sm">
+        {item.value}
+      </TabsTrigger>
+    {/each}
+  </TabsList>
+
+  {#each items as item}
+    <TabsContent value={item.value}>
+      <CodeSnippet code={item.code} />
+    </TabsContent>
+  {/each}
+</Tabs>
