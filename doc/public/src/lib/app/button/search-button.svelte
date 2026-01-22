f1d25050f3f6d460ad3d855826328063ceae6e7e
Karnravee Chalermmuang <ptmwilfk@gmail.com>
2026-01-22 11:05:25 +0700

    refactor: Extract search input and button into Svelte components and set sidebar to open by default.

---
+++ doc/public/src/lib/app/button/search-button.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import Button from "$lib/components/ui/button/button.svelte";
+  import { SearchIcon } from "@lucide/svelte";
+</script>
+
+<Button id="mobile-search" variant="ghost" size="icon" class="xl:hidden">
+  <SearchIcon class="size-5" />
+</Button>
