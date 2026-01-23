a8be278ed9d74bb43b1206f1a63f1e79fb3da2d4
Karnravee Chalermmuang <ptmwilfk@gmail.com>
2026-01-23 16:50:20 +0700

    feat: refactor search bar component to use store for search state management and improve layout

--- doc/public/src/lib/app/bar/search-bar.svelte
+++ doc/public/src/lib/app/bar/search-bar.svelte
@@ -1,19 +1,10 @@
 <script lang="ts">
+  import { closeSearch, openSearch, searchOpen } from "$lib/app/bar/search-bar.store";
+  import { sidebarOpen } from "$lib/app/sidebar/sidebar.store";
   import Button from "$lib/components/ui/button/button.svelte";
   import { InputGroup, InputGroupAddon, InputGroupInput } from "$lib/components/ui/input-group";
   import { cn } from "$lib/utils";
   import { SearchIcon, XIcon } from "@lucide/svelte";
-  import { writable } from "svelte/store";
-
-  const searchOpen = writable(false);
-
-  function openSearch(): void {
-    searchOpen.set(true);
-  }
-
-  function closeSearch(): void {
-    searchOpen.set(false);
-  }
 </script>
 
 {#snippet searchInput()}
@@ -32,8 +23,7 @@
   </InputGroup>
 {/snippet}
 
-<!-- <div class="relative flex w-full items-center"> -->
-<div class="flex gap-2 w-full items-center justify-center">
+<div class="flex gap-1 w-full items-center justify-center">
   <div class="w-full max-w-xl hidden items-center lg:block">
     {@render searchInput()}
   </div>
@@ -44,7 +34,11 @@
     </div>
   {/if}
 
-  <div class="ml-auto flex items-center gap-1 lg:hidden">
+  <div
+    class={cn("ml-auto flex items-center gap-1 lg:hidden", {
+      hidden: $sidebarOpen,
+    })}
+  >
     <Button
       class={cn("", {
         hidden: !$searchOpen,
