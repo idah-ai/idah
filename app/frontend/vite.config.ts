6efe91ffcf0f4b58e172920f834a2959bf8df20a
roger <rcserrano06@gmail.com>
2026-02-18 14:14:13 +0700

    Frontend: fix prettier check

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -2,36 +2,36 @@ import tailwindcss from "@tailwindcss/vite";
 import { svelteTesting } from "@testing-library/svelte/vite";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
-import path from "path"
+import path from "path";
 
 export default defineConfig({
   plugins: [
     {
-      name: 'watch-build-folder',
+      name: "watch-build-folder",
       configureServer(server) {
-        const buildPath = path.resolve(__dirname, 'build')
+        const buildPath = path.resolve(__dirname, "build");
 
         // Add folder to chokidar watcher
-        server.watcher.add(buildPath)
+        server.watcher.add(buildPath);
 
         // Trigger full reload when anything in build changes
-        server.watcher.on('change', (file) => {
+        server.watcher.on("change", (file) => {
           if (file.startsWith(buildPath)) {
-            console.log('Build file changed:', file)
-            server.ws.send({ type: 'full-reload' })
+            console.log("Build file changed:", file);
+            server.ws.send({ type: "full-reload" });
           }
-        })
-      }
+        });
+      },
     },
     tailwindcss(),
-    sveltekit()
+    sveltekit(),
   ],
   server: {
     watch: {
       // use polling if file system events don’t trigger (Docker, WSL, NFS)
       usePolling: true,
-      interval: 100
-    }
+      interval: 100,
+    },
   },
   test: {
     workspace: [
