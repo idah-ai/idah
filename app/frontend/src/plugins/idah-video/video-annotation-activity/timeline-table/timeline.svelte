6a4bffaa97bd39383782c2908dfb3fb0aab84ec6
whelsm <jiranop.phinyo@gmail.com>
2026-02-20 13:43:12 +0700

    chore: refactor timeline-cell.svelte and add functionality to toggle visibility, lock and delete all annotations in a group

--- app/frontend/src/plugins/idah-video/video-annotation-activity/timeline-table/timeline.svelte
+++ app/frontend/src/plugins/idah-video/video-annotation-activity/timeline-table/timeline.svelte
@@ -1,89 +1,77 @@
 <script lang="ts">
-
   import type {
-      AnnotationMetadata,
-      AnnotationObj,
-      AnnotationShape,
-      AnnotationValue,
+    AnnotationMetadata,
+    AnnotationObj,
+    AnnotationShape,
+    AnnotationValue,
   } from "@/context/AnnotationContext";
-  import { ENTRY_ROOT } from "../../type";
-  import AnnotationTimelineCell from "./annotation-timeline-cell.svelte";
+  import TimelineCell from "./timeline-cell.svelte";
+
+  type TAnnotationObj = AnnotationObj<AnnotationShape, AnnotationValue, AnnotationMetadata>;
 
   let {
+    groupId,
     annotations,
     currentFrame,
     range,
     scale,
     zoom,
     totalFrames,
-    hoveredColumn,
+    selectedAnnotation,
     onCellHover,
     onSeekFrame,
     onSelectAnnotation,
     onDeleteAnnotation,
-    ...restProps
   }: {
-    annotations: AnnotationObj<AnnotationShape, AnnotationValue, AnnotationMetadata>[];
+    groupId: string;
+    annotations: TAnnotationObj[];
     currentFrame: number;
     range: [number, number];
     scale: number;
     zoom: number;
     totalFrames: number;
-    hoveredColumn?: number;
+    selectedAnnotation?: TAnnotationObj;
     onCellHover: (column?: number) => void;
     onSeekFrame: (frame: number) => void;
-    onSelectAnnotation: (annotation: AnnotationObj<AnnotationShape, AnnotationValue, AnnotationMetadata>) => void;
-    onDeleteAnnotation: (
-      annotation: AnnotationObj<AnnotationShape, AnnotationValue, AnnotationMetadata>,
-      frame: number,
-    ) => void;
+    onSelectAnnotation: (annotation?: TAnnotationObj) => void;
+    onDeleteAnnotation: (annotation: TAnnotationObj, frame: number) => void;
   } = $props();
 
   // Variables
   let frameCells = $derived(Math.ceil((range[1] - range[0]) / scale) + 1);
- let range_span = $derived(Math.min(scale * zoom, totalFrames));
-  let cellWidth: number = $derived((1 / ((range[1] - range[0] + (scale - (range_span % scale))) / 100)) * scale);
+  let hoveredAnnotation: TAnnotationObj | undefined = $state(undefined);
 
   // Functions
-  function setHoveredColumn(column?: number) {
-    onCellHover(column);
+  function setHoveredCell(frame?: number) {
+    onCellHover(frame);
+  }
+
+  function setHoveredAnnotation(annotation?: TAnnotationObj) {
+    hoveredAnnotation = annotation;
   }
 </script>
 
 <div class="h-8">
   {#if frameCells > 0}
-  {#each Array(frameCells) as _u, i (i)}
-  {@const currentFrameInCell = range[0] + i * scale}
- 
-  
-  {#each annotations as annotation (annotation.metadata.id)}
-        <AnnotationTimelineCell
-          {annotation}
-          frame={currentFrameInCell}
-          {currentFrame}
-          {range}
-          {scale}
-          {zoom}
-          {totalFrames}
-          inSpan={annotation.shape.type == ENTRY_ROOT ||
-            (currentFrameInCell >= annotation.shape.start && currentFrameInCell <= annotation.shape.end)}
-          {onSeekFrame}
-          keyframes={(annotation.shape.frames || [])
-            .filter((s) => {
-              const start = range[0] + i * scale;
-              const end = start + scale;
-              return s.frame >= start && s.frame < end;
-            })
-            .map((s) => s.frame)}
-          {onSelectAnnotation}
-          onDeleteFrame={(frame) => onDeleteAnnotation(annotation, frame)}
-          hovered={hoveredColumn == currentFrameInCell}
-          onmouseover={() => setHoveredColumn(currentFrameInCell)}
-          onmouseenter={() => setHoveredColumn(currentFrameInCell)}
-          onmouseleave={() => setHoveredColumn(undefined)}
-          {...restProps}
-        />
-      {/each}
+    {#each Array(frameCells) as _u, i (i)}
+      {@const currentFrameInCell = range[0] + i * scale}
+
+      <TimelineCell
+        {groupId}
+        {annotations}
+        {currentFrameInCell}
+        {range}
+        {scale}
+        {totalFrames}
+        {zoom}
+        {selectedAnnotation}
+        {hoveredAnnotation}
+        {onSeekFrame}
+        onDeleteFrame={onDeleteAnnotation}
+        {onSelectAnnotation}
+        onHoverAnnotation={setHoveredAnnotation}
+        onHoverCell={setHoveredCell}
+      ></TimelineCell>
     {/each}
   {/if}
 </div>
