c20d6e8cfe776f558731fdddf4fa5f3a92310c31
whelsm <jiranop.phinyo@gmail.com>
2026-02-25 16:28:08 +0700

    chore: update sync export and job record, datasource-table, cell and overlays

--- app/frontend/src/lib/data/model/sync/exports/record.ts
+++ app/frontend/src/lib/data/model/sync/exports/record.ts
@@ -1,11 +1,24 @@
-import { createBackendDataSource } from "@/data/BackendDataSource";
+import { createBackendDataSource, resourcePath } from "@/data/BackendDataSource";
+import { clearCache } from "@/data/Cache";
 import { field, Record, RecordFactory, type } from "@/data/model/Record";
+import { parseSingleElementError, parseSingleElementReturn } from "@/data/model/json_api";
+import { Transformers } from "@/data/model/transformers";
+
+import type { ExportFormat } from "@/data/model/sync/exports/type";
+import type { Hash } from "@/utils/types";
 
 @type("sync:exports")
 export class ExportRecord extends Record {
-  @field() public filename!: string;
+  @field() public job_id!: string;
+  @field() public project_id!: string;
+  @field() public created_by_id!: number;
+
+  @field() public file_id!: string | null;
+  @field() public filename!: string | null;
+  @field() public mime_type!: string | null;
   @field() public size!: number;
-  @field() public mime_type!: string;
+
+  @field({ transformer: Transformers.Time }) public created_at!: Date;
 }
 
 RecordFactory.registerTypes(ExportRecord);
@@ -15,17 +28,48 @@ const exportsBasePath = `${import.meta.env.VITE_IDAH_HOST}/api/v1/sync/exports`;
 export function export_download_path(id: string) {
   return `${exportsBasePath}/${id}/download`;
 }
+
 export const ExportsBackendDataSource = createBackendDataSource(ExportRecord, exportsBasePath, {
-  export: async (filter) => {
-    await fetch(`${exportsBasePath}/export`, {
+  formats: async (params: { modalities: Array<string> }): Promise<Array<ExportFormat>> => {
+    const modalitiesQuery = params.modalities.map((modality) => `modalities[]=${modality}`).join("&");
+    const res = await fetch(`${exportsBasePath}/formats?${modalitiesQuery}`, {
+      method: "GET",
+    });
+
+    const body = await res.json();
+
+    return body as Array<ExportFormat>;
+  },
+  export: async (params: { projectId: string; datasetIds: Array<string>; exporter: string }) => {
+    const { projectId, datasetIds, exporter } = params;
+    const res = await fetch(`${exportsBasePath}/export`, {
       method: "POST",
       body: JSON.stringify({
-        filter: filter,
+        project_id: projectId,
+        dataset_ids: datasetIds,
+        exporter: exporter,
       }),
       headers: { "Content-Type": "application/vnd.api+json" },
     });
-  },
-  download: async (id) => {
-    await fetch(export_download_path(id));
+
+    const body = await res.json();
+
+    // Cache Management
+    const cacheIndexKey = resourcePath(exportsBasePath, null, undefined);
+    clearCache(cacheIndexKey);
+
+    if (body && body.errors) {
+      if (body.errors.length > 0) {
+        body.errors.forEach((err: Hash) => {
+          console.error(`Error assigning entry: ${err.title} - ${err.detail}`, err);
+        });
+      }
+
+      return Promise.reject(parseSingleElementError({ status: res.status, errors: body.errors }));
+    }
+
+    if (body.data) return Promise.resolve(parseSingleElementReturn<ExportRecord>(body));
+
+    throw "No data returned";
   },
 });
