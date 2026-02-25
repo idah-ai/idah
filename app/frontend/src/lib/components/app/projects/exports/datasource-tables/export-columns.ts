11d238731c42534e47c2b27607c1aeff89e54a52
whelsm <jiranop.phinyo@gmail.com>
2026-02-25 16:28:08 +0700

    chore: update sync export and job record, datasource-table, cell and overlays

--- app/frontend/src/lib/components/app/projects/exports/datasource-tables/export-columns.ts
+++ app/frontend/src/lib/components/app/projects/exports/datasource-tables/export-columns.ts
@@ -1,53 +1,32 @@
-import type { ColumnsSettings } from "@/components/app/datasource-table/types";
-import type { SyncJobRecord } from "@/data/model/sync/jobs/record";
-import ExportArgumentsCell from "./export-arguments-cell.svelte";
-import ExportDownloadCell from "./export-download-cell.svelte";
-import ExportProgressCell from "./export-progress-cell.svelte";
+import ExportRowActionCell from "@/components/app/projects/exports/datasource-tables/export-row-action-cell.svelte";
 
-export const exportsColumns: ColumnsSettings<SyncJobRecord> = {
-  progress: {
-    label: "progress",
-    dataType: "number",
+import { ExportRecord } from "@/data/model/sync/exports/record";
+
+import type { ColumnsSettings } from "@/components/app/datasource-table/types";
+
+export const exportsColumns: ColumnsSettings<ExportRecord> = {
+  id: {
+    label: "ID",
+    dataType: "string",
     clickable: false,
     sortable: true,
     filterable: true,
     filterOptions: {
-      filterKey: "progress",
-      filterBy: "number-range",
-      filterOperation: "eq",
+      filterKey: "id",
+      filterBy: "string",
+      filterOperation: "match",
     },
     visible: true,
     hidable: false,
-    cellComponent: ExportProgressCell,
   },
-  arguments: {
-    label: "Arguments",
+  action: {
+    label: "Action",
     dataType: "string",
     clickable: false,
     sortable: false,
     filterable: false,
-    // filterOptions: {
-    // filterKey: "progress",
-    // filterBy: "number-range",
-    // filterOperation: "eq",
-    // },
     visible: true,
     hidable: false,
-    cellComponent: ExportArgumentsCell,
-  },
-  action: {
-    label: "Action",
-    dataType: "file",
-    clickable: false,
-    sortable: false,
-    filterable: false,
-    // filterOptions: {
-    // filterKey: "progress",
-    // filterBy: "number-range",
-    // filterOperation: "eq",
-    // },
-    visible: true,
-    hidable: false,
-    cellComponent: ExportDownloadCell,
+    cellComponent: ExportRowActionCell,
   },
 };
--- app/frontend/src/lib/components/app/projects/exports/datasource-tables/export-row-action-cell.svelte
+++ app/frontend/src/lib/components/app/projects/exports/datasource-tables/export-row-action-cell.svelte
@@ -3,17 +3,17 @@
   import type { DataTableCellBaseProps } from "@/components/app/datasource-table/types";
   import Spinner from "@/components/ui/spinner/spinner.svelte";
   import Link from "@/components/ui/text/Link.svelte";
-  import { export_download_path, ExportRecord, ExportsBackendDataSource } from "@/data/model/sync/exports/record";
+  import { export_download_path, ExportRecord } from "@/data/model/sync/exports/record";
   import { SyncJobRecord, SyncJobsBackendDataSource } from "@/data/model/sync/jobs/record";
   import { onDestroy } from "svelte";
   import { writable } from "svelte/store";
 
-  let { record: syncJob }: DataTableCellBaseProps<SyncJobRecord> = $props();
+  let { record: exportRecord }: DataTableCellBaseProps<ExportRecord> = $props();
 
   let projectId = page.params.projectId as string;
   let progressInterval = writable<number | undefined>(undefined); // Note: Need to use writable because it's not reactive
-  let status = $state(syncJob.status);
-  let exports = $state(syncJob.exports);
+  let status = $state(exportRecord.status);
+  let exports = $state(exportRecord.exports);
 
   const processingStatuses = ["running", "pending"];
 
@@ -36,7 +36,7 @@
             return;
           }
 
-          const jobRes = await SyncJobsBackendDataSource.get(syncJob.id, {
+          const jobRes = await SyncJobsBackendDataSource.get(exportRecord.id, {
             included: ["exports"],
             fields: {
               [SyncJobRecord.type]: ["status", "exports.id"],
@@ -52,6 +52,7 @@
         }
       }, 2_000);
     } else {
+      //
     }
   }
 
@@ -72,7 +73,7 @@
     {#if processingStatuses.includes(status)}
       <Spinner />
     {:else}
-      {#each exports as xport}
+      {#each exports as xport (xport.id)}
         <Link href={export_download_path(xport.id)}>
           {xport.filename}
         </Link>
