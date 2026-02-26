3a18354f5e5750211b780db01111fef8db634014
roger <rcserrano06@gmail.com>
2026-02-26 11:28:00 +0700

    Sync: add filter datasets ids in exports create method

--- app/sync/app/service/exports/service.rb
+++ app/sync/app/service/exports/service.rb
@@ -27,9 +27,16 @@ module Exports
 
     # Create an export object + setup the job with it.
     def create(project_id, dataset_ids, exporter)
-      # FIXME: sanitize exporter class +
-      # dataset_ids based on the rights of this user
-      sanitize_export_class! exporter
+      # Ensure that the exporter class is valid
+      sanitize_export_class!(exporter)
+
+      # Filter the dataset IDs based on the user's rights and the project they belong to
+      dataset_ids = filter_dataset_ids_by_rights(project_id, dataset_ids)
+
+      if dataset_ids.empty?
+        raise Verse::Error::Authorization,
+              "You do not have access to any of the datasets provided"
+      end
 
       exports.transaction do
         job = jobs.create(
@@ -81,13 +88,71 @@ module Exports
       exports.find!(export.id)
     end
 
-    # Ensure that the class we are trying to invoke is a
-    # valid class (at least, it exists in the registry)
-    protected def sanitize_export_class!(export)
+    protected
+
+    def sanitize_export_class!(export)
       return if Exports::Registry.valid_export_class?(export)
 
       raise Verse::Error::Authorization,
             "invalid export format: `#{export}`"
+    end
+
+    def filter_dataset_ids_by_rights(project_id, dataset_ids)
+      # Get the list of dataset ids that the user has access to
+      accessible_dataset_ids =
+        auth_context.can!(:read, Resource::Sync::Exports) do |scope|
+          scope.all? { dataset_ids }
+          scope.as_org_owner? {
+            org_ids = auth_context.custom_scopes[:org]
+
+            # Ensure that the project belongs to the organization the user has access to
+            project_ids = Api[:idah].dataset.projects.index_all(
+              filter: { id: project_id, organization_id: org_ids },
+              fields: { "dataset:projects": ["id"] }
+            ).map(&:id)
+
+            if project_ids.empty?
+              raise Verse::Error::Authorization,
+                    "You do not have access to the project provided"
+            end
+
+            # Ensure that datasets belong to the project and organization the user has access to
+            Api[:idah].dataset.datasets.index_all(
+              filter: { project_id:, id: dataset_ids },
+              fields: { "dataset:dataset": ["id"] }
+            ).map(&:id)
+          }
+          scope.as_user? {
+            account_id = auth_context.metadata[:id]
+
+            # Ensure that the user is a project owner of the project
+            project_ids = Api[:idah].dataset.project_members.index_all(
+              filter: { project_id:, account_id:, role: "project_owner", enabled: true },
+              fields: { "dataset:project_members": ["project_id"] }
+            ).map(&:project_id).uniq
+
+            if project_ids.empty?
+              raise Verse::Error::Authorization,
+                    "You do not have access to the project provided"
+            end
+
+            # Ensure that datasets belong to the projects the user has access to
+            project_dataset_ids = Api[:idah].dataset.datasets.index_all(
+              filter: { project_id: project_ids, id: dataset_ids },
+              fields: { "dataset:dataset": ["id"] }
+            ).map(&:id)
+
+            if project_dataset_ids.empty?
+              raise Verse::Error::Authorization,
+                    "Dataset IDs provided do not belong to the project"
+            end
+
+            project_dataset_ids
+          }
+        end
+
+      # Filter the dataset_ids based on the accessible ones
+      dataset_ids.select { |id| accessible_dataset_ids.include?(id) }
     end
   end
 end
--- app/sync/app/service/exports/service_spec.rb
+++ app/sync/app/service/exports/service_spec.rb
@@ -11,39 +11,432 @@ RSpec.describe Exports::Service, database: true do
   let(:job_id) { "019bba87-650e-74e8-a68a-bc49bfff2527" }
 
   describe "#create" do
+    let(:dataset_ids) { [1, 2, 3] }
+    let(:exporter) { "MyExporter" }
+    let(:account_id) { 1 }
+
     subject {
-      auth_context.metadata[:id] = 1 # Simulate a user with ID 1
+      auth_context.metadata[:id] = account_id
       described_class.new(auth_context)
     }
 
     before do
       # By default, always allow export class
       allow(Exports::Registry).to receive(:valid_export_class?).and_return(true)
+
+      # By default, pass through all dataset IDs (no filtering)
+      allow_any_instance_of(described_class).to receive(:filter_dataset_ids_by_rights)
+        .and_call_original
     end
 
-    it "creates an export and a job" do
-      export = subject.create(project_id, [1, 2, 3], "MyExporter")
+    context "with valid exporter class" do
+      before do
+        # Stub the filtering to return all dataset IDs for these basic tests
+        allow_any_instance_of(described_class).to receive(:filter_dataset_ids_by_rights)
+          .with(project_id, dataset_ids).and_return(dataset_ids)
+      end
 
-      expect(export).not_to be_nil
-      expect(export.job_id).not_to be_nil
+      it "creates an export and a job" do
+        export = subject.create(project_id, dataset_ids, exporter)
 
-      job = Jobs::Service.new(auth_context).show(export.job_id)
+        expect(export).not_to be_nil
+        expect(export.job_id).not_to be_nil
+        expect(export.project_id).to eq(project_id)
+        expect(export.created_by_id).to eq(account_id)
 
-      expect(job.job_class).to eq "Exports::Job"
-      expect(job.arguments).to eq(
+        job = Jobs::Service.new(auth_context).show(export.job_id)
+
+        expect(job.job_class).to eq "Exports::Job"
+        expect(job.arguments).to eq(
+          {
+            dataset_ids:,
+            exporter:
+          }
+        )
+      end
+
+      it "creates records within a transaction" do
+        # The transaction is called on the repo's table's db, not directly on the repo
+        expect_any_instance_of(Sequel::Postgres::Database).to receive(:transaction).at_least(:once).and_call_original
+
+        export = subject.create(project_id, dataset_ids, exporter)
+        expect(export).not_to be_nil
+      end
+
+      it "stores the created_by_id from auth context" do
+        export = subject.create(project_id, dataset_ids, exporter)
+
+        expect(export.created_by_id).to eq(account_id)
+      end
+
+      it "validates the exporter class before creating records" do
+        expect(Exports::Registry).to receive(:valid_export_class?).with(exporter).and_return(true)
+
+        subject.create(project_id, dataset_ids, exporter)
+      end
+    end
+
+    context "with invalid exporter class" do
+      it "raises an error" do
+        allow(Exports::Registry).to receive(:valid_export_class?).with("bad_class").and_return(false)
+
+        expect {
+          subject.create(project_id, dataset_ids, "bad_class")
+        }.to raise_error(Verse::Error::Authorization, "invalid export format: `bad_class`")
+      end
+
+      it "does not create an export or job" do
+        allow(Exports::Registry).to receive(:valid_export_class?).with("bad_class").and_return(false)
+
+        initial_export_count = service.index.to_a.size
+        initial_job_count = Jobs::Service.new(auth_context).index.to_a.size
+
+        expect {
+          subject.create(project_id, dataset_ids, "bad_class")
+        }.to raise_error(Verse::Error::Authorization)
+
+        expect(service.index.to_a.size).to eq(initial_export_count)
+        expect(Jobs::Service.new(auth_context).index.to_a.size).to eq(initial_job_count)
+      end
+    end
+
+    context "dataset filtering by rights" do
+      context "when user has access to all  datasets" do
+        before do
+          allow_any_instance_of(described_class).to receive(:filter_dataset_ids_by_rights)
+            .with(project_id, dataset_ids).and_return(dataset_ids)
+        end
+
+        it "creates export with all dataset IDs" do
+          export = subject.create(project_id, dataset_ids, exporter)
+
+          job = Jobs::Service.new(auth_context).show(export.job_id)
+          expect(job.arguments[:dataset_ids]).to eq(dataset_ids)
+        end
+      end
+
+      context "when user has access to only some datasets" do
+        let(:filtered_ids) { [1, 2] }
+
+        before do
+          allow_any_instance_of(described_class).to receive(:filter_dataset_ids_by_rights)
+            .with(project_id, dataset_ids).and_return(filtered_ids)
+        end
+
+        it "creates export with only accessible dataset IDs" do
+          export = subject.create(project_id, dataset_ids, exporter)
+
+          job = Jobs::Service.new(auth_context).show(export.job_id)
+          expect(job.arguments[:dataset_ids]).to eq(filtered_ids)
+        end
+      end
+
+      context "when user has no access to any datasets" do
+        before do
+          allow_any_instance_of(described_class).to receive(:filter_dataset_ids_by_rights)
+            .with(project_id, dataset_ids).and_return([])
+        end
+
+        it "raises authorization error" do
+          expect {
+            subject.create(project_id, dataset_ids, exporter)
+          }.to raise_error(Verse::Error::Authorization, "You do not have access to any of the datasets provided")
+        end
+
+        it "does not create an export or job when no datasets are accessible" do
+          initial_export_count = service.index.to_a.size
+          initial_job_count = Jobs::Service.new(auth_context).index.to_a.size
+
+          expect {
+            subject.create(project_id, dataset_ids, exporter)
+          }.to raise_error(Verse::Error::Authorization)
+
+          expect(service.index.to_a.size).to eq(initial_export_count)
+          expect(Jobs::Service.new(auth_context).index.to_a.size).to eq(initial_job_count)
+        end
+      end
+    end
+
+    context "authorization flow" do
+      it "calls filter_dataset_ids_by_rights with correct parameters" do
+        allow_any_instance_of(described_class).to receive(:filter_dataset_ids_by_rights)
+          .with(project_id, dataset_ids).and_return(dataset_ids)
+
+        expect_any_instance_of(described_class).to receive(:filter_dataset_ids_by_rights)
+          .with(project_id, dataset_ids)
+
+        subject.create(project_id, dataset_ids, exporter)
+      end
+
+      it "validates exporter class before checking dataset rights" do
+        call_order = []
+
+        allow(Exports::Registry).to receive(:valid_export_class?) do |_arg|
+          call_order << :validate_exporter
+          true
+        end
+
+        allow_any_instance_of(described_class).to receive(:filter_dataset_ids_by_rights) do |*_args|
+          call_order << :filter_datasets
+          dataset_ids
+        end
+
+        subject.create(project_id, dataset_ids, exporter)
+
+        expect(call_order).to eq([:validate_exporter, :filter_datasets])
+      end
+    end
+
+    context "with empty dataset_ids array" do
+      let(:empty_dataset_ids) { [] }
+
+      before do
+        allow_any_instance_of(described_class).to receive(:filter_dataset_ids_by_rights)
+          .with(project_id, empty_dataset_ids).and_return([])
+      end
+
+      it "raises authorization error" do
+        expect {
+          subject.create(project_id, empty_dataset_ids, exporter)
+        }.to raise_error(Verse::Error::Authorization, "You do not have access to any of the datasets provided")
+      end
+    end
+  end
+
+  describe "#upload" do
+    let!(:export_id) do
+      repo.create(
         {
-          dataset_ids: [1, 2, 3],
-          exporter: "MyExporter"
+          job_id:,
+          project_id:,
+          created_by_id: 1
         }
       )
     end
 
-    it "raises an error if invalid export class" do
-      allow(Exports::Registry).to receive(:valid_export_class?).with("bad_class").and_return(false)
+    let(:file_path) { "/tmp/test_export.csv" }
+    let(:file_content) { "id,name,value\n1,test,100" }
+    let(:mock_file) do
+      double(
+        "File",
+        path: file_path,
+        read: file_content,
+        size: file_content.bytesize,
+        rewind: true
+      )
+    end
 
-      expect {
-        subject.create(project_id, [1, 2, 3], "bad_class")
-      }.to raise_error(Verse::Error::Authorization, "invalid export format: `bad_class`")
+    let(:uploaded_file) do
+      double(
+        "UploadedFile",
+        id: "uploaded_file_123",
+        size: 1024,
+        mime_type: "text/csv"
+      )
+    end
+
+    let(:mock_storage) do
+      double("Storage")
+    end
+
+    before do
+      allow(File).to receive(:basename).with(file_path).and_return("test_export.csv")
+      allow(Verse::Plugin).to receive(:[]).and_call_original
+    end
+
+    context "with valid file and export" do
+      before do
+        allow(Verse::Plugin[:shrine]).to receive(:with_storage).and_yield(mock_storage)
+        allow(mock_storage).to receive(:upload).with(mock_file).and_return(uploaded_file)
+        allow(repo.table.db).to receive(:after_rollback)
+      end
+
+      it "uploads the file to storage" do
+        expect(mock_storage).to receive(:upload).with(mock_file).and_return(uploaded_file)
+
+        service.upload(export_id, mock_file)
+      end
+
+      it "updates the export with file metadata" do
+        service.upload(export_id, mock_file)
+
+        updated_export = repo.find!(export_id)
+        expect(updated_export.file_id).to eq("uploaded_file_123")
+        expect(updated_export.filename).to eq("test_export.csv")
+        expect(updated_export.size).to eq(1024)
+        expect(updated_export.mime_type).to eq("text/csv")
+      end
+
+      it "returns the updated export" do
+        result = service.upload(export_id, mock_file)
+
+        expect(result).not_to be_nil
+        expect(result.id.to_s).to eq(export_id)
+        expect(result.file_id).to eq("uploaded_file_123")
+        expect(result.filename).to eq("test_export.csv")
+      end
+
+      it "extracts filename from file path" do
+        expect(File).to receive(:basename).with(file_path).and_return("test_export.csv")
+
+        service.upload(export_id, mock_file)
+      end
+
+      it "registers rollback callback for file cleanup" do
+        expect(repo.table.db).to receive(:after_rollback)
+
+        service.upload(export_id, mock_file)
+      end
+    end
+
+    context "with different file types" do
+      let(:json_file) do
+        double(
+          "File",
+          path: "/tmp/export.json",
+          read: '{"data": []}',
+          size: 13,
+          rewind: true
+        )
+      end
+
+      let(:json_uploaded_file) do
+        double(
+          "UploadedFile",
+          id: "json_file_456",
+          size: 256,
+          mime_type: "application/json"
+        )
+      end
+
+      before do
+        allow(File).to receive(:basename).with("/tmp/export.json").and_return("export.json")
+        allow(Verse::Plugin[:shrine]).to receive(:with_storage).and_yield(mock_storage)
+        allow(mock_storage).to receive(:upload).with(json_file).and_return(json_uploaded_file)
+        allow(repo.table.db).to receive(:after_rollback)
+      end
+
+      it "handles JSON files correctly" do
+        service.upload(export_id, json_file)
+
+        updated_export = repo.find!(export_id)
+        expect(updated_export.filename).to eq("export.json")
+        expect(updated_export.mime_type).to eq("application/json")
+      end
+    end
+
+    context "rollback behavior" do
+      before do
+        allow(Verse::Plugin[:shrine]).to receive(:with_storage).and_yield(mock_storage)
+        allow(mock_storage).to receive(:upload).with(mock_file).and_return(uploaded_file)
+      end
+
+      it "registers callback to delete uploaded file on rollback" do
+        rollback_block = nil
+        allow(repo.table.db).to receive(:after_rollback) do |&block|
+          rollback_block = block
+        end
+
+        service.upload(export_id, mock_file)
+
+        expect(rollback_block).not_to be_nil
+
+        # Simulate rollback
+        expect(mock_storage).to receive(:delete).with("uploaded_file_123")
+        allow(Verse.logger).to receive(:warn)
+
+        rollback_block.call
+      end
+
+      it "logs warning message on rollback" do
+        rollback_block = nil
+        allow(repo.table.db).to receive(:after_rollback) do |&block|
+          rollback_block = block
+        end
+
+        service.upload(export_id, mock_file)
+
+        allow(mock_storage).to receive(:delete)
+        expect(Verse.logger).to receive(:warn)
+
+        rollback_block.call
+      end
+    end
+
+    context "when export does not exist" do
+      it "raises RecordNotFound error" do
+        expect {
+          service.upload(99_999, mock_file)
+        }.to raise_error(Verse::Error::RecordNotFound)
+      end
+
+      it "does not upload file if export not found" do
+        expect(mock_storage).not_to receive(:upload)
+
+        expect {
+          service.upload(99_999, mock_file)
+        }.to raise_error(Verse::Error::RecordNotFound)
+      end
+    end
+
+    context "when file upload fails" do
+      before do
+        allow(Verse::Plugin[:shrine]).to receive(:with_storage).and_yield(mock_storage)
+        allow(mock_storage).to receive(:upload).and_raise(StandardError.new("Upload failed"))
+      end
+
+      it "raises error and does not update export" do
+        expect {
+          service.upload(export_id, mock_file)
+        }.to raise_error(StandardError, "Upload failed")
+
+        # Verify export was not updated
+        export = repo.find!(export_id)
+        expect(export.file_id).to be_nil
+      end
+    end
+
+    context "updating existing file" do
+      let(:new_file) do
+        double(
+          "File",
+          path: "/tmp/new_export.csv",
+          read: "updated content",
+          size: 15,
+          rewind: true
+        )
+      end
+
+      let(:new_uploaded_file) do
+        double(
+          "UploadedFile",
+          id: "new_file_789",
+          size: 2048,
+          mime_type: "text/csv"
+        )
+      end
+
+      before do
+        # First upload
+        allow(File).to receive(:basename).with(file_path).and_return("test_export.csv")
+        allow(Verse::Plugin[:shrine]).to receive(:with_storage).and_yield(mock_storage)
+        allow(mock_storage).to receive(:upload).with(mock_file).and_return(uploaded_file)
+        allow(repo.table.db).to receive(:after_rollback)
+        service.upload(export_id, mock_file)
+
+        # Setup for second upload
+        allow(File).to receive(:basename).with("/tmp/new_export.csv").and_return("new_export.csv")
+        allow(mock_storage).to receive(:upload).with(new_file).and_return(new_uploaded_file)
+      end
+
+      it "replaces existing file metadata" do
+        service.upload(export_id, new_file)
+
+        updated_export = repo.find!(export_id)
+        expect(updated_export.file_id).to eq("new_file_789")
+        expect(updated_export.filename).to eq("new_export.csv")
+        expect(updated_export.size).to eq(2048)
+      end
     end
   end
 
