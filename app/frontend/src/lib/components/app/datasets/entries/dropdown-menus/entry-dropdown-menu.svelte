b22218a80f9f2bef47026a7dbc046ba1787b803e
whelsm <jiranop.phinyo@gmail.com>
2026-02-27 10:21:45 +0700

    chore: remove export feature on entry

--- app/frontend/src/lib/components/app/datasets/entries/dropdown-menus/entry-dropdown-menu.svelte
+++ app/frontend/src/lib/components/app/datasets/entries/dropdown-menus/entry-dropdown-menu.svelte
@@ -40,18 +40,13 @@
     onDelete: () => {
       openConfirmDeleteEntryModal = true;
     },
-    onExport: () => {
-      openConfirmExportEntryModal = true;
-    },
   }).filter((m) => m.label !== "Set Priority");
 
   let currentAccount = $authStatus.authContext;
   let canUpdateEntry = $state(false);
   let canDeleteEntry = $state(false);
-  let canExportEntry = $state(false);
   let openAssignEntryFormModal: boolean = $state(false);
   let openConfirmDeleteEntryModal: boolean = $state(false);
-  let openConfirmExportEntryModal: boolean = $state(false);
 
   // Lifecycle
   onMount(async () => {
@@ -66,7 +61,6 @@
       (await currentAccount?.can("update", "dataset:entries", ["as_org_owner", as_project_owner])) || false;
     canDeleteEntry =
       (await currentAccount?.can("delete", "dataset:entries", ["as_org_owner", as_project_owner])) || false;
-    canExportEntry = (await currentAccount?.can("create", "sync:exports", ["as_org_owner", as_project_owner])) || false;
   });
 
   // Functions
@@ -93,8 +87,6 @@
       showActionFailedToast(error);
     }
   }
-
-  async function exportEntry() {}
 </script>
 
 {#if canUpdateEntry || canDeleteEntry}
@@ -128,11 +120,5 @@
     description="Are you sure you want to delete this entry?"
     onConfirm={deleteEntry}
     bind:open={openConfirmDeleteEntryModal}
-  />
-  <ConfirmModal
-    title="Export entry"
-    description="Are you sure you want to export this entry?"
-    onConfirm={exportEntry}
-    bind:open={openConfirmExportEntryModal}
   />
 {/if}
