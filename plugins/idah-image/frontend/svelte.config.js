6254b01815af90fde5cb07bc62a85ddf581116f2
Karnravee Chalermmuang <ptmwilfk@gmail.com>
2026-03-04 15:22:55 +0700

    refactor(svelte.config): remove unused path alias configuration

--- plugins/idah-image/frontend/svelte.config.js
+++ plugins/idah-image/frontend/svelte.config.js
@@ -1,15 +1,8 @@
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-import path from "path";
-
 
 /** @type {import('@sveltejs/vite-plugin-svelte').Options} */
 const config = {
   preprocess: vitePreprocess(),
-  resolve: {
-  alias: {
-    "$lib": path.resolve(__dirname, "src/lib"),
-  }
-},
 };
 
 export default config;
