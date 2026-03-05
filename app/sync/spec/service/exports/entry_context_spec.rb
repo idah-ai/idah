f66dbbd6c5548d5c5d887b610ab322bd7a69f602
roger <rcserrano06@gmail.com>
2026-03-05 14:33:24 +0700

    Sync: add specs for entry, annotaton and media contexts

--- app/sync/spec/service/exports/entry_context_spec.rb
+++ app/sync/spec/service/exports/entry_context_spec.rb
@@ -2,7 +2,10 @@
 
 require "spec_helper"
 require_relative "../../../app/service/exports/entry_context"
+require_relative "../../../app/service/exports/annotation_context"
+require_relative "../../../app/service/exports/media_context"
 
+# Register the endpoints for the spec
 Api[:idah].register(
   :dataset, :annotations, :index_all
 ) do |params = {}|
@@ -16,19 +19,46 @@ Api[:idah].register(
   end
 end
 
-RSpec.describe Exports::EntryContext do
-  let(:entry_id) { "019b2ad6-017f-7c32-aa1d-082f54fca92d" }
-  let(:annotations_json) { File.read("app/sync/app/spec_data/api_data/annotations.json") }
+Api[:idah].register(
+  :media, :medias, :index_all
+) do |params = {}|
+  Api.all(params) do |p|
+    output = get(
+      "media/medias",
+      params: p,
+      options: { auth: :bearer }
+    )
+    deserialize(output.body)
+  end
+end
 
-  subject { described_class.new(entry_id) }
+RSpec.describe Exports::EntryContext do
+  let(:entry_id) { "019bc0fa-025e-7a8c-a3f4-82b276508315" }
+  let(:entry_resource) { "4c2052a1475842e9.mov" }
+  let(:entry) do
+    double(
+      "Entry",
+      id: entry_id,
+      resource: entry_resource
+    )
+  end
+  let(:annotations_json) { File.read("app/spec_data/api_data/annotations.json") }
+  let(:medias_json) { File.read("app/spec_data/api_data/medias.json") }
+
+  subject { described_class.new(entry) }
 
   before do
-    Api[:idah].base_url = "http://idah.test/api/v1"
+    Api[:idah].base_url = "http://idah.test"
 
-    stub_request(:post, "http://idah.test/api/v1/iam/auth/login").
+    # Mock the login request to get a token
+    stub_request(:post, "http://idah.test/iam/auth/login").
+      with(
+        body: { "email" => "sync@services.idah.ai", "password" => "password" }
+      ).
       to_return(
         status: 200,
         body: JSON.generate(
+          data: {},
           meta: {
             token: "test.eyJleHAiOjE3ODA3MzMyODB9.test"
           }
@@ -37,9 +67,18 @@ RSpec.describe Exports::EntryContext do
       )
   end
 
+  describe "#entry" do
+    it "returns the entry object" do
+      expect(subject.entry).to eq(entry)
+    end
+  end
+
   describe "#annotations" do
-    it "returns an array of annotations" do
-      stub_request(:get, "http://idah.test/api/v1/dataset/annotations?filter[entry_id]=#{entry_id}&included=project_members&page[number]=1&page[size]=1000").
+    it "returns an array of AnnotationContext objects" do
+      stub_request(
+        :get,
+        "http://idah.test/dataset/annotations?filter%5Bentry_id%5D=#{entry_id}&included%5B%5D=project_members&page%5Bnumber%5D=1&page%5Bsize%5D=1000"
+      ).
         to_return(
           status: 200,
           body: annotations_json,
@@ -49,8 +88,28 @@ RSpec.describe Exports::EntryContext do
       annotations = subject.annotations
       expect(annotations).to be_an(Array)
       expect(annotations.size).to eq(1)
-      expect(annotations.first).to be_a(Verse::JsonApi::Record)
-      expect(annotations.first.id).to eq("019b2aec-94ff-7b50-bb44-8a3675e266f3")
+      expect(annotations.first).to be_a(Exports::AnnotationContext)
+      expect(annotations.first.annotation.id).to eq("019b2aec-94ff-7b50-bb44-8a3675e266f3")
+    end
+  end
+
+  describe "#medias" do
+    it "returns an array of MediaContext objects" do
+      stub_request(
+        :get,
+        "http://idah.test/media/medias?filter%5Bresource%5D=#{entry_resource}&page%5Bnumber%5D=1&page%5Bsize%5D=1000"
+      ).
+        to_return(
+          status: 200,
+          body: medias_json,
+          headers: { 'Content-Type': "application/json" }
+        )
+
+      medias = subject.medias
+      expect(medias).to be_an(Array)
+      expect(medias.size).to eq(11)
+      expect(medias.first).to be_a(Exports::MediaContext)
+      expect(medias.first.media.id).to eq("004f2b1c21bf42e0efe8b709a688afce.mov")
     end
   end
 end
