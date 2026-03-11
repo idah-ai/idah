3dc412edda23c3ec4381509e23461525e380d74b
roger <rcserrano06@gmail.com>
2026-03-11 15:06:59 +0700

    Plugin: add service class to plugin module

--- plugins/idah-video/backends/media/idah_video.rb
+++ plugins/idah-video/backends/media/idah_video.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 module IdahVideo
-  def self.init(context)
-    context.register_processor(
-      "idah-video",
-      class_name: "IdahVideo::Processor::Video",
-      options_class_name: "IdahVideo::Processor::Options"
-    )
+  class Media
+    def self.init(context)
+      context.register_processor(
+        "idah-video",
+        class_name: "IdahVideo::Processor::Video",
+        options_class_name: "IdahVideo::Processor::Options"
+      )
+    end
   end
 end
