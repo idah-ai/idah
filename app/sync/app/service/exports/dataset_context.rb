a49c2cbcd8632000d6e53ef08f476770ff389ec5
roger <rcserrano06@gmail.com>
2026-03-13 15:46:05 +0700

    Sync: rename all export context to record

--- app/sync/app/service/exports/dataset_context.rb
+++ app/sync/app/service/exports/dataset_context.rb
@@ -2,15 +2,15 @@
 
 module Exports
   class DatasetContext
-    attr_reader :dataset
+    attr_reader :record
 
     def initialize(id)
-      @dataset = Api[:idah].dataset.datasets.show(id:)
+      @record = Api[:idah].dataset.datasets.show(id:)
     end
 
     def entries(filter = {})
       Api[:idah].dataset.entries.index_all(
-        filter: filter.merge(dataset_id: @dataset.id),
+        filter: filter.merge(dataset_id: @record.id),
         included: []
       ).map do |entry|
         EntryContext.new(entry)
