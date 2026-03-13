a9dbe4a1ef6bb61b9cfad5209b6d32318ba02f89
whelsm <jiranop.phinyo@gmail.com>
2026-03-13 11:38:49 +0700

    wip: remove timeline-cell, add timeline vertical line

--- app/frontend/src/routes/timeline/components/timeline.svelte
+++ app/frontend/src/routes/timeline/components/timeline.svelte
@@ -21,10 +21,25 @@
   let { timelineHeight, timelineCellWidth }: Props = $props();
 
   // Variables
+  let timeline: HTMLDivElement;
+  let x: number = $state(0);
+  let showLine: boolean = $state(false);
   let annotationGroups = groupAnnotations(annotations);
+
+  // Functions
+  function handleMouseMove(event: MouseEvent) {
+    const rect = timeline.getBoundingClientRect();
+    x = event.clientX - rect.left;
+  }
 </script>
 
-<div id="timeline" class="w-full max-w-screen">
+<div
+  id="timeline"
+  role="document"
+  bind:this={timeline}
+  class="relative w-full max-w-screen"
+  onmousemove={handleMouseMove}
+>
   <TimelineHeaderRow>
     <TimelineRowHeader>
       <TimelineRowHeading class="font-semibold">Annotations</TimelineRowHeading>
@@ -38,7 +53,7 @@
   <ScrollArea>
     <div style:height="{timelineHeight - 90}px">
       {#each annotationGroups as annotationGroup (annotationGroup.groupId)}
-        <TimelineRow>
+        <TimelineRow onmouseenter={() => (showLine = true)} onmouseleave={() => (showLine = false)}>
           <TimelineRowGroup class="border-b" {annotationGroup} {timelineCellWidth}>
             <TimelineRowHeader>
               <TimelineRowHeading>
@@ -53,4 +68,21 @@
       {/each}
     </div>
   </ScrollArea>
+
+  <!-- Draw a vertical line when mouse move -->
+  {#if showLine}
+    <div class="cursor-line" style="transform: translateX({x}px)"></div>
+  {/if}
 </div>
+
+<style>
+  .cursor-line {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    width: 1px;
+    background: black;
+    pointer-events: none;
+    z-index: 9999;
+  }
+</style>
