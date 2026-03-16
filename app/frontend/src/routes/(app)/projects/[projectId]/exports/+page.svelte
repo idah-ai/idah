f836107ba06fe741d3fa75cda6f4e8588db8c03f
whelsm <jiranop.phinyo@gmail.com>
2026-03-16 15:28:17 +0700

    chore: add format, included medias and status column on dataset export datasource-table

--- app/frontend/src/routes/(app)/projects/[projectId]/exports/+page.svelte
+++ app/frontend/src/routes/(app)/projects/[projectId]/exports/+page.svelte
@@ -55,14 +55,17 @@
 
     const datasetsRes = await datasetsBackendDataSource.list({
       fields: {
-        [DatasetRecord.type]: ["id", "name"],
+        [DatasetRecord.type]: ["id", "name", "modality"],
       },
       filters: {
         id: datasetIds,
       },
     });
 
-    return { datasets: datasetsRes.data };
+    const modalities = Array.from(new Set(datasetsRes.data.map((dataset) => dataset.modality)));
+    const exportFormats = await ExportsBackendDataSource.formats({ modalities });
+
+    return { datasets: datasetsRes.data, exportFormats };
   }
 </script>
 
