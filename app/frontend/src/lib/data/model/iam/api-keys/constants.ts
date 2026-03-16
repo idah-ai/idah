c2f26e8f9d185b254c033f20d310f2a532d10cfe
Karnravee Chalermmuang <ptmwilfk@gmail.com>
2026-03-16 15:20:53 +0700

    fix: update API key form to use correct permissions and clean up imports

--- app/frontend/src/lib/data/model/iam/api-keys/constants.ts
+++ app/frontend/src/lib/data/model/iam/api-keys/constants.ts
@@ -1,7 +1,12 @@
 import type { LabelValue } from "@/utils/types";
 
-export const scopeTypes: LabelValue<string | number>[] = [
+export const scopeTypes: LabelValue<string>[] = [
   { label: "All", value: "all" },
   { label: "Organization", value: "organization" },
   { label: "Project", value: "project" },
+];
+
+export const apiKeyPermissions: LabelValue<string>[] = [
+  { label: "Organization Read-Only", value: "read" },
+  { label: "Organization Read-Write", value: "write" },
 ];
