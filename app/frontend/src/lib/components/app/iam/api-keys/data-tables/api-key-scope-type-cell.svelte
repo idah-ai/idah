2ecb0363df302610ff5f6b87439ecf10a9d7297a
Karnravee Chalermmuang <ptmwilfk@gmail.com>
2026-03-16 16:15:07 +0700

    feat: add Copyable component to display API key and Badge for scope type in data tables

--- app/frontend/src/lib/components/app/iam/api-keys/data-tables/api-key-scope-type-cell.svelte
+++ app/frontend/src/lib/components/app/iam/api-keys/data-tables/api-key-scope-type-cell.svelte
@@ -1,8 +1,17 @@
 <script lang="ts">
+  import Badge from "@/components/ui/badge/badge.svelte";
+
   import { ApiKeyRecord } from "@/data/model/iam/api-keys/record";
 
   import type { DataTableCellBaseProps } from "@/components/app/datasource-table/types";
 
   // Props
   let { record: apiKeyRecord }: DataTableCellBaseProps<ApiKeyRecord> = $props();
+
+  // Variables
+  let { scope_type } = $derived(apiKeyRecord);
 </script>
+
+<Badge>
+  {scope_type}
+</Badge>
