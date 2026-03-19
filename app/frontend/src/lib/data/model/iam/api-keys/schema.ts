5222b8fd25db3358b44ccd20f5a04700ef8f6d06
Karnravee Chalermmuang <ptmwilfk@gmail.com>
2026-03-19 13:26:33 +0700

    feat: add required validation and error handling for scope_value in API key form

--- app/frontend/src/lib/data/model/iam/api-keys/schema.ts
+++ app/frontend/src/lib/data/model/iam/api-keys/schema.ts
@@ -4,18 +4,24 @@ export const apiKeySchema = z.object({
   name: z.string("Api Key name is required.").min(3, "Api Key name must be at least 3 characters."),
 
   scope_type: z.string("Scope type is required."),
-  permissions: z.array(z.string()).default([]),
+  scope_value: z.string().array().min(1, {
+    message: "Please select at least 1."
+  }),
+  permissions: z.string().array().min(1, {
+    message: "Please select at least 1 permission."
+  }),
 
-  created_at: z.string().transform((str) => new Date(str)),
-  updated_at: z.string().transform((str) => new Date(str)),
+  expired_at: z.coerce.date().nullable().default(null)
 });
 
 export const createApiKeySchema = apiKeySchema.pick({
   name: true,
   scope_type: true,
+  scope_value: true,
+  permissions: true,
+  expired_at: true,
 });
 
 export const updateApiKeySchema = apiKeySchema.pick({
-  name: true,
-  scope_type: true,
+  expired_at: true
 });
