f9cf815f10cd56f3baffd6b4ea6ef5fbf0b3e7ae
Karnravee Chalermmuang <ptmwilfk@gmail.com>
2026-03-20 12:10:42 +0700

    feat: update API key date columns and add new date display components

---
+++ app/frontend/src/lib/components/app/iam/api-keys/data-tables/api-key-created-at-cell.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import DateText from "@/components/app/texts/date-text.svelte";
+
+  import { ApiKeyRecord } from "@/data/model/iam/api-keys/record";
+
+  import type { DataTableCellBaseProps } from "@/components/app/datasource-table/types";
+
+  // Props
+  let { record: apiKey }: DataTableCellBaseProps<ApiKeyRecord> = $props();
+
+</script>
+
+  <DateText datetime={apiKey.created_at} datetimeFormat="MMM dd, yyyy HH:mm:ss" size="sm" showTooltip></DateText>
