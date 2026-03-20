f7dbbc6b98798113091b76cabdf478a2b8c63983
Karnravee Chalermmuang <ptmwilfk@gmail.com>
2026-03-20 12:10:42 +0700

    feat: update API key date columns and add new date display components

--- app/frontend/src/lib/components/app/iam/api-keys/data-tables/api-key-columns.ts
+++ app/frontend/src/lib/components/app/iam/api-keys/data-tables/api-key-columns.ts
@@ -1,10 +1,12 @@
+import type { Component } from "svelte";
+
 import ApiKeyCell from "@/components/app/iam/api-keys/data-tables/api-key-cell.svelte";
 import ApiKeyPermissionsCell from "@/components/app/iam/api-keys/data-tables/api-key-permissions-cell.svelte";
 import ApiKeyScopeTypeCell from "@/components/app/iam/api-keys/data-tables/api-key-scope-type-cell.svelte";
 
 import { ApiKeyRecord } from "@/data/model/iam/api-keys/record";
 
-import type { ColumnSettings, ColumnsSettings } from "@/components/app/datasource-table/types";
+import type { ColumnSettings, ColumnsSettings, DataTableCellBaseProps } from "@/components/app/datasource-table/types";
 
 export const apiKeyCreatedAtColumn = (params: { label: string }): ColumnSettings<ApiKeyRecord> => {
   const { label = "Created At" } = params;
@@ -24,21 +26,16 @@ export const apiKeyCreatedAtColumn = (params: { label: string }): ColumnSettings
   };
 };
 
-export const apiKeyLastUsedColumn = (params: { label: string }): ColumnSettings<ApiKeyRecord> => {
-  const { label = "Last Used" } = params;
+export const apiKeyDateColumn = (params: { label: string, cellComponent?: Component<DataTableCellBaseProps<ApiKeyRecord>, object, "">; }): ColumnSettings<ApiKeyRecord> => {
+  const { label = "Last Used", cellComponent } = params;
   return {
     label,
     dataType: "datetime",
     clickable: false,
     sortable: true,
-    filterable: true,
-    filterOptions: {
-      filterKey: "last_used",
-      filterBy: "date-range",
-      filterOperation: "gte",
-    },
     visible: true,
     hidable: false,
+    cellComponent,
   };
 };
 
@@ -82,22 +79,9 @@ export const apiKeyColumns: ColumnsSettings<ApiKeyRecord> = {
     hidable: false,
     cellComponent: ApiKeyPermissionsCell,
   },
-  last_used: apiKeyLastUsedColumn({ label: "Last Used" }),
-  created_at: apiKeyCreatedAtColumn({ label: "Created At" }),
-  expire_at: {
-    label: "Expired At",
-    dataType: "datetime",
-    clickable: false,
-    sortable: true,
-    filterable: true,
-    filterOptions: {
-      filterKey: "expire_at",
-      filterBy: "date-range",
-      filterOperation: "gte",
-    },
-    visible: true,
-    hidable: false,
-  },
+  last_used_at: apiKeyDateColumn({ label: "Last Used" }),
+  created_at: apiKeyDateColumn({ label: "Created At", }),
+  expire_at:  apiKeyDateColumn({ label: "Expired At", }),
   action: {
     label: "Action",
     dataType: "string",
