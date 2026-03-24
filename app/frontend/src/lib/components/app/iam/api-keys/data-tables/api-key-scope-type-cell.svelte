e20fac5bd6c7a1b3f6c05b77f335b14d8e32d682
Karnravee Chalermmuang <ptmwilfk@gmail.com>
2026-03-24 16:34:39 +0700

    feat: enhance API key management by adding organization and project context handling

--- app/frontend/src/lib/components/app/iam/api-keys/data-tables/api-key-scope-type-cell.svelte
+++ app/frontend/src/lib/components/app/iam/api-keys/data-tables/api-key-scope-type-cell.svelte
@@ -4,14 +4,63 @@
   import { ApiKeyRecord } from "@/data/model/iam/api-keys/record";
 
   import type { DataTableCellBaseProps } from "@/components/app/datasource-table/types";
+  import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+  import type { ProjectRecord } from "@/data/model/dataset/projects/project-record";
+  import type { OrganizationRecord } from "@/data/model/iam/organizations/record";
 
   // Props
-  let { record: apiKeyRecord }: DataTableCellBaseProps<ApiKeyRecord> = $props();
+  let { record: apiKeyRecord, contexts }: DataTableCellBaseProps<ApiKeyRecord> = $props();
+
+  // Types
+  type Context = {
+    permissions: Record<string, { title: string }>;
+    organizations: OrganizationRecord[];
+    projects: ProjectRecord[];
+  };
+  let { permissions, organizations, projects }: Context = $derived(
+    (contexts as Context) || {
+      permissions: [],
+      organizations: [],
+      projects: [],
+    },
+  );
 
   // Variables
-  let { scopeTypeLabel } = $derived(apiKeyRecord);
+  let { scopeTypeLabel, scope_value, scope_type } = $derived(apiKeyRecord);
+
+  // Functions
+  function getOrganizationName(orgId: string) {
+    return organizations.find((org) => org.id == orgId)?.name || orgId;
+  }
+
+  function getProjectName(projectId: string) {
+    return projects.find((project) => project.id == projectId)?.name || projectId;
+  }
 </script>
 
-<Badge>
-  {scopeTypeLabel}
-</Badge>
+<TooltipProvider disableCloseOnTriggerClick>
+  <Tooltip delayDuration={0}>
+    <TooltipTrigger>
+      <Badge>
+        {scopeTypeLabel}
+      </Badge>
+    </TooltipTrigger>
+
+    <TooltipContent>
+      {#if scope_type === "all"}
+        All Access
+      {:else}
+        {`${scopeTypeLabel}: `}
+      {/if}
+      {#each scope_value as value}
+        <div class="flex flex-col gap-1">
+          {#if scope_type === "org"}
+            {getOrganizationName(value)}
+          {:else if scope_type === "project"}
+            {getProjectName(value)}
+          {/if}
+        </div>
+      {/each}
+    </TooltipContent>
+  </Tooltip>
+</TooltipProvider>
