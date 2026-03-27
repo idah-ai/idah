2f887e9300357a20c6d8febe1240e420c68e24f6
Karnravee Chalermmuang <ptmwilfk@gmail.com>
2026-03-27 10:24:38 +0700

    Refactor: update import paths for Calendar and ButtonVariant in calendar.svelte

--- app/frontend/src/lib/components/ui/calendar/calendar.svelte
+++ app/frontend/src/lib/components/ui/calendar/calendar.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
+  import * as Calendar from "$lib/components/ui/calendar/index.js";
   import { cn, type WithoutChildrenOrChild } from "$lib/utils.js";
   import { isEqualMonth, type DateValue } from "@internationalized/date";
   import { Calendar as CalendarPrimitive } from "bits-ui";
+
+  import type { ButtonVariant } from "$lib/components/ui/button/button.svelte";
   import type { Snippet } from "svelte";
-  import type { ButtonVariant } from "../button/button.svelte";
-  import * as Calendar from "./index.js";
 
   let {
     ref = $bindable(null),
