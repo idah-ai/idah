754402197e4b1f3cbc87a7fcf9fbb6cf152f54b2
Karnravee Chalermmuang <ptmwilfk@gmail.com>
2026-04-01 12:01:10 +0700

    Refactor IdahImage module structure to include Media submodule for better organization

--- plugins/idah-image/backends/media/idah_image.rb
+++ plugins/idah-image/backends/media/idah_image.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 module IdahImage
-  def self.init(context)
-    context.register_processor(
-      "idah-image",
-      class_name: "IdahImage::Processor::Image",
-      options_class_name: "IdahImage::Processor::Options"
-    )
+  module Media
+    def self.init(context)
+      context.register_processor(
+        "idah-image",
+        class_name: "IdahImage::Processor::Image",
+        options_class_name: "IdahImage::Processor::Options"
+      )
+    end
   end
 end
