7718264cfafa0e5b03e5e34ade0f3396b146e6e0
whelsm <jiranop.phinyo@gmail.com>
2026-04-07 10:41:35 +0700

    chore: implement shift current frame range logic when press arrow key left/right

--- plugins/idah-video/frontend/src/lib/plugin/video-annotation-activity/timeline/utils.ts
+++ plugins/idah-video/frontend/src/lib/plugin/video-annotation-activity/timeline/utils.ts
@@ -1,10 +1,13 @@
 import { get } from "svelte/store";
 
+import { currentFrame } from "$lib/plugin/video-annotation-activity/store/store";
 import {
   currentFrameRange,
   framePerScale,
+  setCurrentFrameRange,
   TIMELINE_ROW_HEADER_WIDTH,
-  timelineCellWidth as timelineCellWidthStore,
+  timelineCellWidth,
+  timelineRulerWidth,
 } from "$lib/plugin/video-annotation-activity/timeline/store";
 
 import type { AnnotationGroup } from "$idah/context/annotation-context";
@@ -24,9 +27,9 @@ export function getAnnotationGroupFrameRanges(props: {
 
 export function getFrameFromMouseX(props: { clientX: number }) {
   const { clientX } = props;
-  const timelineCellWidth = get(timelineCellWidthStore);
+  const timelineCellWidthStore = get(timelineCellWidth);
   const framePerScaleStore = get(framePerScale);
-  const frameFloat = (clientX - TIMELINE_ROW_HEADER_WIDTH) / timelineCellWidth;
+  const frameFloat = (clientX - TIMELINE_ROW_HEADER_WIDTH) / timelineCellWidthStore;
 
   return Math.ceil(frameFloat * framePerScaleStore);
 }
@@ -34,20 +37,20 @@ export function getFrameFromMouseX(props: { clientX: number }) {
 export function getMouseXFromFrame(props: { frame: number }) {
   const { frame } = props;
 
-  const timelineCellWidth = get(timelineCellWidthStore);
+  const timelineCellWidthStore = get(timelineCellWidth);
   const framePerScaleStore = get(framePerScale);
   const startOfCurrentFrameRange = get(currentFrameRange)[0];
   const frameFloat = (frame - startOfCurrentFrameRange) / framePerScaleStore;
 
-  const actualMouseX = Math.ceil(frameFloat * timelineCellWidth) + TIMELINE_ROW_HEADER_WIDTH;
-  const displayMouseX = Number(actualMouseX - timelineCellWidth) + 1;
+  const actualMouseX = Math.ceil(frameFloat * timelineCellWidthStore) + TIMELINE_ROW_HEADER_WIDTH;
+  const displayMouseX = Number(actualMouseX - timelineCellWidthStore) + 1;
 
   return Math.max(displayMouseX, TIMELINE_ROW_HEADER_WIDTH);
 }
 
 export function getSelectedFrameXFromCurrentFrame(props: { currentFrame: number }) {
   const { currentFrame } = props;
-  const timelineCellWidth = get(timelineCellWidthStore);
+  const timelineCellWidthStore = get(timelineCellWidth);
   const framePerScaleStore = get(framePerScale);
   const startOfCurrentFrameRange = get(currentFrameRange)[0];
 
@@ -57,5 +60,41 @@ export function getSelectedFrameXFromCurrentFrame(props: { currentFrame: number 
   const normalizedFrameInScale = normalizedCurrentFrame - normalizedStartOfCurrentFrameRange;
   const middleOfnormalizedFrameInScale = normalizedFrameInScale - Number(0.5 / framePerScaleStore);
 
-  return Number(middleOfnormalizedFrameInScale * timelineCellWidth) + TIMELINE_ROW_HEADER_WIDTH;
+  return Number(middleOfnormalizedFrameInScale * timelineCellWidthStore) + TIMELINE_ROW_HEADER_WIDTH;
+}
+
+export function paginateCurrentFrameRange(props: { frameStep: number }) {
+  /**
+   * NOTE:
+   * frameStep is positive when you want to shift current frame range to the right
+   * frameStep is negative when you want to shift current frame range to the left
+   */
+  const { frameStep } = props;
+
+  /** Need to check shift left / right, otherwise the currentFrameSpan will start at negative */
+  const shiftRight = frameStep > 0;
+  const shiftLeft = frameStep < 0;
+
+  const currentFrameBeforePress = get(currentFrame);
+  const currentFrameAfterPress = currentFrameBeforePress + frameStep;
+  const currentFrameRangeStore = get(currentFrameRange);
+  const [startFrameIndexOfCurrentFrameRange, endFrameIndexOfCurrentFrameRange] = currentFrameRangeStore;
+
+  const timelineRulerWidthStore = get(timelineRulerWidth);
+  const tinmelineCellWidthStore = get(timelineCellWidth);
+  const rulerScale = timelineRulerWidthStore / tinmelineCellWidthStore;
+
+  if (currentFrameAfterPress > endFrameIndexOfCurrentFrameRange && shiftRight) {
+    const newStart = startFrameIndexOfCurrentFrameRange + frameStep;
+    const newEnd = endFrameIndexOfCurrentFrameRange + frameStep;
+    setCurrentFrameRange([newStart, newEnd]);
+  }
+
+  if (currentFrameAfterPress <= startFrameIndexOfCurrentFrameRange && shiftLeft) {
+    const newStart = Math.max(startFrameIndexOfCurrentFrameRange + frameStep, 0);
+    const newEnd = Math.max(endFrameIndexOfCurrentFrameRange + frameStep, rulerScale);
+    console.log(newStart, newEnd);
+
+    setCurrentFrameRange([newStart, newEnd]);
+  }
 }
