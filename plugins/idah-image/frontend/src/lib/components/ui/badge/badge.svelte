1d1f01164b4f3a06aea44db47325eff328aab692
Karnravee Chalermmuang <ptmwilfk@gmail.com>
2026-04-27 10:21:50 +0700

    feat: implement Badge component with variants and styles

---
+++ plugins/idah-image/frontend/src/lib/components/ui/badge/badge.svelte
@@ -0,0 +1,66 @@
+<script lang="ts" module>
+	import { tv, type VariantProps } from "tailwind-variants";
+
+	export const badgeVariants = tv({
+		base: "h-5 gap-1 rounded-4xl border border-transparent px-2 py-0.5 text-xs font-medium transition-all has-data-[icon=inline-end]:pr-1.5 has-data-[icon=inline-start]:pl-1.5 [&>svg]:size-3! focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive group/badge inline-flex w-fit shrink-0 items-center justify-center overflow-hidden whitespace-nowrap transition-colors focus-visible:ring-[3px] [&>svg]:pointer-events-none",
+		variants: {
+			variant: {
+				 default: "bg-primary text-primary-foreground [a&]:hover:bg-primary/90 border-transparent",
+        secondary: "bg-secondary text-secondary-foreground [a&]:hover:bg-secondary/90 border-transparent",
+        destructive:
+          "bg-destructive [a&]:hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/70 border-transparent text-white",
+        outline: "text-foreground [a&]:hover:bg-accent [a&]:hover:text-accent-foreground",
+        warning:
+          "bg-amber-300 text-secondary-foreground dark:text-secondary [a&]:hover:bg-amber-300/90 border-transparent",
+        success: "bg-green-600 text-primary-foreground [a&]:hover:bg-green-600/90 border-transparent",
+        "success-200":
+          "bg-green-200 text-secondary-foreground [a&]:hover:bg-green-200/90 border-transparent dark:bg-green-700",
+        info: "bg-sky-200 text-secondary-foreground [a&]:hover:bg-sky-200/90 border-transparent dark:bg-sky-700",
+        gray: "bg-gray-200 text-secondary-foreground dark:bg-gray-600 [a&]:hover:bg-secondary/90 border-transparent",
+      },
+			rounded: {
+        none: "rounded-none",
+        sm: "rounded-sm",
+        md: "rounded-md",
+        default: "rounded-lg",
+        full: "rounded-full",
+      },
+		},
+		defaultVariants: {
+			variant: "default",
+      rounded: "default",
+		},
+	});
+
+	export type BadgeVariant = VariantProps<typeof badgeVariants>["variant"];
+	export type BadgeRounded = VariantProps<typeof badgeVariants>["rounded"];
+</script>
+
+<script lang="ts">
+	import { cn, type WithElementRef } from "$lib/utils.js";
+	import type { HTMLAnchorAttributes } from "svelte/elements";
+
+	let {
+		ref = $bindable(null),
+		href,
+		class: className,
+		variant = "default",
+		rounded = "default",
+		children,
+		...restProps
+	}: WithElementRef<HTMLAnchorAttributes> & {
+		variant?: BadgeVariant;
+		rounded?: BadgeRounded;
+	} = $props();
+</script>
+
+<svelte:element
+	this={href ? "a" : "span"}
+	bind:this={ref}
+	data-slot="badge"
+	{href}
+	class={cn(badgeVariants({ variant, rounded }), className)}
+	{...restProps}
+>
+	{@render children?.()}
+</svelte:element>
