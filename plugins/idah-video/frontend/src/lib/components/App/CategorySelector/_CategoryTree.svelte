6656de91dfcedbac2015fbcb2db4ca7ff04ac777
Karnravee Chalermmuang <ptmwilfk@gmail.com>
2026-05-29 13:16:05 +0700

    refactor: improve visibility of category action buttons on hover

--- plugins/idah-video/frontend/src/lib/components/App/CategorySelector/_CategoryTree.svelte
+++ plugins/idah-video/frontend/src/lib/components/App/CategorySelector/_CategoryTree.svelte
@@ -342,22 +342,20 @@
             })}
 
             <!-- Icon Actions -->
-            <div class="ml-auto flex content-center items-center gap-0">
+             <div class="ml-auto flex shrink-0 items-center">
               {#if mode == DEFAULT_MODE}
                 {#each actions as { label, icon, alwaysShow, disabled, onClick }, index (index)}
-                  <CategoryAction
-                    {label}
-                    {icon}
-                    {disabled}
-                    onclick={(e) => {
-                      e.stopPropagation();
-                      onClick(e);
-                    }}
-                    class={cn("opacity-0", {
-                      "opacity-100": alwaysShow,
-                      "group-hover:opacity-100": !alwaysShow,
-                    })}
-                  ></CategoryAction>
+                  <div class={cn("", alwaysShow ? "opacity-100" : "opacity-0 group-hover:opacity-100")}>
+                    <CategoryAction
+                      {label}
+                      {icon}
+                      {disabled}
+                      onclick={(e) => {
+                        e.stopPropagation();
+                        onClick(e);
+                      }}
+                    ></CategoryAction>
+                  </div>
                 {/each}
               {/if}
 
