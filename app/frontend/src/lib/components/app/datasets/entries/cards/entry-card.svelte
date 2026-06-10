ab0f7ef934230358b8f5d305e9cab2962f0adcd8
Karnravee Chalermmuang <ptmwilfk@gmail.com>
2026-06-10 14:07:44 +0700

    Fetch dataset information when retrieving entry data to ensure up-to-date state

--- app/frontend/src/lib/components/app/datasets/entries/cards/entry-card.svelte
+++ app/frontend/src/lib/components/app/datasets/entries/cards/entry-card.svelte
@@ -175,7 +175,9 @@
            */
           if (!jobId) {
             /** Fetch the entry again to get the job_id and update the entry state (to prevent index cache) */
-            const entryRes = await entriesBackendDataSource.get(entryId, { noCache: true });
+            const entryRes = await entriesBackendDataSource.get(entryId, {
+              included: ["dataset"], 
+              noCache: true });
             entry = entryRes.data;
             jobId = entryRes.data.job_id;
           }
