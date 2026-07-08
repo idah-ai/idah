3b6d8a208538421efe81ed44e6626f53976a6fb5
Karnravee Chalermmuang <ptmwilfk@gmail.com>
2026-07-08 11:23:40 +0700

    capitalize workflow step in badge component

--- app/frontend/src/lib/plugin/layout/header/annotation-header-bar-state-badge.svelte
+++ app/frontend/src/lib/plugin/layout/header/annotation-header-bar-state-badge.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import Badge from "@/components/ui/badge/badge.svelte";
 
+  import { capitalize } from "@/utils/string";
+
   import type { IdahDriverV2 } from "@/plugin/v2/driver";
 
   type Props = {
@@ -10,4 +12,4 @@
   let { driver }: Props = $props();
 </script>
 
-<Badge variant="info">{driver.workflowStep}</Badge>
+<Badge variant="info">{capitalize(driver.workflowStep)}</Badge>
