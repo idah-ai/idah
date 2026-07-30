605e255ab820d3161145113086ebc081b4b0e334
Karnravee Chalermmuang <ptmwilfk@gmail.com>
2026-07-30 13:38:44 +0700

    Refactor import statements for dropdown menu components for improved readability

--- app/frontend/src/lib/components/app/datasets/entries/dropdown-menus/entry-dropdown-menu.svelte
+++ app/frontend/src/lib/components/app/datasets/entries/dropdown-menus/entry-dropdown-menu.svelte
@@ -7,11 +7,11 @@
   import ConfirmModal from "@/components/app/overlays/modals/confirm-modal.svelte";
   import Button from "@/components/ui/button/button.svelte";
   import {
-      DropdownMenu,
-      DropdownMenuContent,
-      DropdownMenuGroup,
-      DropdownMenuItem,
-      DropdownMenuTrigger,
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
   } from "@/components/ui/dropdown-menu";
 
   import { getEntryDropdownMenuActions } from "@/components/app/datasets/entries/dropdown-menus/entry-dropdown-menu";
