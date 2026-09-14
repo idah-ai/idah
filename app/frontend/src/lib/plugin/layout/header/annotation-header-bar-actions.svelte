1e1f5b1ceaa11e268925cb569ed6c41c97f2e96c
tojo <yraimtj@gmail.com>
2026-09-14 16:05:34 +0300

    disable action header when entry in externat step

--- app/frontend/src/lib/plugin/layout/header/annotation-header-bar-actions.svelte
+++ app/frontend/src/lib/plugin/layout/header/annotation-header-bar-actions.svelte
@@ -74,7 +74,7 @@
   });
 
   let showAutoSelect = $derived(
-    driver.entryStatus && driver.entryStatus !== "completed" && driver.entryStatus !== "errored",
+    driver.entryStatus && driver.entryStatus !== "completed" && driver.entryStatus !== "errored" && !driver.externalSteps.includes(driver.workflowStep),
   );
 
   // Switch to review mode when entry is completed (only evaluated at mount since entryStatus is static)
@@ -367,7 +367,7 @@
     </ToolTooltip>
   {/if}
 
-  {#if driver.workflowStep === "done"}
+  {#if driver.workflowStep === "done" || driver.externalSteps.includes(driver.workflowStep)}
     <!-- TODO: What to show? -->
   {:else if driver.workflowStep === "review"}
     {#if currentStepConfig}
