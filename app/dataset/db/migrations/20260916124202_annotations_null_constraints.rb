8e5b406dbf798473eed332bc98a3e967fba6e2f8
Brian West <brian.west@ingedata.ai>
2026-09-16 11:42:05 +0700

    delayed migrations

---
+++ app/dataset/db/migrations/20260916124202_annotations_null_constraints.rb
@@ -0,0 +1,17 @@
+Sequel.migration do
+  up do
+    alter_table(:annotations) do
+      set_column_not_null :shape_type
+      set_column_not_null :shape_args
+      set_column_not_null :category
+    end
+  end
+
+  down do
+    alter_table(:annotations) do
+      set_column_allow_null :shape_type
+      set_column_allow_null :shape_args
+      set_column_allow_null :category
+    end
+  end
+end
---
+++ app/dataset/db/migrations/20260916124203_drop_legacy_columns.rb
@@ -0,0 +1,15 @@
+Sequel.migration do
+  up do
+    alter_table(:annotations) do
+      drop_column :dimensions
+      drop_column :annotation
+    end
+  end
+
+  down do
+    alter_table(:annotations) do
+      add_column :dimensions, :jsonb
+      add_column :annotation, :jsonb
+    end
+  end
+end
