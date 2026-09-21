0d9210bfbad5d05582e73678da018ced2be07b93
brian-west-ingedata <brian.west@ingedata.ai>
2026-09-21 10:31:17 +0700

    fix: annotations refactor delayed drop columns migration (#602)
    * soft deletion and annotation record review wip
    
    * migration fix
    
    * CI fixes
    
    * add missing commas
    
    * change quote to double quote
    
    * null guards
    
    * remove spaces
    
    * update can delete spec (soft-delete)
    
    * fix admin delete_by_email spec
    
    * specs round
    
    * bump updcli version
    
    * soft deletion refactorisation consolidation
    
    * missed consolidations
    
    * CI type checking
    
    * Style/StringLiterals: Prefer double-quoted strings
    
    * update migration date
    
    * remove drop column and constraints migration
    
    * Revert "remove drop column and constraints migration"
    
    This reverts commit fc8dda92c767095ac5fe1325e5a061e373973a96.
    
    * delayed migrations
    
    * remove dimensions constraint
    
    * remove annotation constraint
    
    * frozen literal
    
    * Add an empty line after magic comments
    
    ---------
    
    Co-authored-by: roger <rcserrano06@gmail.com>

---
+++ app/dataset/db/migrations/20260916124202_annotations_null_constraints.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+Sequel.migration do
+  up do
+    alter_table(:annotations) do
+      set_column_not_null :shape_type
+      set_column_not_null :shape_args
+      set_column_not_null :category
+    end
+  end
+
+  down do
+    alter_table(:annotations) do
+      set_column_allow_null :shape_type
+      set_column_allow_null :shape_args
+      set_column_allow_null :category
+    end
+  end
+end
